2f491c61602d0c94cbf6e1c58035df48122b2db4
nicojs <jansennico@gmail.com>
2016-02-16 08:05:55 +0100

    Removed last proof that we ever used maven stuff.

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,35 +59,16 @@ module.exports = function(grunt) {
       }
     },
     /* End code coverage */
-    /* Maven related stuff.. */
-    gruntMavenProperties: grunt.file.readJSON('grunt-maven.json'),
-    mavenPrepare: {
-      options: {
-        resources: ['**']
-      },
-      dev: {}
-
-    },
-    pkg: grunt.file.readJSON('package.json'),
-    mavenDist: {
-      options: {
-        warName: 'ROOT',
-        deliverables: ["<%= pkg.name %>*.js", "*.js", "!js/test/**"]
-      },
-      dev: {}
-    },
-    /* End of Maven related stuff.. */
   });
 
-  grunt.loadNpmTasks('grunt-maven');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-mocha-istanbul');
 
-  grunt.registerTask('default', ['mavenPrepare', 'coverage', 'mavenDist']);
+  grunt.registerTask('default', ['test']);
   grunt.registerTask('watch-test', ['test', 'watch']);
-  grunt.registerTask('test', ['mochaTest:unit']);
+  grunt.registerTask('test', ['coverage', 'integration']);
   grunt.registerTask('integration', ['mochaTest:integration']);
   grunt.registerTask('coverage', ['mocha_istanbul:coverage']);
 
