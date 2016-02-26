b2a652bce9b466a47cfcb05af21511457da672f1
nicojs <jansennico@gmail.com>
2016-02-26 14:35:27 +0100

    Altered api for StrykerOptions, start with clean slate

--- src/api/StrykerOptions.ts
+++ src/api/StrykerOptions.ts
@@ -3,26 +3,6 @@
 interface StrykerOptions {
   // this ensures that custom config for for example 'karma' can be added under the 'karma' key
   [customConfig: string]: any;
-  
-  /**
-   * A list of library files.
-   */
-  libs: string[];
-  
-  /**
-   * Amount of additional time, in milliseconds, the mutation test is allowed to run
-   */
-  timeoutMs: number;
-  
-  /**
-   * The factor is applied on top of the other timeouts when during mutation testing
-   */
-  timeoutFactor: number;
-  
-  /**
-   * Runs each test separately instead of entire test files
-   */
-  individualTests: boolean;
 }
 
 export default StrykerOptions;
