ae5bcd9c5f8cfdd25f0626a8ca9b78936b21edeb
nicojs <jansennico@gmail.com>
2016-02-28 00:22:45 +0100

    Altered first karma test runner integration test to make use of chai-as-promised

--- test/integration/karma-runner/KarmaTestRunnerSpec.ts
+++ test/integration/karma-runner/KarmaTestRunnerSpec.ts
@@ -1,8 +1,12 @@
 'use strict';
 
-import {expect} from 'chai';
+import * as chai from 'chai';
 import KarmaTestRunner from '../../../src/karma-runner/KarmaTestRunner';
 import TestResult from '../../../src/api/TestResult';
+import TestRunResult from '../../../src/api/TestRunResult';
+import * as chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+let expect = chai.expect;
 
 describe('KarmaTestRunner', () => {
 
@@ -14,21 +18,17 @@ describe('KarmaTestRunner', () => {
       sut = new KarmaTestRunner(['test/sampleProject/src/Add.js'], ['test/sampleProject/test/AddSpec.js'], { port: 9877, tempFolder: '.tmp' }, {});
     });
 
-    it('should report completed tests', done => {
+    it.only('should report completed tests', () => {
+      return expect(sut.run()).to.eventually.satisfy((testResult: TestRunResult) => {
+        expect(testResult.succeeded).to.be.eq(5);
+        expect(testResult.failed).to.be.eq(0);
+        expect(testResult.result).to.be.eq(TestResult.Complete);
+        let coverageOfFirstFile = testResult.coverage[Object.keys(testResult.coverage)[0]];
+        expect(coverageOfFirstFile.statementMap).to.be.ok;
+        expect(coverageOfFirstFile.s).to.be.ok;
+        return true;
+      });
 
-      sut.run().then(testResult => {
-        try {
-          expect(testResult.succeeded).to.be.eq(5);
-          expect(testResult.failed).to.be.eq(0);
-          expect(testResult.result).to.be.eq(TestResult.Complete);
-          let coverageOfFirstFile = testResult.coverage[Object.keys(testResult.coverage)[0]];
-          expect(coverageOfFirstFile.statementMap).to.be.ok;
-          expect(coverageOfFirstFile.s).to.be.ok;
-          done();
-        } catch (err) {
-          done(err);
-        }
-      }, err => console.error('ERRPR' + err));
     });
   });
 
