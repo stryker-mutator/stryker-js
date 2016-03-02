61e0a79923a10998163b2d8b026a47ea9b14998b
nicojs <jansennico@gmail.com>
2016-03-02 09:59:32 +0100

    Removed tempFolder from runnerOptions, as we'll move away from code coverage written and

--- src/api/test_runner/RunnerOptions.ts
+++ src/api/test_runner/RunnerOptions.ts
@@ -6,11 +6,6 @@ interface RunnerOptions{
   port: number;
   
   /**
-   * Represents a temp folder which the test runner can choose to use
-   */
-  tempFolder: string;
-  
-  /**
    * Enable code coverage. When enabled, the calling party is interested in code coverage.
    */
   coverageEnabled?: boolean
