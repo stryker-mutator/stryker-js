7ae460174962c3fa3d03952735f900e3afc7e9c7
nicojs <jansennico@gmail.com>
2016-03-03 08:50:42 +0100

    Added plugins section to StrykerOptions

--- src/api/core/StrykerOptions.ts
+++ src/api/core/StrykerOptions.ts
@@ -3,6 +3,11 @@
 interface StrykerOptions {
   // this ensures that custom config for for example 'karma' can be added under the 'karma' key
   [customConfig: string]: any;
+  
+  /**
+   * A list of plugins. These plugins will be imported ('required') by Stryker upon loading.
+   */
+  plugins?: string[];
 }
 
 export default StrykerOptions;
