f6fa458e074e834ee1c1ca2696a09f75ffe3b7fb
nicojs <jansennico@gmail.com>
2016-03-06 20:49:15 +0100

    Added new file io helper to api

---
+++ src/api/util/StrykerTempFolder.ts
@@ -0,0 +1,55 @@
+import * as os from 'os'
+import * as fs from 'fs';
+import * as path from 'path';
+
+let baseTempFolder = os.tmpdir() + path.sep + 'stryker';
+let tempFolder = baseTempFolder + path.sep + random();
+ensureFolderExists(baseTempFolder);
+ensureFolderExists(tempFolder);
+
+function createRandomFolder(prefix: string) {
+  return ensureFolderExists(tempFolder + path.sep + prefix + random());
+}
+
+function random() {
+  Math.ceil(Math.random() * 10000000)
+}
+
+function ensureFolderExists(path: string) {
+  if (!fileOrFolderExists(path)) {
+    fs.mkdirSync(path);
+  }
+  return path;
+}
+
+function writeFile(fileName: string, data: string): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    fs.writeFile(fileName, data, { encoding: 'utf8' }, (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+  
+/**
+   * Checks if a file or folder exists.
+   * @function
+   * @param path - The path to the file or folder.
+   * @returns True if the file exists.
+   */
+function fileOrFolderExists(path: string): boolean {
+  try {
+    var stats = fs.lstatSync(path);
+    return true;
+  } catch (errror) {
+    return false;
+  }
+};
+
+export default {
+  createRandomFolder,
+  writeFile
+};
