275b1fb45fd78c0710d1800484091c59ce13bdac
nicojs <jansennico@gmail.com>
2016-03-06 07:39:20 +0100

    Improved Factory api, now including a suggestion in the error and added the constructor settings argument as generic type

--- src/api/test_runner/TestRunnerFactory.ts
+++ src/api/test_runner/TestRunnerFactory.ts
@@ -1,18 +1,23 @@
 import {Factory} from '../core';
 import TestRunner from './TestRunner';
+import RunnerOptions from './RunnerOptions';
 
-namespace TestRunnerFactory{
-  
-  class TestRunnerFactory extends Factory<TestRunner> {
-    constructor(){
+namespace TestRunnerFactory {
+
+  class TestRunnerFactory extends Factory<RunnerOptions, TestRunner> {
+    constructor() {
       super('testrunner');
     }
+    
+    importSuggestion(name: string){
+      return `stryker-${name}-runner`;
+    }
   }
-  
-  let testRunnerInstance = new TestRunnerFactory();
-  
-  export function instance(){
-    return <Factory<TestRunner>>testRunnerInstance;
+
+  let testRunnerFactoryInstance = new TestRunnerFactory();
+
+  export function instance() {
+    return <Factory<RunnerOptions, TestRunner>>testRunnerFactoryInstance;
   }
 }
 
