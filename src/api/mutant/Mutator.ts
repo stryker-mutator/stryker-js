f224d1bfc79dbba1bd0408d0e2ad8624996c808f
Simon de Lang <simondelang@gmail.com>
2016-03-29 15:23:49 +0200

    docs(Factory, Mutator) Add API documentation

--- src/api/mutant/Mutator.ts
+++ src/api/mutant/Mutator.ts
@@ -1,8 +1,17 @@
-
+/**
+ * Represents a class which can mutate parts of an Abstract Syntax Tree.
+ */
 interface Mutator {
-  
+  /**
+   * The name of the Mutator which may be used by reporters.
+   */
   name: string;
   
+  /**
+   * Applies the Mutator to a Node. This can result in multiple mutated Nodes.
+   * @param node A FROZEN Node which could be cloned and mutated.
+   * @returns An array of mutated Nodes.
+   */
   applyMutations(node: ESTree.Node): ESTree.Node[];
 }
 
