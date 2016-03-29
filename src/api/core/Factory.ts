f8a58d950af96bfe5c0eb4850975cb5a06598736
Simon de Lang <simondelang@gmail.com>
2016-03-29 15:23:49 +0200

    docs(Factory, Mutator) Add API documentation

--- src/api/core/Factory.ts
+++ src/api/core/Factory.ts
@@ -1,29 +1,42 @@
-
+/**
+ * Represents a Factory to which items can register themselves and which can be used to create instances of said items.
+ */
 abstract class Factory<TSettings, T> {
 
+  /**
+   * Creates a new Factory.
+   * @param factoryName The name of the Factory.
+   */
   constructor(private factoryName: string) {
   }
 
   private classMap: { [name: string]: { new (settings: TSettings): T } } = Object.create(null);
 
   /**
-   * Retrieves the known names registered to this factory
+   * Retrieves the known names registered to this factory.
+   * @returns A list of sorted items which are registered.
    */
-  knownNames() {
+  knownNames(): string[] {
     var keys = Object.keys(this.classMap);
     keys.sort();
     return keys;
   }
 
   /**
-   * Registeres a constructor function to this factory 
+   * Registers a constructor function to this factory.
+   * @param name The name of the item.
+   * @param constructor The constructor of the item.
    */
   register(name: string, constructor: { new (settings: TSettings): T }): void {
     this.classMap[name] = constructor;
   }
    
   /**
-   * Creates a new 
+   * Creates a new instance of a registered item.
+   * @param name The name of the item.
+   * @param settings The settings object related to the item.
+   * @throws Will throw if no item has been registered with the specified name
+   * @returns A new instance of the requested item.
    */
   create(name: string, settings: TSettings): T {
     if (Object.keys(this.classMap).indexOf(name) < 0) {
