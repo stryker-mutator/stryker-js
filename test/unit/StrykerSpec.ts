a8dcb69652ef523c82fb11ee0bc3df661ac79bad
nicojs <jansennico@gmail.com>
2016-03-31 09:46:11 +0200

    (fix-errors-not-reported) Added logging for when errors occurred during initial test run. Also added a test for it

--- test/unit/StrykerSpec.ts
+++ test/unit/StrykerSpec.ts
@@ -1,38 +1,64 @@
-//TODO: Fix this test when the new testrunner has been implemented
+'use strict';
 
-// 'use strict';
+import FileUtils from '../../src/utils/FileUtils';
+import Stryker from '../../src/Stryker';
+import {InputFile} from '../../src/api/core';
+import {RunResult, TestResult} from '../../src/api/test_runner';
+import {expect} from 'chai';
+import * as sinon from 'sinon';
+import * as inputFileResolver from '../../src/InputFileResolver';
+import * as testRunnerOrchestrator from '../../src/TestRunnerOrchestrator';
 
-// var expect = require('chai').expect;
-// import BaseTestRunner from '../../src/testrunners/BaseTestRunner';
-// import FileUtils from '../../src/utils/FileUtils';
-// import Stryker from '../../src/Stryker';
-// import TestRunnerFactory from '../../src/TestRunnerFactory';
-// require('mocha-sinon');
+describe('Stryker runMutationTest()', function() {
+  let sut: Stryker;
+  let sandbox: Sinon.SinonSandbox;
+  let inputFileResolverStub: any;
+  let testRunnerOrchestratorStub: any;
+  let inputFiles: InputFile[];
+  let initialRunResults: RunResult[];
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    sandbox.stub(console, 'log');
+  });
 
-// describe("Stryker", function() {
-//   class MockTestRunner extends BaseTestRunner {
-//     sourceFiles: string[];
-//     testFiles: string[];
-//   }
+  describe('with correct input file globbing', () => {
+    beforeEach(() => {
+      inputFiles = [{ path: 'someFile', shouldMutate: true }];
+      inputFileResolverStub = {
+        resolve: sandbox.stub().returns(Promise.resolve(inputFiles))
+      }
+      sandbox.stub(inputFileResolver, 'default').returns(inputFileResolverStub);
+      sut = new Stryker(['mutateFile'], ['allFiles']);
+    });
 
-//   it("should do an initial test run", function() {
-//     this.sinon.stub(FileUtils.prototype, 'createDirectory');
-//     var mockRunner = new MockTestRunner({});
-//     mockRunner.sourceFiles = [];
-//     mockRunner.testFiles = [];
-//     mockRunner.testAndCollectCoverage = function(sourceFiles, testFiles) {
-//       this.sourceFiles = sourceFiles;
-//       this.testFiles = testFiles;
-//     };
+    describe('when coverage can be collected', () => {
 
-//     var sourceFiles = ['hello.js'];
-//     var testFiles = ['helloSpec.js'];
-//     var stryker = new Stryker(sourceFiles, testFiles);
-//     stryker.testRunner = mockRunner;
+      beforeEach(() => {
+        initialRunResults = [];
+        testRunnerOrchestratorStub = {
+          recordCoverage: sandbox.stub().returns(Promise.resolve(initialRunResults))
+        };
+        sandbox.stub(testRunnerOrchestrator, 'default').returns(testRunnerOrchestratorStub);
+      });
 
-//     stryker.runMutationTest(function() { });
+      describe('but contains an error and failed tests', () => {
 
-//     expect(mockRunner.sourceFiles).to.equal(sourceFiles);
-//     expect(mockRunner.testFiles).to.equal(testFiles);
-//   });
-// });
+        beforeEach(() => {
+          initialRunResults.push({result: TestResult.Error, errorMessages: ['An error!']})
+          return sut.runMutationTest();
+        });
+        
+        it('should have logged the errors', () => {
+          expect(console.log).to.have.been.calledWith('ERROR: One or more tests errored in the initial test run:');
+          expect(console.log).to.have.been.calledWith('\t', 'An error!');
+        });
+      });
+    });
+
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+});
