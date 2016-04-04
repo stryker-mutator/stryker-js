d0235d85978d04c8f62a464a927061d0d27a2b48
Simon de Lang <simondelang@gmail.com>
2016-04-04 15:37:15 +0200

    docs(test_selector) Add API documentation

--- src/api/test_selector/TestSelector.ts
+++ src/api/test_selector/TestSelector.ts
@@ -1,7 +1,19 @@
-interface TestSelector{
-  
+/**
+ * Represents a TestSelector which can select one or more tests to be executed.
+ */
+interface TestSelector {
+
+  /**
+   * Gets the files that this TestSelector uses to select tests.
+   * @returns The files that this TestSelector uses.
+   */
   files(): string[];
-  
+
+  /**
+   * Selects a set of tests from the pool of tests.
+   * @param id A list of testID's to select.
+   * @returns A promise to eventually select the tests.
+   */
   select(id: number[]): Promise<void>;
 }
 
