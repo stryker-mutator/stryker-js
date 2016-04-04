9d823bb5648e86dadc7cd08b6ca92ae37521da1e
Simon de Lang <simondelang@gmail.com>
2016-04-04 15:37:15 +0200

    docs(test_selector) Add API documentation

--- src/api/test_selector/TestSelectorSettings.ts
+++ src/api/test_selector/TestSelectorSettings.ts
@@ -1,7 +1,12 @@
 import {StrykerOptions} from '../core';
 
+/**
+ * Represents an settings object for a TestSelector.
+ */
 interface TestSelectorSettings {
-  
+  /**
+   * The StrykerOptions.
+   */
   options: StrykerOptions;
 }
 
