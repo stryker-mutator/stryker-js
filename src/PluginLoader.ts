a65c9fc57525a7ef68568c80d7e6945a7142e76e
Nico (N.C.) Jansen <nico.jansen@ing.nl>
2016-04-15 01:33:53 +0200

    feat(read-config) Add PluginLoader class

---
+++ src/PluginLoader.ts
@@ -0,0 +1,55 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as log4js from 'log4js';
+import * as _ from 'lodash';
+import {importModule} from './utils/fileUtils'
+
+let log = log4js.getLogger('PluginLoader');
+
+var IGNORED_PACKAGES = ['stryker-cli'];
+
+export default class PluginLoader {
+
+  constructor(private plugins: string[]) { }
+
+  public load() {
+    this.getModules().forEach(this.requirePlugin);
+  }
+
+  private getModules() {
+    let modules: string[] = [];
+    this.plugins.forEach(function(plugin) {
+      if (_.isString(plugin)) {
+        if (plugin.indexOf('*') !== -1) {
+          var pluginDirectory = path.normalize(__dirname + '/../..')
+          var regexp = new RegExp('^' + plugin.replace('*', '.*'))
+
+          log.debug('Loading %s from %s', plugin, pluginDirectory)
+          fs.readdirSync(pluginDirectory)
+            .filter(pluginName => IGNORED_PACKAGES.indexOf(pluginName) === -1 && regexp.test(pluginName))
+            .map(pluginName => pluginDirectory + '/' + pluginName)
+            .forEach(moduleName => modules.push(moduleName));
+        } else {
+          modules.push(plugin);
+        }
+      } else {
+        log.warn('Ignoring plugin %s, as its not a string type', plugin)
+      }
+    });
+    return modules
+  }
+
+  private requirePlugin(name: string) {
+    log.debug(`Loading plugins ${name}`);
+    try {
+      importModule(name);
+    } catch (e) {
+      if (e.code === 'MODULE_NOT_FOUND' && e.message.indexOf(name) !== -1) {
+        log.warn('Cannot find plugin "%s".\n  Did you forget to install it ?\n' +
+          '  npm install %s --save-dev', name, name)
+      } else {
+        log.warn('Error during loading "%s" plugin:\n  %s', name, e.message)
+      }
+    }
+  }
+}
