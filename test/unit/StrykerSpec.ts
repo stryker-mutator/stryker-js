62edb44d540dfd29ee9edde5603c6532b87acde4
Nico Jansen <jansennico@gmail.com>
2016-04-15 08:51:11 +0200

    fix(stryker) Correct the promise resolving. (#88)
    * A rejection now actually results in a rejection
    * Stryker is resolved when everything went according to plan
      (but might contain survived mutations)

--- test/unit/StrykerSpec.ts
+++ test/unit/StrykerSpec.ts
@@ -20,6 +20,28 @@ describe('Stryker runMutationTest()', function() {
     sandbox.stub(console, 'log');
   });
 
+  function itShouldResultInARejection() {
+    it('should result in a rejection', (done) => {
+      sut.runMutationTest().then(() => {
+        done(new Error('Stryker resolved while a rejection was expected'));
+      }, () => {
+        done();
+      });
+    });
+  }
+
+  describe('when input file globbing results in a rejection', () => {
+    beforeEach(() => {
+      inputFileResolverStub = {
+        resolve: sandbox.stub().returns(Promise.reject(['a rejection']))
+      }
+      sandbox.stub(inputFileResolver, 'default').returns(inputFileResolverStub);
+      sut = new Stryker(null, null);
+    });
+
+    itShouldResultInARejection();
+  });
+
   describe('with correct input file globbing', () => {
     beforeEach(() => {
       inputFiles = [{ path: 'someFile', shouldMutate: true }];
@@ -30,26 +52,68 @@ describe('Stryker runMutationTest()', function() {
       sut = new Stryker(['mutateFile'], ['allFiles']);
     });
 
+    describe('when recordCoverage() resulted in a rejection', () => {
+
+      beforeEach(() => {
+        testRunnerOrchestratorStub = { recordCoverage: sandbox.stub().returns(Promise.reject('a rejection')) };
+        sandbox.stub(testRunnerOrchestrator, 'default').returns(testRunnerOrchestratorStub);
+      });
+      
+      itShouldResultInARejection();
+    });
+
     describe('when coverage can be collected', () => {
+      let runMutationsPromiseResolve: () => any;
+      let strykerPromise: Promise<void>;
 
       beforeEach(() => {
         initialRunResults = [];
         testRunnerOrchestratorStub = {
-          recordCoverage: sandbox.stub().returns(Promise.resolve(initialRunResults))
+          recordCoverage: sandbox.stub().returns(Promise.resolve(initialRunResults)),
+          runMutations: sandbox.stub().returns(new Promise(res => runMutationsPromiseResolve = res))
         };
         sandbox.stub(testRunnerOrchestrator, 'default').returns(testRunnerOrchestratorStub);
       });
 
       describe('but contains an error and failed tests', () => {
 
-        beforeEach(() => {
-          initialRunResults.push({result: TestResult.Error, errorMessages: ['An error!']})
-          return sut.runMutationTest();
+        beforeEach((done) => {
+          initialRunResults.push({ result: TestResult.Error, errorMessages: ['An error!'] })
+          strykerPromise = sut.runMutationTest();
+          strykerPromise.then(() => done(), () => done());
         });
-        
+
         it('should have logged the errors', () => {
           expect(console.log).to.have.been.calledWith('ERROR: One or more tests errored in the initial test run:');
           expect(console.log).to.have.been.calledWith('\t', 'An error!');
+        });
+
+        itShouldResultInARejection();
+      });
+
+      describe('without errors or failed tests', () => {
+
+        beforeEach(() => {
+          strykerPromise = sut.runMutationTest();
+        });
+
+        it('should not directly resolve the stryker promise', (done) => {
+          let resolved = false;
+          strykerPromise.then(() => resolved = true);
+          setTimeout(() => {
+            expect(resolved).to.be.eq(false);
+            done();
+          }, 100);
+        });
+
+        describe('and running of mutators was successfull', () => {
+          beforeEach(() => {
+            runMutationsPromiseResolve();
+          });
+
+          it('should resolve the stryker promise', () => {
+            return strykerPromise;
+          });
         });
       });
     });
