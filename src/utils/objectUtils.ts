8c8f2787ad4fedd29204a9bba7fe0e47cff8e221
Nico (N.C.) Jansen <nico.jansen@ing.nl>
2016-04-16 16:37:47 +0200

    feat(objectUtils) Add utility for recursive freezing

--- src/utils/objectUtils.ts
+++ src/utils/objectUtils.ts
@@ -1,9 +1,9 @@
 import * as _ from 'lodash';
 
-export function freezeRecursively(target: {[customConfig: string]: any}){
+export function freezeRecursively(target: { [customConfig: string]: any }) {
   Object.freeze(target);
   Object.keys(target).forEach(key => {
-    if(_.isObject(target[key])){
+    if (_.isObject(target[key])) {
       freezeRecursively(target[key]);
     }
   });
