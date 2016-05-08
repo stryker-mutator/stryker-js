dc5c055e044ae4d7b9ba6547b9b97a1e1f0f6a38
Simon de Lang <simondelang@gmail.com>
2016-05-08 12:00:53 +0200

    fix(RemoveConditionalsMutatorSpec) Change RemoveConditionalsMutatorSpec to accept child nodes being returned

--- test/unit/mutators/RemoveConditionalsMutatorSpec.ts
+++ test/unit/mutators/RemoveConditionalsMutatorSpec.ts
@@ -4,17 +4,17 @@ import * as parserUtils from '../../../src/utils/parserUtils';
 import * as chai from 'chai';
 let expect = chai.expect;
 
-describe('RemoveConditionalsMutator', function() {
+describe('RemoveConditionalsMutator', function () {
   let removeConditionalsMutator: RemoveConditionalsMutator;
   let doWhileLoop: ESTree.DoWhileStatement;
   let forLoop: ESTree.ForStatement;
   let whileLoop: ESTree.WhileStatement;
   let ifStatement: ESTree.IfStatement;
 
-  beforeEach(function() {
+  beforeEach(function () {
     removeConditionalsMutator = new RemoveConditionalsMutator();
     let code =
-    `var price = 99.95;
+      `var price = 99.95;
     if(price > 25){
       console.log("Too expensive");
     }
@@ -36,55 +36,55 @@ describe('RemoveConditionalsMutator', function() {
     forLoop = <ESTree.ForStatement>ast.body[4];
   });
 
-  function applyMutation(node: ESTree.IfStatement| ESTree.DoWhileStatement | ESTree.WhileStatement | ESTree.ForStatement){
+  function applyMutation(node: ESTree.IfStatement | ESTree.DoWhileStatement | ESTree.WhileStatement | ESTree.ForStatement) {
     return removeConditionalsMutator.applyMutations(node, _.cloneDeep);
   }
 
-  describe('should not generate an infinite loop', function() {
-    it('when given a do-while loop', function() {
+  describe('should not generate an infinite loop', function () {
+    it('when given a do-while loop', function () {
       let mutatedNodes = applyMutation(doWhileLoop);
-      let testValue = (<ESTree.Literal> (<ESTree.ConditionalExpression> mutatedNodes[0]).test).value;
+      let testValue = (<ESTree.Literal>mutatedNodes[0]).value;
 
       expect(testValue).to.be.false;
     });
 
-    it('when given a while loop', function() {
+    it('when given a while loop', function () {
       let mutatedNodes = applyMutation(whileLoop);
-      let testValue = (<ESTree.Literal> (<ESTree.ConditionalExpression> mutatedNodes[0]).test).value;
+      let testValue = (<ESTree.Literal>mutatedNodes[0]).value;
 
       expect(testValue).to.be.false;
     });
 
-    it('when given a for loop', function() {
+    it('when given a for loop', function () {
       let mutatedNodes = applyMutation(forLoop);
-      let testValue = (<ESTree.Literal> (<ESTree.ConditionalExpression> mutatedNodes[0]).test).value;
+      let testValue = (<ESTree.Literal>mutatedNodes[0]).value;
 
       expect(testValue).to.be.false;
     });
   });
 
-  describe('should generate a single mutant', function() {
-    it('when given a do-while loop', function() {
+  describe('should generate a single mutant', function () {
+    it('when given a do-while loop', function () {
       let mutatedNodes = applyMutation(doWhileLoop);
 
       expect(mutatedNodes).to.have.lengthOf(1);
     });
 
-    it('when given a while loop', function() {
+    it('when given a while loop', function () {
       let mutatedNodes = applyMutation(whileLoop);
 
       expect(mutatedNodes).to.have.lengthOf(1);
     });
 
-    it('when given a for loop', function() {
+    it('when given a for loop', function () {
       let mutatedNodes = applyMutation(forLoop);
 
       expect(mutatedNodes).to.have.lengthOf(1);
     });
   });
 
-  describe('should generate multiple mutants', function() {
-    it('when given an if-statement', function() {
+  describe('should generate multiple mutants', function () {
+    it('when given an if-statement', function () {
       let mutatedNodes = applyMutation(ifStatement);
 
       expect(mutatedNodes).to.have.length.above(1);
