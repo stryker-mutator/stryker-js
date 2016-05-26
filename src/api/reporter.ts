bb0833808118a81d2e41ec526f802b0f16c518dd
nicojs <jansennico@gmail.com>
2016-05-26 08:41:51 +0200

    feat(reporter-api) Rename 'reporter' to 'report'

--- src/api/reporter.ts
+++
@@ -1,6 +0,0 @@
-
-export {default as Reporter} from './reporter/Reporter';
-export {default as MutantResult} from './reporter/MutantResult';
-export {default as MutantStatus} from './reporter/MutantStatus';
-export {default as ReporterFactory} from './reporter/ReporterFactory';
-export {default as SourceFile} from './reporter/SourceFile';
