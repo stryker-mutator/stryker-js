318441c23a6faadf603adbf1c95b5512e067edf1
Simon de Lang <simondelang@gmail.com>
2016-06-16 10:27:42 +0200

    Manual testFramework and testRunner configuration (#108)
    * Refactor(Config) Remove default testFramework and testRunner
    
    * Documentation(Stryker, README) Add documentation for the testFramework and testRunner options

--- testResources/module/stryker.conf.js
+++ testResources/module/stryker.conf.js
@@ -2,6 +2,8 @@ module.exports = function(config){
   config.set({
     files: ['../sampleProject/src/?(Circle|Add).js', '../sampleProject/test/?(AddSpec|CircleSpec).js'],
     mutate: ['../sampleProject/src/?(Circle|Add).js'],
-    logLevel: 'debug'
+    logLevel: 'debug',
+    testFramework: 'jasmine',
+    testRunner: 'karma'
   });
 }
