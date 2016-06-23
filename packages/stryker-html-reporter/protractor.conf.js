2b1839f85f6af60615408b0a39cf79360bd323c9
nicojs <jansennico@gmail.com>
2016-06-23 08:09:42 +0200

    test(protractor) Add protractor.conf.js for travis

---
+++ packages/stryker-html-reporter/protractor.conf.js
@@ -0,0 +1,21 @@
+exports.config = {
+  directConnect: true,
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  framework: 'mocha',
+
+  specs: ['test/helpers.js', 'test/ui/**/*.js'],
+
+  mochaOpts: {
+    timeout: 30000
+  }
+};
+
+if (process.env.TRAVIS) {
+  config.capabilities = {
+    browserName: 'firefox'
+  };
+}
