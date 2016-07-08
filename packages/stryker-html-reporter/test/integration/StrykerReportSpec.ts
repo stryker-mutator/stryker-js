4ad91bd6ad0758e6da57ed47c2396e1ad136f995
nicojs <jansennico@gmail.com>
2016-07-08 21:53:11 +0200

    fix(stryker-test): Test on unix
    * Make sure we normalize the file paths before sending events

--- packages/stryker-html-reporter/test/integration/StrykerReportSpec.ts
+++ packages/stryker-html-reporter/test/integration/StrykerReportSpec.ts
@@ -2,6 +2,7 @@ import HtmlReporter from '../../src/HtmlReporter';
 import * as fs from 'fs';
 import * as path from 'path';
 import {expect} from 'chai';
+import * as _ from 'lodash';
 
 const EVENTS_DIR = 'testResources/strykerEvents';
 const REPORT_DIR = 'reports/stryker';
@@ -9,7 +10,7 @@ const REPORT_DIR = 'reports/stryker';
 describe('Html report of stryker', () => {
   let sut: any;
 
-  let replayAll = (eventPromisses: Promise<{ name: string, content: string }>[]) => {
+  let replayAll = (eventPromisses: Promise<{ name: string, content: any }>[]) => {
     return Promise.all(eventPromisses).then(events => {
       events.forEach(event => {
         if (sut[event.name]) {
@@ -30,8 +31,8 @@ describe('Html report of stryker', () => {
 
       beforeEach(() => sut.wrapUp());
 
-      it('should work', () => {
-        expect(readDirectoryTree()).to.be.deep.eq({"name":"stryker","directories":[{"name":"bootstrap","directories":[{"name":"css","directories":[],"files":["bootstrap-theme.css","bootstrap-theme.min.css","bootstrap.css","bootstrap.min.css"]},{"name":"js","directories":[],"files":["bootstrap.js","bootstrap.min.js","npm.js"]}],"files":[]},{"name":"highlightjs","directories":[{"name":"styles","directories":[],"files":["default.css"]}],"files":[]},{"name":"isolated-runner","directories":[],"files":["index.html","IsolatedTestRunnerAdapter.js.html","IsolatedTestRunnerAdapterFactory.js.html","IsolatedTestRunnerAdapterWorker.js.html","Message.js.html","ResultMessageBody.js.html","RunMessageBody.js.html","StartMessageBody.js.html"]},{"name":"jasmine_test_selector","directories":[],"files":["index.html","JasmineTestSelector.js.html"]},{"name":"mutators","directories":[],"files":["BlockStatementMutator.js.html","ConditionalBoundaryMutator.js.html","index.html","MathMutator.js.html","OperatorMutator.js.html","OperatorMutatorMap.js.html","RemoveConditionalsMutator.js.html","ReverseConditionalMutator.js.html","UnaryOperatorMutator.js.html"]},{"name":"reporters","directories":[],"files":["BroadcastReporter.js.html","ClearTextReporter.js.html","EventReporter.js.html","index.html","ProgressReporter.js.html"]},{"name":"utils","directories":[],"files":["fileUtils.js.html","index.html","objectUtils.js.html","parserUtils.js.html","StrykerTempFolder.js.html"]}],"files":["ConfigReader.js.html","index.html","InputFileResolver.js.html","Mutant.js.html","MutantRunResultMatcher.js.html","MutatorOrchestrator.js.html","PluginLoader.js.html","ReporterOrchestrator.js.html","stryker-80x80.png","stryker.css","stryker.js","Stryker.js.html","TestRunnerOrchestrator.js.html","TestSelectorOrchestrator.js.html"]});
+      it('should build all files in the report', () => {
+        expect(readDirectoryTree()).to.be.deep.eq({ "name": "stryker", "directories": [{ "name": "bootstrap", "directories": [{ "name": "css", "directories": [], "files": ["bootstrap-theme.css", "bootstrap-theme.min.css", "bootstrap.css", "bootstrap.min.css"] }, { "name": "js", "directories": [], "files": ["bootstrap.js", "bootstrap.min.js", "npm.js"] }], "files": [] }, { "name": "highlightjs", "directories": [{ "name": "styles", "directories": [], "files": ["default.css"] }], "files": [] }, { "name": "isolated-runner", "directories": [], "files": ["index.html", "IsolatedTestRunnerAdapter.js.html", "IsolatedTestRunnerAdapterFactory.js.html", "IsolatedTestRunnerAdapterWorker.js.html", "Message.js.html", "ResultMessageBody.js.html", "RunMessageBody.js.html", "StartMessageBody.js.html"] }, { "name": "jasmine_test_selector", "directories": [], "files": ["index.html", "JasmineTestSelector.js.html"] }, { "name": "mutators", "directories": [], "files": ["BlockStatementMutator.js.html", "ConditionalBoundaryMutator.js.html", "index.html", "MathMutator.js.html", "OperatorMutator.js.html", "OperatorMutatorMap.js.html", "RemoveConditionalsMutator.js.html", "ReverseConditionalMutator.js.html", "UnaryOperatorMutator.js.html"] }, { "name": "reporters", "directories": [], "files": ["BroadcastReporter.js.html", "ClearTextReporter.js.html", "EventReporter.js.html", "index.html", "ProgressReporter.js.html"] }, { "name": "utils", "directories": [], "files": ["fileUtils.js.html", "index.html", "objectUtils.js.html", "parserUtils.js.html", "StrykerTempFolder.js.html"] }], "files": ["ConfigReader.js.html", "index.html", "InputFileResolver.js.html", "Mutant.js.html", "MutantRunResultMatcher.js.html", "MutatorOrchestrator.js.html", "PluginLoader.js.html", "ReporterOrchestrator.js.html", "stryker-80x80.png", "stryker.css", "stryker.js", "Stryker.js.html", "TestRunnerOrchestrator.js.html", "TestSelectorOrchestrator.js.html"] });
       });
     });
   });
@@ -80,11 +81,31 @@ let retrieveEventName = (filename: string) => {
   return filename.substring(filename.indexOf('-') + 1, filename.indexOf('.'));
 };
 
+let normalizePath = (event: any) => {
+  if (event.path) {
+    event.path = path.normalize(event.path);
+  }
+  if (event.sourceFilePath) {
+    event.sourceFilePath = path.normalize(event.sourceFilePath);
+  }
+  return event;
+};
+
+let eventContent = (fileContent: any) => {
+  let event = JSON.parse(fileContent);
+  if (_.isArray(event)) {
+    event.forEach((item: any) => normalizePath(item));
+  } else {
+    normalizePath(event);
+  }
+  return event;
+}
+
 let readAllEvents = () => {
   let files = fs.readdirSync(EVENTS_DIR).sort();
   let allEvents: Promise<{ name: string, content: string }>[] = [];
   files.forEach(filename => {
-    allEvents.push(readFile(path.join(EVENTS_DIR, filename)).then(fileContent => ({ name: retrieveEventName(filename), content: JSON.parse(fileContent) })));
+    allEvents.push(readFile(path.join(EVENTS_DIR, filename)).then(fileContent => ({ name: retrieveEventName(filename), content: eventContent(fileContent) })));
   });
   return allEvents;
 }
