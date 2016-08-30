90b117c521bf60571224b69d1c76bacf47acb626
Nico Jansen <jansennico@gmail.com>
2016-08-30 08:51:06 +0200

    feat(testFramework): Use test framework (#3)
    * Changed how the test runner for karma is defined. Order is: (1) use custom `karmaConfig` settings, (2) use stryker `testFramework` setting,  (3) use `'jasmine'`
    * Add unit tests for it.

---
+++ packages/stryker-karma-runner/test/helpers/chaiSetup.ts
@@ -0,0 +1,3 @@
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+chai.use(sinonChai);
---
+++ packages/stryker-karma-runner/test/unit/KarmaTestRunnerSpec.ts
@@ -0,0 +1,68 @@
+import { expect } from 'chai';
+import KarmaTestRunner from '../../src/KarmaTestRunner';
+import { TestRunner, TestResult, RunResult, RunnerOptions, CoverageCollection } from 'stryker-api/test_runner';
+import * as karma from 'karma';
+import * as sinon from 'sinon';
+
+describe('KarmaTestRunner', () => {
+
+  let options: RunnerOptions;
+  let sut: KarmaTestRunner;
+  let sandbox: sinon.SinonSandbox;
+
+  beforeEach(() => {
+    options = {
+      files: [],
+      port: 0,
+      strykerOptions: {}
+    };
+    sandbox = sinon.sandbox.create();
+    sandbox.stub(karma, 'Server').returns({ on: sandbox.stub(), start: sandbox.stub() });
+  });
+
+  describe('when no testFramework is supplied', () => {
+    beforeEach(() => sut = new KarmaTestRunner(options));
+
+    it('should use "jasmine" as a default', () => {
+        expect(karma.Server).to.have.been.calledWithNew;
+        expect(karma.Server).to.have.been.calledWithMatch(sinon.match({ 'frameworks': ['jasmine'] }), sinon.match.func);
+    });
+
+  });
+
+  describe('when testFramework is supplied', () => {
+
+    beforeEach(() => {
+      options.strykerOptions.testFramework = 'some framework';
+    });
+
+    describe('and karmaConfig overrides the frameworks', () => {
+
+      beforeEach(() => {
+        options.strykerOptions['karmaConfig'] = { frameworks: ['karma config override', 'second framework'] };
+        sut = new KarmaTestRunner(options);
+      });
+
+      it('should use the testFramework specified in karma config', () => {
+        expect(karma.Server).to.have.been.calledWithNew;
+        expect(karma.Server).to.have.been.calledWithMatch(sinon.match({ 'frameworks': ['karma config override', 'second framework'] }), sinon.match.func);
+      });
+
+    });
+
+    describe('and no karmaConfig overrides the framework', () => {
+
+      beforeEach(() => {
+        sut = new KarmaTestRunner(options);
+      });
+
+      it('should use the testFramework', () => {
+        expect(karma.Server).to.have.been.calledWithNew;
+        expect(karma.Server).to.have.been.calledWithMatch(sinon.match({ 'frameworks': ['some framework'] }), sinon.match.func);
+      });
+
+    });
+  });
+
+  afterEach(() => sandbox.restore());
+});
