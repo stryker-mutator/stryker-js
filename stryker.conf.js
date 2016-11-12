fb80531a24e2ccbcf420f4b57fb3f7d3cd0da734
Nico Jansen <jansennico@gmail.com>
2016-11-12 10:53:49 +0100

    chore(stryker.conf.js): Update stryker.conf.js

--- stryker.conf.js
+++ stryker.conf.js
@@ -3,8 +3,9 @@ module.exports = function (config) {
     files: ['test/helpers/**/*.js', 'test/unit/**/*.js', { pattern: 'src/**/*.js', included: false, mutated: true }],
     testFramework: 'mocha',
     testRunner: 'mocha',
-    reporter: ['progress', 'clear-text', 'html', 'event-recorder'],
-    testFramework: null,
-    plugins: ['stryker-mocha-runner', 'stryker-html-reporter']
+    reporter: ['progress', 'clear-text'],
+    coverageAnalysis: 'off',
+    logLevel: 'info',
+    plugins: ['stryker-mocha-runner']
   });
 };
