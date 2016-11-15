6716519ac015a0d4cd9509b65d213b14a04a4496
Nico Jansen <jansennico@gmail.com>
2016-11-15 18:08:40 +0100

    feat(one-pass-coverage): Update test runner (#4)
    * feat(api-0.3): Update test runner api
    
    * Update test runner api to v0.3.x
    * Added a simple Timer class to time the tests
    
    * fix(timer): Make timer mock independent
    
    * refactor(timer): Add sinon to cleanup a test
    
    * feat(test-framework): Add mocha test framework
    
    * Implement the filtering using monkey patching / duck punching
    * Implement the beforeEach and afterEach as a mocha beforeEach and afterEach hook.
    
    * fix(deps): Update dependency versions
    
    * refactor(test_runner): Rename state -> status
    
    * refactor(test_runner): Rename errorMessage
    
    * Rename `errorMessages` of `TestResult` to `failureMessages`
    
    * refactor(mocha-reporter): Cleanup debug code
    
    * feat(filtering): Implement filtering of tests
    
    * Previous implementation of filtering by overriding the global `it` didn't work, as every describe gets executed in its own context and has it's own `it`. Now overriding the `addTest` method on the suite.
    
    * feat(bail): Use mocha bail functionality
    
    * Bail makes you abandon a test run as soon as the first failed test appears. This is a good idea for mutation testing.

--- packages/stryker-mocha-runner/src/MochaTestRunner.ts
+++ packages/stryker-mocha-runner/src/MochaTestRunner.ts
@@ -1,16 +1,20 @@
-import {TestRunner, TestResult, RunResult, RunnerOptions, CoverageCollection} from 'stryker-api/test_runner';
-import {InputFile} from 'stryker-api/core';
 import * as path from 'path';
 import * as log4js from 'log4js';
+import { EventEmitter } from 'events';
+import { TestRunner, TestResult, RunResult, TestStatus, RunStatus, RunnerOptions, CoverageCollection } from 'stryker-api/test_runner';
+import { InputFile } from 'stryker-api/core';
+
+
 // import * as Mocha from 'mocha';
 const Mocha = require('mocha');
 import StrykerMochaReporter from './StrykerMochaReporter';
 
 const log = log4js.getLogger('MochaTestRunner');
-export default class MochaTestRunner implements TestRunner {
+export default class MochaTestRunner extends EventEmitter implements TestRunner {
   private files: InputFile[];
 
   constructor(runnerOptions: RunnerOptions) {
+    super();
     this.files = runnerOptions.files;
   }
 
@@ -22,7 +26,7 @@ export default class MochaTestRunner implements TestRunner {
     return new Promise<RunResult>((resolve, fail) => {
       try {
         this.purgeFiles();
-        let mocha = new Mocha({ reporter: StrykerMochaReporter });
+        let mocha = new Mocha({ reporter: StrykerMochaReporter, bail: true });
         this.files.filter(file => file.included).forEach(f => mocha.addFile(f.path));
         try {
           let runner: any = mocha.run((failures: number) => {
@@ -31,7 +35,8 @@ export default class MochaTestRunner implements TestRunner {
           });
         } catch (error) {
           resolve({
-            result: TestResult.Error,
+            status: RunStatus.Error,
+            tests: [],
             errorMessages: [error]
           });
         }
--- packages/stryker-mocha-runner/src/StrykerMochaReporter.ts
+++ packages/stryker-mocha-runner/src/StrykerMochaReporter.ts
@@ -1,53 +1,57 @@
-import {RunResult, TestResult} from 'stryker-api/test_runner';
+import { RunResult, TestResult, RunStatus, TestStatus } from 'stryker-api/test_runner';
 import * as log4js from 'log4js';
+import Timer from './Timer';
 
 const log = log4js.getLogger('StrykerMochaReporter');
 
 export default class StrykerMochaReporter {
-  private runResult: RunResult = {
-    result: TestResult.Error,
-    testNames: <string[]>[],
-    succeeded: 0,
-    failed: 0,
-    timeSpent: 0,
-    errorMessages: <string[]>[]
-  };
-  private startDate: Date;
 
-  constructor(runner: any) {
-    this.registerEvents(runner);
+  private runResult: RunResult;
+  private timer = new Timer();
+  private passedCount = 0;
+
+  constructor(private runner: NodeJS.EventEmitter) {
+    this.registerEvents();
   }
 
-  private registerEvents(runner: any) {
-    runner.on('start', () => {
-      this.startDate = new Date();
-
+  private registerEvents() {
+    this.runner.on('start', () => {
+      this.passedCount = 0;
+      this.timer.reset();
+      this.runResult = {
+        status: RunStatus.Error,
+        tests: [],
+        errorMessages: []
+      };
       log.debug('Starting Mocha test run');
     });
 
-    runner.on('pass', (test: any) => {
-      this.runResult.succeeded++;
-      this.runResult.testNames.push(test.fullTitle());
-
+    this.runner.on('pass', (test: any) => {
+      this.runResult.tests.push({
+        status: TestStatus.Success,
+        name: test.fullTitle(),
+        timeSpentMs: this.timer.elapsedMs()
+      });
+      this.passedCount++;
+      this.timer.reset();
       log.debug(`Test passed: ${test.fullTitle()}`);
     });
 
-    runner.on('fail', (test: any, err: any) => {
-      this.runResult.failed++;
-      this.runResult.testNames.push(test.fullTitle());
+    this.runner.on('fail', (test: any, err: any) => {
+      this.runResult.tests.push({
+        status: TestStatus.Failed,
+        name: test.fullTitle(),
+        timeSpentMs: this.timer.elapsedMs(),
+        failureMessages: [err.message]
+      });
       this.runResult.errorMessages.push(err.message);
-      console.log(err.stack);
-
       log.debug(`Test failed: ${test.fullTitle()}. Error: ${err.message}`);
     });
 
-    runner.on('end', () => {
-      this.runResult.result = TestResult.Complete;
-      this.runResult.timeSpent = new Date().getTime() - this.startDate.getTime();
-
-      runner.runResult = this.runResult;
-
-      log.debug(`Mocha test run completed: ${this.runResult.succeeded}/${this.runResult.succeeded + this.runResult.failed} passed`);
+    this.runner.on('end', () => {
+      this.runResult.status = RunStatus.Complete;
+      (this.runner as any).runResult = this.runResult;
+      log.debug(`Mocha test run completed: ${this.passedCount}/${this.runResult.tests.length} passed`);
     });
   }
 }
