6f38885e98690e64b5cf7db8afe3b0fbe10c96e1
Nico Jansen <jansennico@gmail.com>
2016-12-15 17:54:26 +0100

    feat(es2015-promise): Remove dep to es6-promise (#5)

--- packages/stryker-mocha-runner/Gruntfile.js
+++ packages/stryker-mocha-runner/Gruntfile.js
@@ -85,7 +85,9 @@ module.exports = function (grunt) {
         failOnTypeErrors: true
       },
       build: {
-        tsconfig: true,
+        tsconfig: {
+          passThrough: true
+        }
       },
     },
     
