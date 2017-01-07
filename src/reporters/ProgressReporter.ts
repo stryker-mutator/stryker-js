7b6850657ab8ecc32fd7432e302d9bcc468cc571
Nico Jansen <jansennico@gmail.com>
2017-01-07 07:57:12 +0100

    feat(append-only-progress): Implement new reporter (#213)
    * feat(append-only-progress): Implement new reporter
    
    * Create a new progress reporter, one which only appends. It prints progress every 10 seconds.
    * When the "progress" reporter is not used when the console does not support it.
    
    * refactor(progress-reporter-spec): Remove duplicate code

--- src/reporters/ProgressReporter.ts
+++ src/reporters/ProgressReporter.ts
@@ -1,28 +1,14 @@
-import { Reporter, MatchedMutant, MutantResult, MutantStatus } from 'stryker-api/report';
+import { MatchedMutant, MutantResult, MutantStatus } from 'stryker-api/report';
+import ProgressKeeper from './ProgressKeeper';
 import ProgressBar from './ProgressBar';
 import * as chalk from 'chalk';
 
-export default class ProgressReporter implements Reporter {
+export default class ProgressBarReporter extends ProgressKeeper {
   private progressBar: ProgressBar;
 
-  // tickValues contains Labels, because on initation of the ProgressBar the width is determined based on the amount of characters of the progressBarContent inclusive ASCII-codes for colors
-  private tickValues = {
-    error: 0,
-    survived: 0,
-    killed: 0,
-    timeout: 0,
-    noCoverage: 0,
-    killedLabel: chalk.green.bold('killed'),
-    survivedLabel: chalk.red.bold('survived'),
-    noCoverageLabel: chalk.red.bold('no coverage'),
-    timeoutLabel: chalk.yellow.bold('timeout'),
-    errorLabel: chalk.yellow.bold('error')
-  };
-
   onAllMutantsMatchedWithTests(matchedMutants: ReadonlyArray<MatchedMutant>): void {
-    let progressBarContent: string;
-
-    progressBarContent =
+    super.onAllMutantsMatchedWithTests(matchedMutants);
+    const progressBarContent =
       `Mutation testing  [:bar] :percent (ETC :etas)` +
       `[:killed :killedLabel] ` +
       `[:survived :survivedLabel] ` +
@@ -34,36 +20,26 @@ export default class ProgressReporter implements Reporter {
       width: 50,
       complete: '=',
       incomplete: ' ',
-      total: matchedMutants.filter(m => m.scopedTestIds.length > 0).length
+      stream: process.stdout,
+      total: this.progress.totalCount
     });
   }
 
   onMutantTested(result: MutantResult): void {
-    switch (result.status) {
-      case MutantStatus.NoCoverage:
-        this.tickValues.noCoverage++;
-        this.progressBar.render(this.tickValues);
-        break;
-      case MutantStatus.Killed:
-        this.tickValues.killed++;
-        this.tick();
-        break;
-      case MutantStatus.Survived:
-        this.tickValues.survived++;
-        this.tick();
-        break;
-      case MutantStatus.TimedOut:
-        this.tickValues.timeout++;
-        this.tick();
-        break;
-      case MutantStatus.Error:
-        this.tickValues.error++;
-        this.tick();
-        break;
+    const ticksBefore = this.progress.testedCount;
+    super.onMutantTested(result);
+    if (ticksBefore < this.progress.testedCount) {
+      this.tick();
+    } else {
+      this.render();
     }
   }
 
-  tick(): void {
-    this.progressBar.tick(this.tickValues);
+  private tick(): void {
+    this.progressBar.tick(this.progress);
+  }
+
+  private render(): void {
+    this.progressBar.render(this.progress);
   }
 }
