f92ea1a9ef92f6bee00efb7b7fd0c37f41c1729d
Stijn Meijerink <stijn.meijerink@gmail.com>
2017-01-28 17:31:59 +0100

    feat(BroadcastReporter): Add StrictReporter
    Add a StrictReporter interface and implement it on BroadcastReporter.

--- src/reporters/BroadcastReporter.ts
+++ src/reporters/BroadcastReporter.ts
@@ -1,8 +1,10 @@
 
-import { Reporter, SourceFile, MutantResult } from 'stryker-api/report';
+import { Reporter, SourceFile, MutantResult, MatchedMutant } from 'stryker-api/report';
 import { StrykerOptions } from 'stryker-api/core';
 import * as log4js from 'log4js';
 import { isPromise } from '../utils/objectUtils';
+import StrictReporter from './StrictReporter';
+import ConfigReader from '../ConfigReader';
 
 const log = log4js.getLogger('BroadcastReporter');
 
@@ -11,25 +13,21 @@ export interface NamedReporter {
   reporter: Reporter;
 }
 
-export const ALL_EVENT_METHOD_NAMES = ['onSourceFileRead', 'onAllSourceFilesRead', 'onAllMutantsMatchedWithTests', 'onMutantTested', 'onAllMutantsTested', 'onConfigRead'];
+export const ALL_EVENT_METHOD_NAMES = ['onSourceFileRead', 'onAllSourceFilesRead', 'onAllMutantsMatchedWithTests', 'onMutantTested', 'onAllMutantsTested'];
 
-export default class BroadcastReporter implements Reporter {
+export default class BroadcastReporter implements StrictReporter {
 
   constructor(private reporters: NamedReporter[]) {
-    ALL_EVENT_METHOD_NAMES.concat('wrapUp').forEach(method => {
-      (<any>this)[method] = (arg: any) => {
-        return this.broadcast(method, arg);
-      };
-    });
+    //Empty constructor
   }
 
-  private broadcast(methodName: string, eventArgs: any) {
+  private broadcast(methodName: string, eventArgs?: any) {
     let allPromises: Promise<any>[] = [];
     this.reporters.forEach(namedReporter => {
       let reporter = <any>namedReporter.reporter;
       if (reporter[methodName] && typeof reporter[methodName] === 'function') {
         try {
-          let maybePromise: void | Promise<any> = reporter[methodName](eventArgs);
+          let maybePromise: void | Promise<any> = eventArgs ? reporter[methodName](eventArgs) : reporter[methodName]();
           if (isPromise(maybePromise)) {
             allPromises.push(maybePromise.catch(error => {
               this.handleError(error, methodName, namedReporter.name);
@@ -42,6 +40,33 @@ export default class BroadcastReporter implements Reporter {
     });
     if (allPromises.length) {
       return Promise.all(allPromises);
+    }
+  }
+
+  onSourceFileRead(file: SourceFile): void {
+    this.broadcast('onSourceFileRead', file);
+  }
+
+  onAllSourceFilesRead(files: SourceFile[]): void {
+    this.broadcast('onAllSourceFilesRead', files);
+  }
+
+  onAllMutantsMatchedWithTests(results: ReadonlyArray<MatchedMutant>): void {
+    this.broadcast('onAllMutantsMatchedWithTests', results);
+  }
+
+  onMutantTested(result: MutantResult): void {
+    this.broadcast('onMutantTested', result);
+  }
+
+  onAllMutantsTested(results: MutantResult[]): void {
+    this.broadcast('onAllMutantsTested', results);
+  }
+
+  wrapUp(): void | Promise<void> {
+    let maybePromise: void | Promise<any> = this.broadcast('wrapUp', 'wrapUp');
+    if (isPromise(maybePromise)) {
+      return Promise.resolve(maybePromise);
     }
   }
 
---
+++ src/reporters/StrictReporter.ts
@@ -0,0 +1,20 @@
+import {Reporter, SourceFile, MutantResult, MatchedMutant} from 'stryker-api/report';
+
+/**
+ * Represents a reporter which can report during or after a Stryker run
+ */
+interface StrictReporter extends Reporter{
+
+    onSourceFileRead(file: SourceFile): void;
+
+    onAllSourceFilesRead(files: SourceFile[]): void;
+
+    onAllMutantsMatchedWithTests(results: ReadonlyArray<MatchedMutant>): void;
+
+    onMutantTested(result: MutantResult): void;
+
+    onAllMutantsTested(results: MutantResult[]): void;
+
+    wrapUp(): void | Promise<void>;
+}
+export default StrictReporter;
