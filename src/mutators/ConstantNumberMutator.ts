e183f2840188d4e93bf733e8a7afc2fb6da333c3
Simon de Lang <simondelang@gmail.com>
2017-02-06 22:12:04 +0100

    fix(ContantNumberMutator): Check if init exists

--- src/mutators/ConstantNumberMutator.ts
+++ src/mutators/ConstantNumberMutator.ts
@@ -13,7 +13,7 @@ export default class ConstantNumberMutator implements Mutator {
     applyMutations(node: estree.Node, copy: <T>(obj: T, deep?: boolean) => T): void | estree.Node | estree.Node[] {
         if (node.type === Syntax.VariableDeclaration && node.kind === 'const' && node.declarations.length === 1) {
             let declaration = node.declarations[0];
-            if (declaration.init.type === Syntax.Literal) {
+            if (declaration.init && declaration.init.type === Syntax.Literal) {
                 let mutatedNode: estree.SimpleLiteral | estree.RegExpLiteral = copy(declaration.init);
                 if (typeof(mutatedNode.value) === 'number') {
                     mutatedNode.value = mutatedNode.value === 0 ? 1 : 0;
