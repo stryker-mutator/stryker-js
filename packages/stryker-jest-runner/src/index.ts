b8559e2548fe73e5396018b09eb3afc8d788de9a
Maarten Mulders <m.th.mulders@xs4all.nl>
2017-02-12 20:30:34 +0100

    fix(plugin) Use correct name for registering plugin

--- packages/stryker-jest-runner/src/index.ts
+++ packages/stryker-jest-runner/src/index.ts
@@ -4,4 +4,4 @@ import JestTestRunner from './JestTestRunner';
 // This is the main file loaded when stryker loads this plugin
 // Report your plugin to the correct Factory
 
-TestRunnerFactory.instance().register('jest-test-runner', JestTestRunner);
+TestRunnerFactory.instance().register('jest', JestTestRunner);
