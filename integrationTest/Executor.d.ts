53a70e68e6f82b0d872f3e6690db9839b621c618
Nico Jansen <jansennico@gmail.com>
2017-04-07 22:21:25 +0200

    refactor(integrationTest): Add ts compilation
    * Add ts compilation
    * Remove redundant files

--- integrationTest/Executor.d.ts
+++
@@ -1,7 +0,0 @@
-export default class Executor {
-    private silent;
-    private cwd;
-    constructor(cwd: string, silent?: boolean);
-    exec(command: string, options: any, done: (error: any, stdout: string) => any): void;
-    private handleProcess(child, done);
-}
