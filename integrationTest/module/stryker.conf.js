210f9677000c343cda413ab7c16c31b97600cecc
Nico Jansen <jansennico@gmail.com>
2017-04-10 07:36:04 +0200

    refactor(sample): Move sample outside of integration test

--- integrationTest/module/stryker.conf.js
+++ integrationTest/module/stryker.conf.js
@@ -1,7 +1,7 @@
 module.exports = function(config){
   config.set({
-    files: ['../sampleProject/src/?(Circle|Add).js', '../sampleProject/test/?(AddSpec|CircleSpec).js'],
-    mutate: ['../sampleProject/src/?(Circle|Add).js'],
+    files: ['../../sampleProject/src/?(Circle|Add).js', '../../sampleProject/test/?(AddSpec|CircleSpec).js'],
+    mutate: ['../../sampleProject/src/?(Circle|Add).js'],
     logLevel: 'trace',
     testFramework: 'jasmine',
     testRunner: 'karma'
