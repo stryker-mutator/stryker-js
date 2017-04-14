7b3d299b6d13be321501681ab9ef52d5a037e1b0
Nico Jansen <jansennico@gmail.com>
2017-04-14 18:45:32 +0200

    Remove unused task

--- tasks/link.js
+++
@@ -1,5 +0,0 @@
-const ParentBinLinker = require('link-parent-bin').ParentBinLinker;
-const linker = new ParentBinLinker({ childDirectoryRoot: 'packages', linkDevDependencies: true, linkDependencies: false });
-linker.linkBinsToChildren()
-    .then(() => console.log('done'))
-    .catch(err => console.error('Error Linking packages', err));
