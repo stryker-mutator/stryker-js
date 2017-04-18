b5b215aa9634f200ddef44ece69a6ecb23bec06c
Alex van Assem <avassem@gmail.com>
2017-04-18 22:57:20 +0200

    Fix stryker.conf.js to origal version

--- packages/stryker/stryker.conf.js
+++ packages/stryker/stryker.conf.js
@@ -1,22 +1,11 @@
-
-  module.exports = function(config){
-    config.set(
-      {
-  "files": [
-    {
-      "pattern": "src/**/*.js",
-      "mutated": true,
-      "included": false
-    },
-    "test/**/*.js"
-  ],
-  "coverageAnalysis": "perTest",
-  "reporter": [
-    "html",
-    "progress"
-  ],
-  "testFramework": "mocha",
-  "testRunner": "mocha"
-}
-    );
-  }
+module.exports = function (config) {
+  config.set({
+    files: ['test/helpers/**/*.js', 'test/unit/**/*.js', { pattern: 'src/**/*.js', included: false, mutated: true }],
+    testFramework: 'mocha',
+    testRunner: 'mocha',
+    reporter: ['progress', 'clear-text'],
+    coverageAnalysis: 'perTest',
+    logLevel: 'info',
+    plugins: ['stryker-mocha-runner']
+  });
+};
