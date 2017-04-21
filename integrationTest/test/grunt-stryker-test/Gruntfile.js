e51a1f4a612394fb1425816038b3c96de13ca8a3
Nico Jansen <jansennico@gmail.com>
2017-04-21 14:35:06 +0200

    Cleaner grunt stryker and integration tests (#283)

---
+++ integrationTest/test/grunt-stryker-test/Gruntfile.js
@@ -0,0 +1,38 @@
+console.log('gruntsworking');
+module.exports = function (grunt) {
+  grunt.initConfig({
+    stryker: {
+      withoutConfigFile: {
+        files: {
+          src: [
+            'sampleProject/src/**/*.js',
+            '!sampleProject/src/InfiniteAdd.js'
+          ],
+          tests: [
+            'sampleProject/test/**/*.js',
+            '!sampleProject/test/FailingAddSpec.js'
+          ]
+        },
+        mutate: {
+          src: [
+            'sampleProject/src/**/*.js',
+            '!sampleProject/src/InfiniteAdd.js'
+          ]
+        },
+        options: {
+          testFramework: 'jasmine',
+          testRunner: 'karma',
+          logLevel: 'debug'
+        },
+      },
+      withConfigFile: {
+        options: {
+          configFile: './stryker.conf.js'
+        }
+      }
+    }
+  });
+
+  require('grunt-stryker/tasks/stryker')(grunt);
+  grunt.registerTask('test', ['stryker']);
+};
---
+++ integrationTest/test/module/stryker.conf.js
@@ -0,0 +1,9 @@
+module.exports = function(config){
+  config.set({
+    files: ['../../../sampleProject/src/?(Circle|Add).js', '../../../sampleProject/test/?(AddSpec|CircleSpec).js'],
+    mutate: ['../../../sampleProject/src/?(Circle|Add).js'],
+    logLevel: 'trace',
+    testFramework: 'jasmine',
+    testRunner: 'karma'
+  });
+};
