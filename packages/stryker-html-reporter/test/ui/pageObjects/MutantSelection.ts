0ec1be24bcb182886713807885795841697cdc3c
Nico Jansen <jansennico@gmail.com>
2017-04-25 07:28:27 +0200

    test(MutantSelection): Fix page object implementation
    The `test-decoration` property now includes the line type and color. Filter those values when testing.

--- packages/stryker-html-reporter/test/ui/pageObjects/MutantSelection.ts
+++ packages/stryker-html-reporter/test/ui/pageObjects/MutantSelection.ts
@@ -1,11 +1,13 @@
+import { ElementFinder } from 'protractor';
 
 export default class MutantSelection {
-  constructor(private button: protractor.ElementFinder, private originalCodeSpan: protractor.ElementFinder, private mutantReplacementSpan: protractor.ElementFinder) { }
+  constructor(private button: ElementFinder, private originalCodeSpan: ElementFinder, private mutantReplacementSpan: ElementFinder) { }
 
   mutantId = () => this.button.getAttribute('data-mutant');
 
   isMutantReplacementCodeVisible = () => this.mutantReplacementSpan.isDisplayed();
-  originalCodeTextDecoration = () => this.originalCodeSpan.getCssValue('text-decoration');
+  originalCodeTextDecoration = () => this.originalCodeSpan.getCssValue('text-decoration')
+    .then(textDecoration => /* like "none solid rgb(68, 68, 68)" */ textDecoration.split(' ')[0])
   mutantStatusAnnotation = () => this.button.getAttribute('data-mutant-status-annotation');
   originalCode = () => this.originalCodeSpan.getText();
   isButtonVisible = () => this.button.isDisplayed();
