6e25cdf1658fc868a0a9fa1d49d930ed73859584
Nico Jansen <jansennico@gmail.com>
2017-04-25 21:08:52 +0200

    test(protractor): Configure travis to run protractor tests
    * Let travis install java8
    * Let travis install latest firefox
    * Let travis do the webdriver-manager update
    * Hope for the best

--- packages/stryker-html-reporter/protractor.conf.js
+++ packages/stryker-html-reporter/protractor.conf.js
@@ -1,33 +1,6 @@
 const spawn = require('child_process').spawn;
 const os = require('os');
 
-function spawnWebdriverManager() {
-  return new Promise((res, rej) => {
-    const cmd = os.platform() === 'win32' ? 'webdriver-manager.cmd' : 'webdriver-manager';
-    const webdriverProcess = spawn(cmd, ['start']);
-    webdriverProcess.stdout.on('data', (data) => {
-      console.log(`stdout: ${data.toString()}`);
-    });
-
-    webdriverProcess.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
-      if (data.toString().indexOf('Selenium Server is up and running')) {
-        res();
-      }
-    });
-
-    webdriverProcess.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
-    });
-
-
-    process.on('SIGINT', () => {
-      console.log('Received SIGINT, killing webdriver-manager');
-      webdriverProcess.kill('SIGINT');
-    });
-  });
-}
-
 var config = {
   directConnect: true,
 
@@ -44,17 +17,13 @@ var config = {
   }
 };
 
-// if (process.env.TRAVIS) {
-//   const webdriverPromise = spawnWebdriverManager();
-
-//   config.capabilities = {
-//     browserName: 'firefox'
-//   };
-//   config.seleniumAddress = 'http://localhost:4444/wd/hub';
-//   config.directConnect = false;
-//   config.beforeLaunch = function () {
-//     return webdriverPromise;
-//   }
-// }
+if (process.env.TRAVIS) {
+  // Use the travis selenium server spawned in the container, only firefox is working atm
+  config.capabilities = {
+    browserName: 'firefox'
+  };
+  config.seleniumAddress = 'http://localhost:4444/wd/hub';
+  config.directConnect = false;
+}
 
 exports.config = config;
