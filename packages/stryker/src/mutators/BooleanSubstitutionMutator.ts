ced5afdcc1999ea0696234dbf1a048c5371b7f99
Alex van Assem <avassem@gmail.com>
2017-04-27 12:17:25 +0200

    feat(#252): Add boolean substitutions (true, false and !)

---
+++ packages/stryker/src/mutators/BooleanSubstitutionMutator.ts
@@ -0,0 +1,33 @@
+import { Mutator, IdentifiedNode } from 'stryker-api/mutant';
+import { Syntax } from 'esprima';
+
+export default class BooleanSubstitutionMutator implements Mutator {
+  name = 'BooleanSubstitution';
+  private type = Syntax.Literal;
+  private booleanValues: { [targetedOperator: string]: string } = {
+    'true': 'false',
+    'false': 'true'
+  };
+
+  applyMutations(node: IdentifiedNode, copy: <T>(obj: T, deep?: boolean) => T): IdentifiedNode[] {
+    let nodes: IdentifiedNode[] = [];
+    
+
+    // !a -> a
+    if (node.type === Syntax.UnaryExpression && node.operator === '!') {
+      let mutatedNode = copy(node.argument) as IdentifiedNode;
+      mutatedNode.nodeID = node.nodeID;
+      nodes.push(mutatedNode);
+    }
+
+    // true -> false or false -> true
+    if (node.type === this.type && this.booleanValues[node.raw]) {
+      let mutatedNode = copy(node);
+      mutatedNode.raw = this.booleanValues[node.raw];
+      mutatedNode.value = JSON.parse(mutatedNode.raw);
+      nodes.push(mutatedNode);
+    }
+    return nodes;
+  }
+
+}
---
+++ packages/stryker/test/unit/mutators/BooleanSubstitutionMutatorSpec.ts
@@ -0,0 +1,56 @@
+import { expect } from 'chai';
+import * as estree from 'estree';
+import { Identified } from 'stryker-api/mutant';
+import BooleanSubstitutionMutator from '../../../src/mutators/BooleanSubstitutionMutator';
+import { parse } from '../../../src/utils/parserUtils';
+import { copy } from '../../../src/utils/objectUtils';
+
+describe.only('BooleanSubstitutionMutator', () => {
+  let sut: BooleanSubstitutionMutator;
+
+  beforeEach(() => sut = new BooleanSubstitutionMutator());
+
+  it('should mutate when supplied a expression with !', () => {
+    // Arrange
+    const program = parse(`!a.a()`);
+    const nodeUnaryExpression = ((program.body[0] as estree.ExpressionStatement).expression as estree.UnaryExpression & Identified);
+
+    // Act
+    const result = sut.applyMutations(nodeUnaryExpression, copy)[0] as estree.Expression & Identified;
+
+    // Assert
+    expect(result).to.be.ok;
+    expect(result.type).to.be.eq(nodeUnaryExpression.argument.type);
+    expect(result.nodeID).to.be.eq(nodeUnaryExpression.nodeID);
+  });
+
+  it('should mutate true -> false', () => {
+    // Arrange
+    const program = parse(`true`);
+    const nodeLiteral = ((program.body[0] as estree.ExpressionStatement).expression as estree.Literal & Identified);
+
+    // Act
+    const result = sut.applyMutations(nodeLiteral, copy)[0] as estree.Literal & Identified;
+
+    // Assert
+    expect(result).to.be.ok;
+    expect(result.raw).to.be.eq('false');
+    expect(result.value).to.be.false;
+    expect(result.nodeID).to.be.eq(nodeLiteral.nodeID);
+  });
+
+  it('should mutate false -> true', () => {
+    // Arrange
+    const program = parse(`false`);
+    const nodeLiteral = ((program.body[0] as estree.ExpressionStatement).expression as estree.Literal & Identified);
+
+    // Act
+    const result = sut.applyMutations(nodeLiteral, copy)[0] as estree.Literal & Identified;
+
+    // Assert
+    expect(result).to.be.ok;
+    expect(result.raw).to.be.eq('true');
+    expect(result.value).to.be.true;
+    expect(result.nodeID).to.be.eq(nodeLiteral.nodeID);
+  });
+});
