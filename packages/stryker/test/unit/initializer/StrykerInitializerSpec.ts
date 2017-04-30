99f6f0ad89e7bac9cdc09d383a3b1ba0624169bc
Alex van Assem <avassem@gmail.com>
2017-04-30 14:59:20 +0200

    Added extra unit tests for StykerInitializer

--- packages/stryker/test/unit/initializer/StrykerInitializerSpec.ts
+++ packages/stryker/test/unit/initializer/StrykerInitializerSpec.ts
@@ -6,12 +6,13 @@ import * as inquirer from 'inquirer';
 import StrykerInitializer from '../../../src/initializer/StrykerInitializer';
 import * as restClient from 'typed-rest-client/RestClient';
 
-describe('StrykerInitializer', () => {
+describe.only('StrykerInitializer', () => {
   let sut: StrykerInitializer;
   let sandbox: sinon.SinonSandbox;
   let inquirerPrompt: sinon.SinonStub;
   let childExecSync: sinon.SinonStub;
   let fsWriteFile: sinon.SinonStub;
+  let fsExistsSync: sinon.SinonStub;
   let restClientNew: sinon.SinonStub;
   let restClientGet: sinon.SinonStub;
   let log: sinon.SinonStub;
@@ -22,6 +23,7 @@ describe('StrykerInitializer', () => {
     inquirerPrompt = sandbox.stub(inquirer, 'prompt');
     childExecSync = sandbox.stub(child, 'execSync');
     fsWriteFile = sandbox.stub(fs, 'writeFile');
+    fsExistsSync = sandbox.stub(fs, 'existsSync');
     restClientGet = sandbox.stub().returns({
       statusCode: 404
     });
@@ -54,6 +56,74 @@ describe('StrykerInitializer', () => {
           expect(testFramework.name).to.eq('testFramework');
           return true;
         }));
+      });
+
+      describe('when additional testrunners are found on npm', () => {
+
+        beforeEach(() => {
+          restClientGet.returns({
+            statusCode: 200,
+            result: {
+              total: 1,
+              results: [{ package: { name: 'stryker-jest-runner' } }]
+            }
+          });
+          inquirerPrompt.resolves({ 'testFramework': 'Mocha', 'testRunner': 'Mocha' });
+        });
+
+        it('should prompt the new testrunners as options', async () => {
+          await sut.initialize();
+          expect(inquirer.prompt).to.have.been.calledWith(sinon.match((questions: inquirer.Question[]) => {
+            const testRunner = questions[0];
+            expect(testRunner.choices).to.deep.include('Jest');
+            return true;
+          }));
+        });
+
+        describe('when new testrunner jest is selected', () => {
+          beforeEach(() => {
+            inquirerPrompt.resolves({ 'testFramework': 'Other/none', 'testRunner': 'Jest' });
+          });
+
+          it('should install jest-runner and html-reporter', async () => {
+            await sut.initialize();
+            expect(log).to.have.been.calledWith('Installing NPM dependencies...');
+            expect(child.execSync).to.have.been.calledWith('npm i --save-dev stryker-jest-runner stryker-html-reporter', { stdio: [0, 1, 2] });
+          });
+
+          it('should configure stryker with jest', async () => {
+            await sut.initialize();
+            expect(log).to.have.been.calledWith('Writing stryker.conf.js...');
+            expect(fs.writeFile).to.have.been.calledWith('stryker.conf.js', sinon.match('"testRunner": "jest"'));
+          });
+        });
+      });
+
+      describe('when karm.conf.js is found in project directory', () => {
+
+        beforeEach(() => {
+          fsExistsSync.returns(true);
+          inquirerPrompt.resolves({ 'testFramework': 'Jasmine', 'testRunner': 'Karma' });
+        });
+        
+        it('should log that karma.conf.js is found', async () => {
+          await sut.initialize();
+          expect(log).to.have.been.calledWith('Found karma.conf.js');
+        });
+
+        it('should select karma as default testrunner', async () => {
+          await sut.initialize();
+          expect(inquirer.prompt).to.have.been.calledWith(sinon.match((questions: inquirer.Question[]) => {
+            const testRunner = questions[0];
+            expect(testRunner.default).to.eq('Karma');
+            return true;
+          }));
+        });
+
+        it('should configure the karma.conf.js in stryker.conf.js', async () => {
+          await sut.initialize();
+          expect(fs.writeFile).to.have.been.calledWith('stryker.conf.js', sinon.match('"karmaConfigFile": "./karma.conf.js"'));
+        });
       });
 
       describe('when answered with mocha', () => {
@@ -119,7 +189,7 @@ describe('StrykerInitializer', () => {
         fsWriteFile.rejects(expectedError);
         inquirerPrompt.resolves({ 'testFramework': 'Mocha', 'testRunner': 'Mocha' });
       });
-      
+
       it('should reject with that error', () => {
         return expect(sut.initialize()).to.eventually.be.rejectedWith(expectedError);
       });
