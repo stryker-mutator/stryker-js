8bf1c816381df163be6d02dc6145dd5df92b59b2
Nico Jansen <jansennico@gmail.com>
2017-05-05 15:24:02 +0200

    feat(mocha-framework): Move mocha test framework to seperate package
    Move mocha test framework to its seperate package. This should prevent confusion about the state of the mocha test framework plugin. It only works with the stryker-mocha-runner test runner plugin at the moment, so it will downgrade `coverageAnalysis: 'perTest'` to `coverageAnalysis: 'all'` when another test runner is used.
    
    BREAKING CHANGE: Users with `testRunner: "mocha",testFramework: "mocha"` should now also install "stryker-mocha-framework".

---
+++ integrationTest/test/mocha-mocha/stryker.conf.js
@@ -0,0 +1,9 @@
+module.exports = function(config){
+  config.set({
+    files: ['src/*.js', 'test/*.js'],
+    mutate: ['src/*.js'],
+    testFramework: 'mocha',
+    testRunner: 'mocha',
+    reporter: ['clear-text', 'html']
+  });
+};
