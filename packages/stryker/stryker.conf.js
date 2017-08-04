e7b5b2bd977f2ffcf298dbb2d3532d30ec0380be
Nico Jansen <jansennico@gmail.com>
2017-08-04 22:35:10 +0200

    test(stryker.conf.js): Change !node_modules to src

--- packages/stryker/stryker.conf.js
+++ packages/stryker/stryker.conf.js
@@ -5,7 +5,7 @@ module.exports = function (config) {
       'test/unit/**/*.js',
       { pattern: 'src/**/*.js', included: false, mutated: true },
       { pattern: 'node_modules/stryker-api/*.js', included: false, mutated: false },
-      { pattern: 'node_modules/stryker-api/!(node_modules)/**/*.js', included: false, mutated: false }],
+      { pattern: 'node_modules/stryker-api/src/**/*.js', included: false, mutated: false }],
     testFramework: 'mocha',
     testRunner: 'mocha',
     reporter: ['progress', 'clear-text', 'event-recorder'],
