7220f8ece33d1c179930f9125d1f37170ad7b282
Nico Jansen <jansennico@gmail.com>
2017-08-10 07:10:30 +0200

    test(own-dogfood): Add stryker-api files in stryker.conf.js (#348)
    When running stryker on stryker, we didn't include the stryker-api module in the test files. This meant that every sandbox shared the stryker-api files with the `IsolatedtestRunnerAdapterWorker` child process. Since we now have a `MochaConfigWriter`, that one is always added to the `ConfigWriterFactory`. It poluted the tests in the sandbox, resulting in an initial test failure.

--- packages/stryker/stryker.conf.js
+++ packages/stryker/stryker.conf.js
@@ -1,10 +1,15 @@
 module.exports = function (config) {
   config.set({
-    files: ['test/helpers/**/*.js', 'test/unit/**/*.js', { pattern: 'src/**/*.js', included: false, mutated: true }],
+    files: [
+      'test/helpers/**/*.js',
+      'test/unit/**/*.js',
+      { pattern: 'src/**/*.js', included: false, mutated: true },
+      { pattern: 'node_modules/stryker-api/*.js', included: false, mutated: false },
+      { pattern: 'node_modules/stryker-api/src/**/*.js', included: false, mutated: false }],
     testFramework: 'mocha',
     testRunner: 'mocha',
     reporter: ['progress', 'clear-text', 'event-recorder'],
-    coverageAnalysis: 'all',
+    coverageAnalysis: 'perTest',
     logLevel: 'info',
     plugins: ['stryker-mocha-runner', 'stryker-mocha-framework']
   });
