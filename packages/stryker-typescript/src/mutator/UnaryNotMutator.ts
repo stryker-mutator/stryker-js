1e54e96834d53d7875cf599c52cbccc6728a9274
Simon de Lang <simondelang@gmail.com>
2017-08-25 18:08:34 +0200

    feat(UnaryNotMutator): Add UnaryNotMutator

---
+++ packages/stryker-typescript/src/mutator/UnaryNotMutator.ts
@@ -0,0 +1,20 @@
+import * as ts from 'typescript';
+import { Mutant } from 'stryker-api/mutant';
+import Mutator from './Mutator';
+
+export default class UnaryNotMutator extends Mutator<ts.PrefixUnaryExpression> {
+
+  name: string = 'UnaryNotMutator';
+
+  public guard(node: ts.Node): node is ts.PrefixUnaryExpression {
+    return node.kind === ts.SyntaxKind.PrefixUnaryExpression;
+  }
+
+  public mutate(node: ts.PrefixUnaryExpression): Mutant[] {
+    if(node.operator === ts.SyntaxKind.ExclamationToken) {
+      return [this.createMutant(node, node.operand.getText())];
+    } else {
+      return [];
+    }
+  }
+}
