75a229a55cf0f5a97c54f82e9e4e28264069660f
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-09-01 10:41:56 +0300

    test: add tests for clean and initialize

--- packages/stryker/test/unit/TempFolderSpec.ts
+++ packages/stryker/test/unit/TempFolderSpec.ts
@@ -4,22 +4,36 @@ import * as sinon from 'sinon';
 import { expect } from 'chai';
 import * as mkdirp from 'mkdirp';
 import * as fs from 'mz/fs';
+import * as fileUtils from '../../src/utils/fileUtils';
+import * as path from 'path';
 
 describe.only('TempFolder', () => {
   let sandbox: sinon.SinonSandbox;
   let cwdStub: sinon.SinonStub;
   let randomStub: sinon.SinonStub;
+  let deleteDirStub: sinon.SinonStub;
+  const defaultTempDirName = '.stryker-tmp';
+  const mockCwd = '/x/y/z/some/dir';
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
-    TempFolder.instance().initialize();
 
     sandbox.stub(mkdirp, 'sync');
     sandbox.stub(fs, 'writeFile');
+    deleteDirStub = sandbox.stub(fileUtils, 'deleteDir');
     cwdStub = sandbox.stub(process, 'cwd');
+    cwdStub.returns(mockCwd);
     randomStub = sandbox.stub(TempFolder.instance(), 'random');
+    randomStub.returns('rand');
+   
+    TempFolder.instance().initialize();
   });
   afterEach(() => sandbox.restore());
+
+  it('should have correct internal state after initialize call', () => {
+    expect(TempFolder.instance().baseTempFolder).to.equal(`${path.join(mockCwd, defaultTempDirName)}`);
+    expect(TempFolder.instance().tempFolder).to.equal(`${path.join(mockCwd, defaultTempDirName, 'rand')}`);
+  });
 
   it('TempFolder is presented', () => {
     const tempFolderInstance = TempFolder.instance();
@@ -33,14 +47,9 @@ describe.only('TempFolder', () => {
 
   describe('createRandomFolder', () => {
     it('should create dir with correct path', () => {
-      const mockCwd = process.cwd() + '/some/dir';
-      
-      cwdStub.returns(mockCwd);
-      randomStub.returns('rand');
-
       const result = TempFolder.instance().createRandomFolder('prefix');
 
-      expect(mkdirp.sync).to.have.been.calledOnce;
+      expect(mkdirp.sync).to.have.been.calledThrice;
       expect(result.includes('prefix')).to.be.true;
       expect(result.includes('rand')).to.be.true;
     });
@@ -48,7 +57,14 @@ describe.only('TempFolder', () => {
 
   describe('clean', () => {
     it('should call deleteDir fileApi', () => {
-      // TODO
+      deleteDirStub.resolves('delResolveStub');
+
+      const tempFolderInstance = TempFolder.instance();
+      const result = tempFolderInstance.clean();
+
+      expect(fileUtils.deleteDir).to.have.been.calledWith(tempFolderInstance.baseTempFolder);
+      
+      result.then(data => expect(data).equals('delResolveStub'))
     });
   });
 
