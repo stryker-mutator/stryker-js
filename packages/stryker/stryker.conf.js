f41a1199f84e4eff9c58292d311911601fdae118
Nico Jansen <jansennico@gmail.com>
2017-09-02 10:45:32 +0200

    create a dual stryker.conf.js for typescript and es5

--- packages/stryker/stryker.conf.js
+++ packages/stryker/stryker.conf.js
@@ -1,40 +1,48 @@
 module.exports = function (config) {
+
+  var typescript = true;
+
+  if (typescript) {
+    config.set({
+      files: [
+        { pattern: 'package.json', included: false, mutated: false },
+        '!test/integration/**/*.ts',
+        '!src/**/*.ts',
+        { pattern: 'src/**/*.ts', included: false, mutated: true },
+        { pattern: 'node_modules/stryker-api/*.js', included: false, mutated: false },
+        { pattern: 'node_modules/stryker-api/src/**/*.js', included: false, mutated: false }
+      ],
+      coverageAnalysis: 'off',
+      tsconfigFile: 'tsconfig.json',
+      mutantGenerator: 'typescript',
+      transpilers: [
+        'typescript'
+      ]
+    })
+  } else {
+    config.set({
+      files: [
+        'test/helpers/**/*.js',
+        'test/unit/**/*.js',
+        { pattern: 'src/**/*.js', included: false, mutated: true },
+        { pattern: 'node_modules/stryker-api/*.js', included: false, mutated: false },
+        { pattern: 'node_modules/stryker-api/src/**/*.js', included: false, mutated: false }
+      ],
+      coverageAnalysis: 'off',
+      mutantGenerator: 'es5'
+    });
+  }
+
   config.set({
-    // files: [
-    //   'test/helpers/**/*.ts',
-    //   'test/unit/**/*.ts',
-    //   { pattern: 'src/**/*.ts', included: false, mutated: true }
-    // ],
-    // { pattern: 'node_modules/stryker-api/*.js', included: false, mutated: false },
-    // { pattern: 'node_modules/stryker-api/src/**/*.js', included: false, mutated: false },
-    // { pattern: 'node_modules/stryker-api/*.d.ts', included: false, mutated: false },
-    // { pattern: 'node_modules/stryker-api/src/**/*.d.ts', included: false, mutated: false }],
-    files: [
-      { pattern: 'package.json', included: false, mutated: false },
-      '!test/integration/**/*.ts',
-      '!src/**/*.ts',
-      { pattern: 'src/ScoreResultCalculator.ts', included: false, mutated: true },
-      { pattern: 'src/**/*.ts', included: false, mutated: false },
-      { pattern: 'node_modules/stryker-api/*.js', included: false, mutated: false },
-      { pattern: 'node_modules/stryker-api/src/**/*.js', included: false, mutated: false },
-      { pattern: 'node_modules/stryker-api/*.d.ts', included: false, mutated: false },
-      { pattern: 'node_modules/stryker-api/src/**/*.d.ts', included: false, mutated: false }
-    ],
     testFramework: 'mocha',
     testRunner: 'mocha',
     reporter: ['progress', 'html', 'clear-text', 'event-recorder'],
-    coverageAnalysis: 'off',
     thresholds: {
       high: 80,
       low: 60,
       break: null
     },
     logLevel: 'info',
-    tsconfigFile: 'tsconfig.json',
-    mutantGenerator: 'typescript',
-    transpilers: [
-      'typescript'
-    ],
     plugins: [
       require.resolve('../stryker-mocha-runner/src/index'),
       require.resolve('../stryker-mocha-framework/src/index'),
