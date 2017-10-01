66f61fb06b7469237a4f44394b629afd1f5cdf3b
Nico Jansen <jansennico@gmail.com>
2017-10-01 20:34:10 +0200

    test(integration): Improve integration test stability (#389)
    * Remove legacy `Executor` class.
    * Add dev dependency on [execa](https://github.com/sindresorhus/execa) to spawn child processes during integration testing
    * Pipe output to host process as that seems to be the most stable for travis builds (don't now why)
    * Configure all integration tests as "info" to reduce integration test output.

--- integrationTest/run-integration-test.ts
+++ integrationTest/run-integration-test.ts
@@ -1,28 +1,25 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import Executor from './Executor';
+import execa = require('execa');
 
 describe('integration-tests', function () {
 
   this.timeout(500000);
 
-  before(done => {
-    new Executor('.').exec('npm install', {}, done);
+  before(() => {
+    console.log('  Exec npm install...');
+    return execa('npm', ['install'], { cwd: __dirname });
   });
 
-  const root = path.join('integrationTest', 'test');
-  const dirs = fs.readdirSync(root)
-    .filter(f => fs.statSync(path.join(root, f)).isDirectory());
+  const testRootDir = path.resolve(__dirname, 'test');
+  const dirs = fs.readdirSync(testRootDir)
+    .filter(file => fs.statSync(path.join(testRootDir, file)).isDirectory());
   dirs.forEach(testDir => {
     describe(testDir, () => {
-      it('should run test', (done) => {
-        new Executor(path.join('test', testDir)).exec('npm test', {}, error => {
-          if (error) {
-            done(new Error(error));
-          } else {
-            done();
-          }
-        });
+      it('should run test', () => {
+        const currentTestDir = path.resolve(testRootDir, testDir);
+        console.log(`    Exec ${testDir} npm test`);
+        execa.sync('npm', ['test'], { cwd: currentTestDir });
       });
     });
   });
