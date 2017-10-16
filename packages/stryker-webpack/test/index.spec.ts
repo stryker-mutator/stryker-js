6a56e55c1e27fbd2c104517d2b4d517fe1de095c
Sander Koenders <sanderkoenders@gmail.com>
2017-10-16 13:47:15 +0200

    Feat(Typescript): Typescript setup

---
+++ packages/stryker-webpack/test/index.spec.ts
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import Main from "../src/index";
+
+describe("index.js", () => {
+    let main: Main;
+
+    beforeEach(() => {
+         main = new Main();
+    });
+
+    it("Should return \"Hello world\" when the hello function is called", () => {
+        expect(main.hello()).to.equal("Hello World!");
+    });
+});
