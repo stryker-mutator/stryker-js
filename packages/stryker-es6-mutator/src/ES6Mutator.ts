8cfa384947393ec876327d024ae6956357413a82
Simon de Lang <simondelang@gmail.com>
2017-10-20 14:12:21 +0200

    refactor(ES6Mutator): Extract Copy function

--- packages/stryker-es6-mutator/src/ES6Mutator.ts
+++ packages/stryker-es6-mutator/src/ES6Mutator.ts
@@ -1,19 +1,11 @@
-import * as _ from 'lodash';
 import * as babel from 'babel-core';
 import { getLogger } from 'log4js';
 import { Mutator, Mutant } from 'stryker-api/mutant';
 import { File, FileKind, TextFile } from 'stryker-api/core';
 import { IdentifiedNode } from './IdentifiedNode';
 import BabelParser from './helpers/BabelParser';
+import Copy from './helpers/Copy';
 import NodeMutatorFactory from './NodeMutatorFactory';
-
-function copy<T>(obj: T, deep?: boolean) {
-  if (deep) {
-    return _.cloneDeep(obj);
-  } else {
-    return _.clone(obj);
-  }
-}
 
 export default class ES6Mutator implements Mutator {
   private log = getLogger(ES6Mutator.name);
@@ -25,12 +17,12 @@ export default class ES6Mutator implements Mutator {
 
     inputFiles.filter(i => i.kind === FileKind.Text && i.mutated).forEach((file: TextFile) => {
       const ast = BabelParser.getAst(file.content);
-      const baseAst = copy(ast, true);
+      const baseAst = Copy(ast, true);
       BabelParser.removeUseStrict(baseAst);
 
       BabelParser.getNodes(ast).forEach(node => {
         mutators.forEach(mutator => {
-          let mutatedNodes = mutator.mutate(node, copy);
+          let mutatedNodes = mutator.mutate(node, Copy);
 
           if (mutatedNodes) {
             const newMutants = this.generateMutants(mutatedNodes, baseAst, file, mutator.name);
