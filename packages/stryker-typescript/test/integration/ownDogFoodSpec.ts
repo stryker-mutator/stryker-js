fe28ac6fdbe721a6c28544c067c3c49525a6aed8
Sander Koenders <sanderkoenders@gmail.com>
2017-10-23 14:11:00 +0200

    Test(Typescript): Fix tests to comply with async transpile

--- packages/stryker-typescript/test/integration/ownDogFoodSpec.ts
+++ packages/stryker-typescript/test/integration/ownDogFoodSpec.ts
@@ -32,27 +32,27 @@ describe('stryker-typescript', function () {
     setGlobalLogLevel('trace');
   });
 
-  it('should be able to transpile itself', () => {
+  it('should be able to transpile itself', async () => {
     const transpiler = new TypescriptTranspiler({ config, keepSourceMaps: true });
-    const transpileResult = transpiler.transpile(inputFiles);
+    const transpileResult = await transpiler.transpile(inputFiles);
     expect(transpileResult.error).to.be.null;
     const outputFiles = transpileResult.outputFiles;
     expect(outputFiles.length).greaterThan(10);
   });
 
-  it('should result in an error if a variable is declared as any and noImplicitAny = true', () => {
+  it('should result in an error if a variable is declared as any and noImplicitAny = true', async () => {
     const transpiler = new TypescriptTranspiler({ config, keepSourceMaps: true });
     inputFiles[0].content += 'function foo(bar) { return bar; } ';
-    const transpileResult = transpiler.transpile(inputFiles);
+    const transpileResult = await transpiler.transpile(inputFiles);
     expect(transpileResult.error).contains('error TS7006: Parameter \'bar\' implicitly has an \'any\' type');
     expect(transpileResult.outputFiles.length).eq(0);
   });
 
-  it('should not result in an error if a variable is declared as any and noImplicitAny = false', () => {
+  it('should not result in an error if a variable is declared as any and noImplicitAny = false', async () => {
     config['tsconfig'].noImplicitAny = false;
     inputFiles[0].content += 'const shouldResultInError = 3';
     const transpiler = new TypescriptTranspiler({ config, keepSourceMaps: true });
-    const transpileResult = transpiler.transpile(inputFiles);
+    const transpileResult = await transpiler.transpile(inputFiles);
     expect(transpileResult.error).null;
   });
 });
