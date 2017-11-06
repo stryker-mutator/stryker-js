becbea718b2230456ef8873564b566d704ab3f08
Sander Koenders <sanderkoenders@gmail.com>
2017-11-06 14:49:23 +0100

    feat(configEditor): Add ConfigEditor to stryker-webpack

--- packages/stryker-webpack/src/index.ts
+++ packages/stryker-webpack/src/index.ts
@@ -1,4 +1,7 @@
 import { TranspilerFactory } from 'stryker-api/transpile';
+import { ConfigEditorFactory } from 'stryker-api/config';
 import WebpackTranspiler from "./WebpackTranspiler";
+import WebpackConfigEditor from "./WebpackConfigEditor";
 
 TranspilerFactory.instance().register('webpack', WebpackTranspiler);
+ConfigEditorFactory.instance().register('webpack', WebpackConfigEditor);
