c381a8703aa855fef197192bf4bdf3fcf6660668
Sander Koenders <sanderkoenders@gmail.com>
2017-11-20 12:16:48 +0100

    Feat(ConfigEditor): WebpackConfigEditor

--- packages/stryker-webpack/src/WebpackConfigEditor.ts
+++ packages/stryker-webpack/src/WebpackConfigEditor.ts
@@ -1,17 +1,32 @@
 import {ConfigEditor, Config} from "stryker-api/config";
+import { Configuration } from 'webpack';
+import { DefaultPreset, AngularPreset } from "./configEditor/presets";
 
 class WebpackConfigEditor implements ConfigEditor {
     public edit(strykerConfig: Config) {
-        strykerConfig.webpackConfig = strykerConfig.webpackConfig || {
-            entry: [],
-            output: {
-                path: "/out",
-                filename: "bundle.js",
-            }
+        // Set project to default when none is provided.
+        strykerConfig.project = strykerConfig.project || 'default';
+
+        // Use a preset configuration file based on the project when none is provided.
+        strykerConfig.webpackConfig = strykerConfig.webpackConfig || this.getConfig(strykerConfig.project);
+
+        // Force the webpack cache to true
+        strykerConfig.webpackConfig.cache = true;
+    }
+
+    private getConfig(project: string): Configuration {
+        let webpackConfig: Configuration;
+
+        switch(project.toLowerCase()) {
+            case 'angular':
+                webpackConfig = AngularPreset.getConfig();
+            break;
+            default:
+                webpackConfig = DefaultPreset.getConfig();
+            break
         }
 
-        // Force cache to true
-        strykerConfig.webpackConfig.cache = true;
+        return webpackConfig;
     }
 }
 
