2716a364c6d8a7ace8fed8141189593c92d44b79
Sander Koenders <sanderkoenders@gmail.com>
2017-11-21 14:29:59 +0100

    Feat(configEditor): Add WebpackConfigEditor

--- packages/stryker-webpack/src/WebpackConfigEditor.ts
+++ packages/stryker-webpack/src/WebpackConfigEditor.ts
@@ -1,25 +1,28 @@
 import { ConfigEditor, Config } from "stryker-api/config";
 import { Configuration } from 'webpack';
 import { DefaultPreset, AngularPreset } from "./configEditor/presets";
+import * as path from "path";
 
 class WebpackConfigEditor implements ConfigEditor {
     public edit(strykerConfig: Config) {
+        const basedir = strykerConfig.basedir || path.resolve(__dirname);
+
         // Set project to default when none is provided.
         strykerConfig.project = strykerConfig.project || 'default';
 
         // Use a preset configuration file based on the project when none is provided.
-        strykerConfig.webpackConfig = strykerConfig.webpackConfig || this.getConfig(strykerConfig.project);
+        strykerConfig.webpackConfig = strykerConfig.webpackConfig || this.getConfig(strykerConfig.project, basedir);
 
         // Force the webpack cache to true
         strykerConfig.webpackConfig.cache = true;
     }
 
-    private getConfig(project: string): Configuration {
+    private getConfig(project: string, root: string): Configuration {
         let webpackConfig: Configuration;
 
         switch(project.toLowerCase()) {
             case 'angular':
-                webpackConfig = AngularPreset.getConfig();
+                webpackConfig = AngularPreset.getConfig(root);
             break;
             default:
                 webpackConfig = DefaultPreset.getConfig();
