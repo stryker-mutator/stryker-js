fbe32ee92b324f9e5655ab59397ded458532b9cf
Sander Koenders <sanderkoenders@gmail.com>
2017-11-21 14:32:23 +0100

    Feat(presets): Angular presets for the ConfigEditor

--- packages/stryker-webpack/src/configEditor/presets/angular/loaders.ts
+++ packages/stryker-webpack/src/configEditor/presets/angular/loaders.ts
@@ -1,37 +1,47 @@
 import * as path from "path"
 import {Rule} from "webpack";
 
-export const typescriptLoader: any = {
-    test: /\.ts$/,
-    loaders: [
-        {
-            loader: 'awesome-typescript-loader',
-            options: { 
-                configFileName: path.join('stryker', 'config', 'tsconfig.json') 
-            }
-        }, 'angular2-template-loader'
-    ]
+export const typescriptLoader = (root: string): any => {
+    return {
+        test: /\.ts$/,
+        loaders: [
+            {
+                loader: 'awesome-typescript-loader',
+                options: { 
+                    configFileName: path.join(root, 'stryker', 'config', 'tsconfig.json') 
+                }
+            }, 'angular2-template-loader'
+        ]
+    };
 };
 
-export const htmlLoader: Rule = {
-    test: /\.html$/,
-    loader: 'html-loader'
+export const htmlLoader = (root: string): Rule => {
+    return {
+        test: /\.html$/,
+        loader: 'html-loader'
+    };
 };
 
-export const imageLoader: Rule = {
-    test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-    loader: 'file-loader?name=assets/[name].[hash].[ext]'
+export const imageLoader = (root: string): Rule => {
+    return {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'null-loader'
+    };
 };
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-export const cssStyleLoader: Rule = {
-    test: /\.css$/,
-    exclude: path.join('src', 'app'),
-    loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap' })
+export const cssStyleLoader = (root: string): Rule => {
+    return {
+        test: /\.css$/,
+        exclude: path.join(root, 'src', 'app'),
+        loader: 'null-loader'
+    };
 };
 
-export const cssRawLoader: Rule = {
-    test: /\.css$/,
-    include: path.join('src', 'app'),
-    loader: 'raw-loader'
+export const cssRawLoader = (root: string): Rule => {
+    return {
+        test: /\.css$/,
+        include: path.join(root, 'src', 'app'),
+        loader: 'raw-loader'
+    };
 };
+
