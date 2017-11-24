85fd540b22d078dcd129c17cb17ae6f4551c263f
Simon de Lang <simondelang@gmail.com>
2017-11-24 16:03:40 +0100

    fix(BooleanSubstitutionMutator): Remove duplicate unarynotmutator

--- packages/stryker-javascript-mutator/src/mutators/BooleanSubstitutionMutator.ts
+++ packages/stryker-javascript-mutator/src/mutators/BooleanSubstitutionMutator.ts
@@ -6,14 +6,6 @@ export default class BooleanSubstitutionMutator implements NodeMutator {
 
   mutate(node: types.Node, copy: <T extends types.Node>(obj: T, deep?: boolean) => T): types.Node[] {
     const nodes: types.Node[] = [];
-    
-    // !a -> a
-    if (types.isUnaryExpression(node) && node.operator === '!') {
-      let mutatedNode = copy(node.argument);
-      mutatedNode.start = node.start;
-      mutatedNode.end = node.end;
-      nodes.push(mutatedNode);
-    }
 
     // true -> false or false -> true
     if (types.isBooleanLiteral(node)) {
