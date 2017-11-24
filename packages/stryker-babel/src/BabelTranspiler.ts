0059031fcdcfe4c03cfec16a807d6c6e268b4191
Sander Koenders <sanderkoenders@gmail.com>
2017-11-24 17:19:24 +0100

    Feat(Babel): Add babel transpiler for Stryker

--- packages/stryker-babel/src/BabelTranspiler.ts
+++ packages/stryker-babel/src/BabelTranspiler.ts
@@ -1,11 +1,57 @@
-import {Transpiler} from 'stryker-api/transpile';
-import { File } from 'stryker-api/core';
-import TranspileResult from 'stryker-api/src/transpile/TranspileResult';
-import FileLocation from 'stryker-api/src/transpile/FileLocation';
+import { Transpiler, TranspilerOptions, TranspileResult, FileLocation } from 'stryker-api/transpile';
+import { File, TextFile, FileKind } from 'stryker-api/core';
+import * as babel from 'babel-core';
 
 class BabelTranspiler implements Transpiler {
+    private _babelConfig: babel.GeneratorOptions;
+
+    public constructor(options: TranspilerOptions) {
+        this._babelConfig = options.config.babelConfig;
+    }
+    
     public transpile(files: Array<File>): Promise<TranspileResult> {
-        throw new Error('Not implemented');
+        const results: Array<File> = [];
+        
+        try {
+            files.forEach((file) => {
+                if (file.kind === FileKind.Text) {
+                    results.push(this.transform(file));
+                }
+                else {
+                    results.push(file);
+                }
+            });            
+        } catch (err) {
+            return Promise.resolve(this.createErrorResult(err));
+        }
+
+        return Promise.resolve(this.createSuccessResult(results));
+    }
+
+    private transform(file: TextFile) {
+        const result = babel.transform(file.content, this._babelConfig).code;
+        
+        if (!result) {
+            throw new Error('Could not transplile file with the Babel transform function');
+        }
+
+        file.content = result;
+
+        return file;
+    }
+
+    private createErrorResult(error: string): TranspileResult {
+        return {
+            error: error,
+            outputFiles: []
+        };
+    }
+
+    private createSuccessResult(files: Array<File>): TranspileResult {
+        return {
+            error: null,
+            outputFiles: files
+        };
     }
 
     public getMappedLocation(): FileLocation {
