04a54a6a9831fb567b74bb24deeb3074eee6d767
Sander Koenders <sanderkoenders@gmail.com>
2017-11-25 10:34:28 +0100

    Test(Transpiler): Add and refactor tests for BabelTranspiler

--- packages/stryker-babel/test/unit/BabelTranspilerSpec.ts
+++ packages/stryker-babel/test/unit/BabelTranspilerSpec.ts
@@ -4,75 +4,90 @@ import { File } from 'stryker-api/core';
 import { Transpiler } from 'stryker-api/transpile';
 import { Position, FileKind } from 'stryker-api/core';
 import { Config } from 'stryker-api/config';
+import { createFile } from '../helpers/producers';
 import * as sinon from 'sinon';
 import * as babel from 'babel-core';
-import { createFile } from '../helpers/producers';
 
 describe('BabelTranspiler', () => {
-    let babelTranspiler: Transpiler;
-    let sandbox: sinon.SinonSandbox;
-    let files: Array<File> = [];
-    let transformStub: sinon.SinonStub;
+  let babelTranspiler: Transpiler;
+  let sandbox: sinon.SinonSandbox;
+  let files: Array<File> = [];
+  let transformStub: sinon.SinonStub;
 
-    beforeEach(() => {
-        sandbox = sinon.sandbox.create();
+  const knownFileExtensions = ['.js', '.jsx', '.ts'];
 
-        transformStub = sandbox.stub(babel, 'transform').callsFake((content) => {
-            return {
-                code: content
-            };
-        });
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
 
-        babelTranspiler = new BabelTranspiler({config: new Config, keepSourceMaps: false});
-
-        files = [
-            createFile('sum', 'lkjghjk'),
-            createFile('main', 'asdfasd'),
-            createFile('lol', 'sretsd')
-        ];
+    transformStub = sandbox.stub(babel, 'transform').callsFake((content) => {
+      return {
+        code: content
+      };
     });
 
-    afterEach(() => sandbox.restore());
+    babelTranspiler = new BabelTranspiler({ config: new Config, keepSourceMaps: false });
 
-    describe('Transpile', () => {
-        it('Should call the babel transform function with all given text files', async () => {
-            const transpileResult = await babelTranspiler.transpile(files);
+    files = [
+      createFile('main.js', 'const main = () => { sum(2); devide(2); }'),
+      createFile('sum.js', 'const sum = (number) => number + number;'),
+      createFile('devide.js', 'const devide = (number) => number / number;')
+    ];
+  });
 
-            expect(transpileResult.outputFiles).to.deep.equal(files);
-        });
+  afterEach(() => sandbox.restore());
 
-        it('Should not call the transform function with any file other than text files', async () => {
-            files.push(createFile('binaryFile', 'asdgvfhaklsnd', FileKind.Binary));
+  describe('transpile', () => {
+    it('should call the babel transform function with all given text files', async () => {
+      const transpileResult = await babelTranspiler.transpile(files);
 
-            await babelTranspiler.transpile(files);
-
-            assert(transformStub.callCount === 3);
-        });
-
-        it('Should return with an error when the babel transform fails', async () => {
-            transformStub.callsFake(() => '');
-
-            const transpileResult = await babelTranspiler.transpile(files);
-
-            expect(transpileResult.outputFiles).to.be.empty;
-            expect(transpileResult.error).to.deep.equal('Could not transpile file with the Babel transform function');
-        });
+      expect(transpileResult.outputFiles).to.deep.equal(files);
     });
 
-    describe('GetMappedLocation', () => {
-        it('Should throw a not implemented error', () => {
-            const position: Position = {
-                line: 0,
-                column: 0
-            };
-    
-            const fileLocation: { fileName: string, start: Position, end: Position } = {
-                fileName: 'test',
-                start: position,
-                end: position
-            };
-    
-            expect(() => babelTranspiler.getMappedLocation(fileLocation)).to.throw(Error, 'Not implemented');
-        });
+    it('should not call the transform function with any file other than text files', async () => {
+      await babelTranspiler.transpile([createFile('myBinaryFile.png', 'S�L!##���XLDDDDDDDD\K�', FileKind.Binary)]);
+
+      assert(transformStub.notCalled);
     });
+
+    it('should only call the transform function when the file extension is a known file extension', async () => {
+      const files: Array<File> = [];
+
+      knownFileExtensions.forEach((knownFileExtension) => {
+        files.push(createFile('file'.concat(knownFileExtension), 'const a = 5;'));
+      });
+
+      const result = await babelTranspiler.transpile(files);
+
+      assert(transformStub.callCount === knownFileExtensions.length);
+      knownFileExtensions.forEach((knownFileExtension, key) => {
+        expect(result.outputFiles[key]).to.not.equal(files[key]);
+      });
+    });
+
+    it('should return with an error when the babel transform fails', async () => {
+      transformStub.callsFake(() => ({ code: '' }));
+
+      const transpileResult = await babelTranspiler.transpile([createFile('picture.js', 'S�L!##���XLDDDDDDDD\K�')]);
+
+      expect(transpileResult.outputFiles).to.be.empty;
+      expect(transpileResult.error).to.deep.equal('picture.js: Could not transpile file with the Babel transform function');
+    });
+  });
+
+  describe('getMappedLocation', () => {
+    it('should throw a not implemented error', () => {
+      const position: Position = {
+        line: 0,
+        column: 0
+      };
+
+      const fileLocation: { fileName: string, start: Position, end: Position } = {
+        fileName: 'test',
+        start: position,
+        end: position
+      };
+
+      expect(() => babelTranspiler.getMappedLocation(fileLocation)).to.throw(Error, 'Not implemented');
+    });
+  });
 });
