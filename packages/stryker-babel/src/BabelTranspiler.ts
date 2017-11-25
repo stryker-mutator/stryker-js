3775f661c7baa941d0167797e1e4f8caac6e3df0
Sander Koenders <sanderkoenders@gmail.com>
2017-11-25 10:33:55 +0100

    Fix(Extension): Check for known file extensions

--- packages/stryker-babel/src/BabelTranspiler.ts
+++ packages/stryker-babel/src/BabelTranspiler.ts
@@ -1,62 +1,90 @@
 import { Transpiler, TranspilerOptions, TranspileResult, FileLocation } from 'stryker-api/transpile';
 import { File, TextFile, FileKind } from 'stryker-api/core';
 import * as babel from 'babel-core';
+import * as path from 'path';
 
 class BabelTranspiler implements Transpiler {
-    private _babelConfig: babel.GeneratorOptions;
+  private _babelConfig: babel.GeneratorOptions;
+  private _knownExtensions: Array<string>;
 
-    public constructor(options: TranspilerOptions) {
-        this._babelConfig = options.config.babelConfig;
-    }
-    
-    public transpile(files: Array<File>): Promise<TranspileResult> {
-        const results: Array<File> = [];
+  public constructor(options: TranspilerOptions) {
+    this._babelConfig = options.config.babelConfig;
+    this._knownExtensions = ['.js', '.jsx', '.ts'];
+  }
 
-        try {
-            files.forEach((file) => {
-                if (file.kind === FileKind.Text) {
-                    results.push(this.transform(file));
-                }
-                else {
-                    results.push(file);
-                }
-            });            
-        } catch (err) {
-            return Promise.resolve(this.createErrorResult(err.message));
+  public transpile(files: Array<File>): Promise<TranspileResult> {
+    const results: Array<File> = [];
+    const errorResults: Array<string> = [];
+
+    files.forEach((file) => {
+      if (file.kind === FileKind.Text) {
+        let content: string = file.content;
+
+        if (this.shouldBeTranspiled(file)) {
+          try {
+            content = this.transform(file.content);
+          } catch (err) {
+            errorResults.push(`${file.name}: ${err.message}`);
+          }
         }
 
-        return Promise.resolve(this.createSuccessResult(results));
+        results.push(this.createTextFile(file.name, content, file.mutated, file.included, file.transpiled));
+      }
+    });
+
+    return Promise.resolve(this.createResult(results, errorResults));
+  }
+
+  private shouldBeTranspiled(file: File) {
+    return this._knownExtensions.indexOf(`${path.extname(file.name)}`) !== -1 && file.transpiled;
+  }
+
+  private transform(content: string) {
+    const result = babel.transform(content, this._babelConfig).code;
+
+    if (!result) {
+      throw new Error('Could not transpile file with the Babel transform function');
     }
 
-    private transform(file: TextFile) {
-        const result = babel.transform(file.content, this._babelConfig).code;
-        
-        if (!result) {
-            throw new Error('Could not transpile file with the Babel transform function');
-        }
+    return result;
+  }
 
-        file.content = result;
+  private createTextFile(name: string, content: string, mutated: boolean, included: boolean, transpiled: boolean): TextFile {
+    return {
+      name: name,
+      content: content,
+      mutated: mutated,
+      included: included,
+      transpiled: transpiled,
+      kind: FileKind.Text
+    };
+  }
 
-        return file;
+  private createResult(results: Array<File>, errorResults: Array<string>): TranspileResult {
+    if (errorResults.length > 0) {
+      return this.createErrorResult(errorResults.join('\n\r'));
     }
 
-    private createErrorResult(error: string): TranspileResult {
-        return {
-            error: error,
-            outputFiles: []
-        };
-    }
+    return this.createSuccessResult(results);
+  }
 
-    private createSuccessResult(files: Array<File>): TranspileResult {
-        return {
-            error: null,
-            outputFiles: files
-        };
-    }
+  private createErrorResult(error: string): TranspileResult {
+    return {
+      error: error,
+      outputFiles: []
+    };
+  }
 
-    public getMappedLocation(): FileLocation {
-        throw new Error('Not implemented');
-    }
+  private createSuccessResult(files: Array<File>): TranspileResult {
+    return {
+      error: null,
+      outputFiles: files
+    };
+  }
+
+  public getMappedLocation(): FileLocation {
+    throw new Error('Not implemented');
+  }
 }
 
 export default BabelTranspiler;
