951adbb2b52354fa2a1dbcf3935727a21ac7667e
Thomas Peters <ThomasPe@infosupport.com>
2017-11-25 10:22:52 +0100

    deleted unnecessary files from branch

--- packages/stryker-typescript/testResources/sampleProject/math.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function add(a: number, b: number): number;
-export declare function concat(a: string, b: string): string;
--- packages/stryker-typescript/testResources/sampleProject/useMath.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var math = require("./math");
-var a = 42;
-var b = 31;
-console.log("a + b ", math.add(a, b));
-//# sourceMappingURL=useMath.js.map
