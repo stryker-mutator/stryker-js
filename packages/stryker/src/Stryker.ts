4f9a7f91c08e5338e9ea917ff8f30a8c063f5779
Alex van Assem <avassem@gmail.com>
2017-11-28 10:04:52 +0100

    Fix name of tempDir configuration

--- packages/stryker/src/Stryker.ts
+++ packages/stryker/src/Stryker.ts
@@ -48,7 +48,7 @@ export default class Stryker {
   async runMutationTest(): Promise<MutantResult[]> {
     this.timer.reset();
     const inputFiles = await new InputFileResolver(this.config.mutate, this.config.files, this.reporter).resolve();
-    TempDir.instance().initialize(this.config.tempFolder);
+    TempDir.instance().initialize(this.config.tempDir);
     const initialTestRunProcess = this.createInitialTestRunner(inputFiles);
     const initialTestRunResult = await initialTestRunProcess.run();
     const testableMutants = await this.mutate(inputFiles, initialTestRunResult);
