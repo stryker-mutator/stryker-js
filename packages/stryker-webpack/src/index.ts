45b781d6cdb3c031ee178a387e177c2e167ead60
Sander Koenders <sanderkoenders@gmail.com>
2017-11-30 09:13:21 +0100

    Feat(ConfigEditor): Temporarily disable config editor

--- packages/stryker-webpack/src/index.ts
+++ packages/stryker-webpack/src/index.ts
@@ -1,7 +1,7 @@
 import { TranspilerFactory } from 'stryker-api/transpile';
-import { ConfigEditorFactory } from 'stryker-api/config';
+// import { ConfigEditorFactory } from 'stryker-api/config';
 import WebpackTranspiler from "./WebpackTranspiler";
-import WebpackConfigEditor from "./WebpackConfigEditor";
+// import WebpackConfigEditor from "./WebpackConfigEditor";
 
 TranspilerFactory.instance().register('webpack', WebpackTranspiler);
-ConfigEditorFactory.instance().register('webpack', WebpackConfigEditor);
+// ConfigEditorFactory.instance().register('webpack', WebpackConfigEditor);
