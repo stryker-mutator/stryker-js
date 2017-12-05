c257c555652a5558fc0145a222b53a9c1bd0f0b2
Sander Koenders <sanderkoenders@gmail.com>
2017-12-05 11:46:01 +0100

    Refactor(ConfigEditor): Remove configEditor from the implementation

--- packages/stryker-webpack/src/index.ts
+++ packages/stryker-webpack/src/index.ts
@@ -1,7 +1,4 @@
 import { TranspilerFactory } from 'stryker-api/transpile';
-// import { ConfigEditorFactory } from 'stryker-api/config';
 import WebpackTranspiler from "./WebpackTranspiler";
-// import WebpackConfigEditor from "./WebpackConfigEditor";
 
 TranspilerFactory.instance().register('webpack', WebpackTranspiler);
-// ConfigEditorFactory.instance().register('webpack', WebpackConfigEditor);
