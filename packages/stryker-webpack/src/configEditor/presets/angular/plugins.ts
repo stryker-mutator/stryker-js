936c15c4d848fb05bb04fa5a1f1f450c28dcc085
Sander Koenders <sanderkoenders@gmail.com>
2017-12-05 11:45:07 +0100

    Refactor(rename): Rename configEditor to configGenerator

--- packages/stryker-webpack/src/configEditor/presets/angular/plugins.ts
+++
@@ -1,9 +0,0 @@
-import * as path from "path";
-import { ContextReplacementPlugin, Plugin } from 'webpack';
-
-export const contextReplacementPlugin = (root: string): Plugin => new ContextReplacementPlugin(
-  // The (\\|\/) piece accounts for path separators in *nix and Windows
-  /angular(\\|\/)core(\\|\/)@angular/,
-  path.join(root, 'src'), // location of your src
-  {} // a map of your routes
-);
