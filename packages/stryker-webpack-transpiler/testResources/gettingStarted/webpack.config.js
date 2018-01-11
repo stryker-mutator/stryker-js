66d49df9efeeda3461ac84b960e16c63999bae80
Nico Jansen <jansennico@gmail.com>
2018-01-11 13:23:07 +0100

    test(webpack-transpiler): Add our first integration test
    More will follow.

---
+++ packages/stryker-webpack-transpiler/testResources/gettingStarted/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+module.exports = {
+  entry: './src/main.js',
+  context: __dirname, 
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-first-webpack.bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.txt$/, use: 'raw-loader' }
+    ]
+  }
+};
