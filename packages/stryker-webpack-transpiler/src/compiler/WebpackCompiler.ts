37f840d6ff1a47647e5cfcae5c44802d1f9cd997
Nico Jansen <jansennico@gmail.com>
2018-01-18 07:51:28 +0100

    refactor(webpack-transpiler): rewrite output file sorting
    Rewrite output file sorting. Instead of  using a webpack plugin, use the output stats object to reach the chunks (needed for sorting).

--- packages/stryker-webpack-transpiler/src/compiler/WebpackCompiler.ts
+++ packages/stryker-webpack-transpiler/src/compiler/WebpackCompiler.ts
@@ -4,23 +4,19 @@ import webpack from './Webpack';
 import * as path from 'path';
 import InputFileSystem from '../fs/InputFileSystem';
 import OutputFileSystem from '../fs/OutputFileSystem';
-import OutputSorterPlugin from './OutputSorterPlugin';
 
 export default class WebpackCompiler {
   private _compiler: Compiler;
-  private _inputFS = new InputFileSystem();
+  private _inputFS: InputFileSystem;
   private _outputFS = new OutputFileSystem();
-  private _sorter = new OutputSorterPlugin();
 
   public constructor(webpackConfig: Configuration) {
     this._compiler = this.createCompiler(webpackConfig);
   }
 
   private createCompiler(webpackConfig: Configuration): Compiler {
-    const plugins = webpackConfig.plugins || (webpackConfig.plugins = []);
-    plugins.push(this._sorter);
     const compiler = webpack(webpackConfig);
-
+    this._inputFS = new InputFileSystem();
     // Setting filesystem to provided fs so compilation can be done in memory
     (compiler as any).inputFileSystem = this._inputFS;
     compiler.outputFileSystem = this._outputFS;
@@ -41,22 +37,15 @@ export default class WebpackCompiler {
   private writeToFs(file: TextFile | BinaryFile): void {
     // Create the directory
     this._inputFS.mkdirpSync(path.dirname(file.name));
-
-    // Write the file to the filesystem
     this._inputFS.writeFileSync(file.name, file.content);
   }
 
   public emit(): Promise<File[]> {
-    return this.compile().then(() => {
-      const outputFiles = this._outputFS.collectFiles();
+    return this.compile().then(stats => {
+      const jsonStats = stats.toJson({ chunks: true});
+      const outputFiles = this._outputFS.collectFiles(jsonStats.chunks);
       this._outputFS.purge();
 
-      const sortedFiles = this._sorter.sortedFileNames;
-      outputFiles.sort((a, b) => {
-        const aName = path.basename(a.name);
-        const bName = path.basename(b.name);
-        return sortedFiles.indexOf(aName) - sortedFiles.indexOf(bName);
-      });
       return outputFiles;
     });
   }
--- packages/stryker-webpack-transpiler/test/unit/fs/OutputFileSystemSpec.ts
+++ packages/stryker-webpack-transpiler/test/unit/fs/OutputFileSystemSpec.ts
@@ -1,15 +1,19 @@
 import * as path from 'path';
-import { FileKind, File } from 'stryker-api/core';
+import { FileKind, File, TextFile } from 'stryker-api/core';
 import { expect } from 'chai';
 import OutputFileSystem from '../../../src/fs/OutputFileSystem';
-
+import { createChunk, Mock, createMockInstance } from '../../helpers/producers';
+import ChunkSorter, * as chunkSorterModule from '../../../src/compiler/ChunkSorter';
 
 describe('OutputFileSystem', () => {
 
   let sut: OutputFileSystem;
+  let chunkSorterMock: Mock<ChunkSorter>;
 
   beforeEach(() => {
     sut = new OutputFileSystem();
+    chunkSorterMock = createMockInstance(ChunkSorter);
+    sandbox.stub(chunkSorterModule, 'default').returns(chunkSorterMock);
   });
 
   actions('mkdir', 'rmdir').forEach(action => {
@@ -24,40 +28,88 @@ describe('OutputFileSystem', () => {
 
   it('should delete files when "unlink" is called', done => {
     sut.writeFile('file1', 'data', () => {
-      expect(sut.collectFiles()).lengthOf(1);
+      expect(sut.collectFiles([])).lengthOf(1);
       sut.unlink('file1', () => {
-        expect(sut.collectFiles()).lengthOf(0);
+        expect(sut.collectFiles([])).lengthOf(0);
         done();
       });
     });
   });
 
-  it('should collect files when "collectFiles" is called', done => {
-    const binContent = Buffer.from('');
-    sut.writeFile('bin1', binContent, () => {
-      sut.writeFile('file1', 'data', () => {
-        const expectedFiles: File[] = [
-          {
-            kind: FileKind.Binary,
-            content: binContent,
-            name: path.resolve('bin1'),
-            mutated: false,
-            transpiled: true,
-            included: false
-          },
-          {
-            kind: FileKind.Text,
-            content: 'data',
-            name: path.resolve('file1'),
-            mutated: true,
-            transpiled: true,
-            included: true
-          }];
-        expect(sut.collectFiles()).deep.eq(expectedFiles);
-        done();
+  describe('when "collectFiles"', () => {
+
+    it('should collect files', done => {
+      const binContent = Buffer.from('');
+      chunkSorterMock.sortedFileNames.returns([]);
+      sut.writeFile('bin1.ico', binContent, () => {
+        sut.writeFile('file1', 'data', () => {
+          const expectedFiles: File[] = [
+            {
+              kind: FileKind.Binary,
+              content: binContent,
+              name: path.resolve('bin1.ico'),
+              mutated: false,
+              transpiled: true,
+              included: false
+            },
+            {
+              kind: FileKind.Text,
+              content: 'data',
+              name: path.resolve('file1'),
+              mutated: true,
+              transpiled: true,
+              included: true
+            }];
+          expect(sut.collectFiles([])).deep.eq(expectedFiles);
+          done();
+        });
       });
     });
+
+    it('should sort the files based on given chunks', () => {
+      // Arrange
+      const expectedChunks = [createChunk({ id: '1' })];
+      chunkSorterMock.sortedFileNames.returns(['1', '2', '3']);
+      sut.writeFile('2', '2', () => { });
+      sut.writeFile('1', '1', () => { });
+      sut.writeFile('3', '3', () => { });
+      const expectedFiles: TextFile[] = [
+        {
+          kind: FileKind.Text,
+          content: '1',
+          name: path.resolve('1'),
+          mutated: true,
+          transpiled: true,
+          included: true
+        },
+        {
+          kind: FileKind.Text,
+          content: '2',
+          name: path.resolve('2'),
+          mutated: true,
+          transpiled: true,
+          included: true
+        },
+        {
+          kind: FileKind.Text,
+          content: '3',
+          name: path.resolve('3'),
+          mutated: true,
+          transpiled: true,
+          included: true
+        },
+      ];
+
+      // Act
+      const actualResult = sut.collectFiles(expectedChunks);
+
+
+      // Assert
+      expect(actualResult).deep.eq(expectedFiles);
+    });
+
   });
+
 
   function actions(...actions: Array<'mkdir' | 'rmdir'>) {
     return actions;
