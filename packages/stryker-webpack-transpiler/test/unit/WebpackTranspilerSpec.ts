d4ffcbd7685766d8855fcf135f6ed31f93bde023
Nico Jansen <jansennico@gmail.com>
2018-01-19 16:18:14 +0100

    feat(webpack-transpiler): support silent config setting
    Support `silent` config setting (default: true). When silent is set to true, the pugin with the name 'ProgessPlugin' is removed from the plugin list.

--- packages/stryker-webpack-transpiler/test/unit/WebpackTranspilerSpec.ts
+++ packages/stryker-webpack-transpiler/test/unit/WebpackTranspilerSpec.ts
@@ -1,47 +1,42 @@
 import WebpackTranspiler from '../../src/WebpackTranspiler';
 import ConfigLoader, * as configLoaderModule from '../../src/compiler/ConfigLoader';
 import WebpackCompiler, * as webpackCompilerModule from '../../src/compiler/WebpackCompiler';
-import { createTextFile, Mock, createMockInstance } from '../helpers/producers';
-import * as sinon from 'sinon';
+import { createTextFile, Mock, createMockInstance, createStrykerWebpackConfig } from '../helpers/producers';
 import { Config } from 'stryker-api/config';
 import { Position, TextFile } from 'stryker-api/core';
 import { expect } from 'chai';
 import { Configuration } from 'webpack';
+import * as log4js from 'log4js';
 
 describe('WebpackTranspiler', () => {
   let webpackTranspiler: WebpackTranspiler;
-  let sandbox: sinon.SinonSandbox;
   let config: Config;
 
   // Stubs
   let configLoaderStub: Mock<ConfigLoader>;
-  let webpackCompilerStub: WebpackCompilerStub;
+  let webpackCompilerStub: Mock<WebpackCompiler>;
 
   let exampleBundleFile: TextFile = createTextFile('bundle.js');
   let webpackConfig: Configuration;
 
   beforeEach(() => {
-    sandbox = sinon.sandbox.create();
-
-    webpackCompilerStub = sinon.createStubInstance(WebpackCompiler);
+    webpackCompilerStub = createMockInstance(WebpackCompiler);
     webpackCompilerStub.emit.returns([exampleBundleFile]);
 
     webpackConfig = { entry: './main.js' };
     configLoaderStub = createMockInstance(ConfigLoader);
-    configLoaderStub.load.returns(webpackConfig);
+    configLoaderStub.load.returns(webpackConfig); 
 
+    sandbox.stub(log4js, 'setGlobalLogLevel');
     sandbox.stub(configLoaderModule, 'default').returns(configLoaderStub);
     sandbox.stub(webpackCompilerModule, 'default').returns(webpackCompilerStub);
 
     config = new Config;
-    config.set({ webpack: { project: 'ExampleProject' } });
-
-    webpackTranspiler = new WebpackTranspiler({ config, keepSourceMaps: false });
+    config.set({ webpack: {} });
   });
 
-  afterEach(() => sandbox.restore());
-
   it('should only create the compiler once', async () => {
+    webpackTranspiler = new WebpackTranspiler({ config, keepSourceMaps: false });
     await webpackTranspiler.transpile([]);
     await webpackTranspiler.transpile([]);
 
@@ -50,11 +45,17 @@ describe('WebpackTranspiler', () => {
     expect(webpackCompilerModule.default).calledOnce;
     expect(webpackCompilerModule.default).calledWithNew;
     expect(configLoaderStub.load).calledOnce;
-    expect(configLoaderStub.load).calledWith('webpack.config.js');
+    expect(configLoaderStub.load).calledWith(createStrykerWebpackConfig());
+  });
 
+  it('should set global log level when compiler is called', () => {
+    config.logLevel = 'foobar level';
+    new WebpackTranspiler({ config, keepSourceMaps: false });
+    expect(log4js.setGlobalLogLevel).calledWith('foobar level');
   });
 
   it('should call the webpackCompiler.writeFilesToFs function with the given files', async () => {
+    webpackTranspiler = new WebpackTranspiler({ config, keepSourceMaps: false });
     const files = [createTextFile('main.js'), createTextFile('sum.js'), createTextFile('divide.js')];
 
     await webpackTranspiler.transpile(files);
@@ -63,6 +64,7 @@ describe('WebpackTranspiler', () => {
   });
 
   it('should call the webpackCompiler.emit function to get the new bundled files', async () => {
+    webpackTranspiler = new WebpackTranspiler({ config, keepSourceMaps: false });
     await webpackTranspiler.transpile([]);
 
     expect(webpackCompilerStub.emit).called;
@@ -70,6 +72,7 @@ describe('WebpackTranspiler', () => {
   });
 
   it('should return a successResult with the bundled files on success', async () => {
+    webpackTranspiler = new WebpackTranspiler({ config, keepSourceMaps: false });
     const transpileResult = await webpackTranspiler.transpile([]);
 
     expect(transpileResult.error).to.be.null;
@@ -77,6 +80,7 @@ describe('WebpackTranspiler', () => {
   });
 
   it('should return a error result when an error occurred', async () => {
+    webpackTranspiler = new WebpackTranspiler({ config, keepSourceMaps: false });
     const fakeError = 'compiler could not compile input files';
     webpackCompilerStub.emit.throwsException(Error(fakeError));
 
@@ -87,6 +91,7 @@ describe('WebpackTranspiler', () => {
   });
 
   it('should throw a not implemented error when calling the getMappedLocation method', () => {
+    webpackTranspiler = new WebpackTranspiler({ config, keepSourceMaps: false });
     const position: Position = {
       line: 0,
       column: 0
@@ -101,8 +106,3 @@ describe('WebpackTranspiler', () => {
     expect(webpackTranspiler.getMappedLocation.bind(this, fileLocation)).to.throw(Error, 'Method not implemented.');
   });
 });
-
-interface WebpackCompilerStub {
-  writeFilesToFs: sinon.SinonStub;
-  emit: sinon.SinonStub;
-}
---
+++ packages/stryker-webpack-transpiler/test/unit/compiler/ConfigLoaderSpec.ts
@@ -0,0 +1,51 @@
+import * as path from 'path';
+import { expect } from 'chai';
+import ConfigLoader from '../../../src/compiler/ConfigLoader';
+import { Configuration, Compiler, Plugin } from 'webpack';
+import { createStrykerWebpackConfig } from '../../helpers/producers';
+
+class FooPlugin implements Plugin { foo = true; apply(compiler: Compiler): void { } }
+class ProgressPlugin implements Plugin { apply(compiler: Compiler): void { } }
+class BarPlugin implements Plugin { bar = true; apply(compiler: Compiler): void { } }
+
+describe('ConfigLoader', () => {
+  let sut: ConfigLoader;
+  let requireStub: sinon.SinonStub;
+
+  beforeEach(() => {
+    requireStub = sandbox.stub();
+    sut = new ConfigLoader(requireStub);
+  });
+
+  it('should load webpack config from given location', () => {
+    requireStub.returns('resolved');
+    const result = sut.load(createStrykerWebpackConfig({ configFile: 'webpack.foo.config.js' }));
+    expect(result).eq('resolved');
+    expect(requireStub).calledWith(path.resolve('webpack.foo.config.js'));
+  });
+
+  it('should remove "ProgressPlugin" if silent is `true`', () => {
+    // Arrange
+    const bazPlugin = { baz: true, apply() { } };
+    const webpackConfig: Configuration = {
+      plugins: [new FooPlugin(), new ProgressPlugin(), new BarPlugin(), bazPlugin]
+    };
+    requireStub.returns(webpackConfig);
+
+    // Act
+    const result = sut.load(createStrykerWebpackConfig({ silent: true }));
+
+    // Assert
+    expect(result.plugins).deep.eq([new FooPlugin(), new BarPlugin(), bazPlugin]);
+    expect(logMock.debug).calledWith('Removing webpack plugin "%s" to keep webpack bundling silent. Set `webpack: { silent: false }` in your stryker.conf.js file to disable this feature.', 'ProgressPlugin');
+  });
+
+  it('should remove "ProgressPlugin" if silent is `false`', () => {
+    const webpackConfig: Configuration = {
+      plugins: [new ProgressPlugin(), new BarPlugin()]
+    };
+    requireStub.returns(webpackConfig);
+    const result = sut.load(createStrykerWebpackConfig({ silent: false }));
+    expect(result.plugins).deep.eq([new ProgressPlugin(), new BarPlugin()]);
+  });
+});
