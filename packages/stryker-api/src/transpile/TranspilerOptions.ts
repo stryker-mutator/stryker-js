bef60258c2423f16eec6e45e69ecfb607f1099f0
Nico Jansen <jansennico@gmail.com>
2018-01-20 00:14:12 +0100

    docs(stryker-api): Update jsdoc of `produceSourceMaps`

--- packages/stryker-api/src/transpile/TranspilerOptions.ts
+++ packages/stryker-api/src/transpile/TranspilerOptions.ts
@@ -7,7 +7,7 @@ export default interface TranspilerOptions {
   config: Config;
 
   /**
-   * Indicates whether or not the source maps need to be kept.
+   * Indicates whether or not the source maps need to be produced as part of the transpiler output.
    * If false, the transpiler may optimize to not calculate source maps.
    */
   produceSourceMaps: boolean;
