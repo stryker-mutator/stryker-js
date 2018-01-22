20215f878da40907a92866f850e162e3c9d4d8ca
Nico Jansen <jansennico@gmail.com>
2018-01-22 09:28:03 +0100

    refactor(webpack-transpiler): Explicit public functions for fs classes

--- packages/stryker-webpack-transpiler/src/fs/InputFileSystem.ts
+++ packages/stryker-webpack-transpiler/src/fs/InputFileSystem.ts
@@ -10,7 +10,7 @@ export default class InputFileSystem implements webpack.InputFileSystem {
   private log = getLogger(InputFileSystem.name);
   private memoryFS = new MemoryFS();
 
-  writeFileSync(name: string, content: string | Buffer) {
+  public writeFileSync(name: string, content: string | Buffer) {
     if (content === '') {
       // The in-memory fs doesn't like empty strings.
       content = ' ';
@@ -18,7 +18,7 @@ export default class InputFileSystem implements webpack.InputFileSystem {
     return this.memoryFS.writeFileSync(name, content);
   }
 
-  stat(path: string, callback: Callback<fs.Stats>): void {
+  public stat(path: string, callback: Callback<fs.Stats>): void {
     this.memoryFS.stat(path, (err: Error, stats: any) => {
       if (err) {
         this.pullIntoMemory(path, undefined, (err: Error) => {
@@ -35,10 +35,10 @@ export default class InputFileSystem implements webpack.InputFileSystem {
     });
   }
 
-  mkdirpSync(path: string) {
+  public mkdirpSync(path: string) {
     this.memoryFS.mkdirpSync(path);
   }
-  readdir(path: string, callback: Callback<string[]>): void {
+  public readdir(path: string, callback: Callback<string[]>): void {
     this.memoryFS.readdir(path, (err: Error, file: string[]) => {
       if (err) {
         fs.readdir(path, callback);
@@ -49,20 +49,7 @@ export default class InputFileSystem implements webpack.InputFileSystem {
     });
   }
 
-  private pullIntoMemory(path: any, optArg: any, callback: any) {
-    fs.readFile(path, optArg, (err: Error, content: string) => {
-      if (err) {
-        callback(err);
-      } else {
-        this.log.debug('Pulling file into memory: %s', path);
-        this.mkdirpSync(dirname(path));
-        this.writeFileSync(path, content);
-        callback(null, content);
-      }
-    });
-  }
-
-  readFile(path: any, optArg: any, callback?: any) {
+  public readFile(path: any, optArg: any, callback?: any) {
     if (!callback) {
       callback = optArg;
       optArg = undefined;
@@ -78,7 +65,7 @@ export default class InputFileSystem implements webpack.InputFileSystem {
     });
   }
 
-  readlink(path: any, callback?: any) {
+  public readlink(path: any, callback?: any) {
     this.memoryFS.readlink(path, (err: Error, file: string) => {
       if (err) {
         fs.readlink(path, callback);
@@ -89,14 +76,14 @@ export default class InputFileSystem implements webpack.InputFileSystem {
     });
   }
 
-  statSync(path: string): webpack.FileStats {
+  public statSync(path: string): webpack.FileStats {
     try {
       return this.memoryFS.statSync(path);
     } catch (err) {
       return fs.statSync(path);
     }
   }
-  readdirSync(path: string): string[] {
+  public readdirSync(path: string): string[] {
     try {
       return this.memoryFS.readdirSync(path);
     } catch (err) {
@@ -108,7 +95,7 @@ export default class InputFileSystem implements webpack.InputFileSystem {
       }
     }
   }
-  readFileSync(path: string, encoding: string) {
+  public readFileSync(path: string, encoding: string) {
     try {
       return this.memoryFS.readFileSync(path, encoding);
     } catch (err) {
@@ -121,6 +108,18 @@ export default class InputFileSystem implements webpack.InputFileSystem {
     }
   }
 
-  readlinkSync = this.memoryFS.readlinkSync.bind(this.memoryFS);
+  public readlinkSync = this.memoryFS.readlinkSync.bind(this.memoryFS);
 
+  private pullIntoMemory(path: any, optArg: any, callback: any) {
+    fs.readFile(path, optArg, (err: Error, content: string) => {
+      if (err) {
+        callback(err);
+      } else {
+        this.log.debug('Pulling file into memory: %s', path);
+        this.mkdirpSync(dirname(path));
+        this.writeFileSync(path, content);
+        callback(null, content);
+      }
+    });
+  }
 }
