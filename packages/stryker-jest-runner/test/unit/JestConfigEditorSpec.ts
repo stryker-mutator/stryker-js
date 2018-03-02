d29e3499dc9c4b8fc1fdd3a5a73bb4f01401af40
Sander Koenders <sanderkoenders@gmail.com>
2018-03-02 09:03:28 +0100

    fix(bail): disable bail (#36)
    This ensures the jest process does not quit with a non-zero exit code when a test fails.

--- packages/stryker-jest-runner/test/unit/JestConfigEditorSpec.ts
+++ packages/stryker-jest-runner/test/unit/JestConfigEditorSpec.ts
@@ -55,7 +55,7 @@ describe('JestConfigEditor', () => {
       testResultsProcessor: undefined,
       collectCoverage: false,
       verbose: false,
-      bail: true
+      bail: false
     });
   });
 
