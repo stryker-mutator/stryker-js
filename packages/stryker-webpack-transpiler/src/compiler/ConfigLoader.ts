bce687472d88fcef573a9767678b85a16a9aad71
Sander Koenders <sanderkoenders@gmail.com>
2018-03-09 15:59:51 +0100

    Feat(context): Return object with context for zero config.

--- packages/stryker-webpack-transpiler/src/compiler/ConfigLoader.ts
+++ packages/stryker-webpack-transpiler/src/compiler/ConfigLoader.ts
@@ -6,16 +6,26 @@ import { getLogger } from 'log4js';
 const PROGRESS_PLUGIN_NAME = 'ProgressPlugin';
 
 export default class ConfigLoader {
-  private log = getLogger(ConfigLoader.name);
+  private _log = getLogger(ConfigLoader.name);
+  private _loader: NodeRequireFunction;
 
-  public constructor(private loader: NodeRequireFunction = require) {
+  public constructor(loader?: NodeRequireFunction) {
+    this._loader = loader || require;
   }
 
   public load(config: StrykerWebpackConfig): Configuration {
-    const webpackConfig: Configuration = this.loader(path.resolve(config.configFile));
-    if (config.silent) {
-      this.configureSilent(webpackConfig);
+    let webpackConfig: Configuration;
+
+    try {
+      webpackConfig = this._loader(path.resolve(config.configFile));
+      if (config.silent) {
+        this.configureSilent(webpackConfig);
+      }
+    } catch (err) {
+      this._log.debug(`Webpack config "${config.configFile}" not found, trying Webpack 4 zero config`);
+      webpackConfig = { context: config.context };
     }
+
     return webpackConfig;
   }
 
@@ -23,7 +33,7 @@ export default class ConfigLoader {
     if (webpackConfig.plugins) {
       webpackConfig.plugins = webpackConfig.plugins.filter(plugin => {
         if (plugin.constructor && plugin.constructor.name === PROGRESS_PLUGIN_NAME) {
-          this.log.debug('Removing webpack plugin "%s" to keep webpack bundling silent. Set `webpack: { silent: false }` in your stryker.conf.js file to disable this feature.', PROGRESS_PLUGIN_NAME);
+          this._log.debug('Removing webpack plugin "%s" to keep webpack bundling silent. Set `webpack: { silent: false }` in your stryker.conf.js file to disable this feature.', PROGRESS_PLUGIN_NAME);
           return false;
         } else {
           return true;
