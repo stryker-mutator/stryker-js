93cfa848633de0fbadd5921f23819a8253152716
Sander Koenders <sanderkoenders@gmail.com>
2018-03-09 15:58:52 +0100

    Feat(context): Add context property for tests
    Should be removed when https://github.com/stryker-mutator/stryker/issues/650 is implemented

--- packages/stryker-webpack-transpiler/src/WebpackTranspiler.ts
+++ packages/stryker-webpack-transpiler/src/WebpackTranspiler.ts
@@ -4,7 +4,7 @@ import WebpackCompiler from './compiler/WebpackCompiler';
 import ConfigLoader from './compiler/ConfigLoader';
 import { setGlobalLogLevel } from 'log4js';
 
-const DEFAULT_STRYKER_WEBPACK_CONFIG = Object.freeze({ configFile: 'webpack.config.js', silent: true });
+const DEFAULT_STRYKER_WEBPACK_CONFIG = Object.freeze({ configFile: 'webpack.config.js', silent: true, context: process.cwd() });
 
 export default class WebpackTranspiler implements Transpiler {
   private config: StrykerWebpackConfig;
@@ -55,4 +55,7 @@ export default class WebpackTranspiler implements Transpiler {
 export interface StrykerWebpackConfig {
   configFile: string;
   silent: boolean;
+
+  // TODO: Remove this when stryker implements projectRoot, see https://github.com/stryker-mutator/stryker/issues/650 */
+  context?: string;
 }
