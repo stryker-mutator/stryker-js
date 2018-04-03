3d99d900eb127456000af1af3ae3b5161f37cf79
Nico Jansen <jansennico@gmail.com>
2018-04-03 07:11:06 +0200

    feat(Transpiler api): change return type of `transpile` to a file array
    A transpiler had 2 ways of communicating an error:
    1. Return an error
    2. Reject the promise.
    
    Yet stryker should handle both errors the same way.
    This is why the first method is removed.
    
    BREACKING CHANGE: transpiler api is updated. Any existing
    transpilers should update their apis:
    
    ```ts
    class MyTranspiler {
       transpile(files: ReadonlyArray<File>) {
          return Promise.resolve({
             outputFiles: files,
             error: null
          });
       }
    }
    ```
    
    Becomes:
    
    ```ts
    class MyTranspiler {
       transpile(files: ReadonlyArray<File>) {
          return Promise.resolve(files);
       }
    }
    ```
    
    If you want to communicate a transpile error, just reject the promise.

--- packages/stryker-api/transpile.ts
+++ packages/stryker-api/transpile.ts
@@ -1,4 +1,3 @@
 export { default as Transpiler } from './src/transpile/Transpiler';
-export { default as TranspileResult } from './src/transpile/TranspileResult';
 export { default as TranspilerFactory } from './src/transpile/TranspilerFactory';
 export { default as TranspilerOptions } from './src/transpile/TranspilerOptions';
