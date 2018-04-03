902038909e8e504e72085318a1467e31e3bfa59b
Nico Jansen <jansennico@gmail.com>
2018-04-03 07:36:37 +0200

    feat(File api): support new `File` format

--- packages/stryker-javascript-mutator/test/unit/JavaScriptMutatorSpec.ts
+++ packages/stryker-javascript-mutator/test/unit/JavaScriptMutatorSpec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { File, FileKind } from 'stryker-api/core';
+import { File } from 'stryker-api/core';
 import JavaScriptMutator from '../../src/JavaScriptMutator';
 import '../../src/index';
 import { Config } from 'stryker-api/config';
@@ -8,16 +8,7 @@ describe('JavaScriptMutator', () => {
 
   it('should generate a correct mutant', () => {
     const mutator = new JavaScriptMutator(new Config());
-    const files: File[] = [
-      {
-        name: 'testFile.js',
-        included: false,
-        mutated: true,
-        transpiled: false,
-        kind: FileKind.Text,
-        content: '"use strict"; var a = 1 + 2;'
-      }
-    ];
+    const files: File[] = [new File('testFile.js', '"use strict"; var a = 1 + 2;')];
 
     const mutants = mutator.mutate(files);
 
@@ -32,14 +23,7 @@ describe('JavaScriptMutator', () => {
 
   it('should generate mutant a correct mutant for jsx code', () => {
     const mutator = new JavaScriptMutator(new Config());
-    const files: File[] = [
-      {
-        name: 'testFile.jsx',
-        included: false,
-        mutated: true,
-        transpiled: false,
-        kind: FileKind.Text,
-        content: `
+    const files: File[] = [new File('testFile.jsx', `
           "use strict";
           import React from 'react'
           import { render } from 'react-dom'
@@ -54,9 +38,7 @@ describe('JavaScriptMutator', () => {
             <App message="Hello!" />,
             document.getElementById('appContainer')
           )
-        `
-      }
-    ];
+        `)];
 
     const mutants = mutator.mutate(files);
 
@@ -71,26 +53,16 @@ describe('JavaScriptMutator', () => {
 
   it('should not mutate unknown extensions', () => {
     const mutator = new JavaScriptMutator(new Config());
-    const files: File[] = [
-      {
-        name: 'testFile.html',
-        included: false,
-        mutated: true,
-        transpiled: false,
-        kind: FileKind.Text,
-        content: `
-          <html>
-            <head>
-              <title>Test</title>
-            </head>
-            <body>
-              <h1>Hello World</h1>
-            </body>
-          </html>
-        `
-      }
-    ];
-
+    const files: File[] = [new File('testFile.html', `
+      <html>
+        <head>
+          <title>Test</title>
+        </head>
+        <body>
+          <h1>Hello World</h1>
+        </body>
+      </html>
+    `)];
     const mutants = mutator.mutate(files);
 
     expect(mutants.length).to.equal(0);
@@ -98,14 +70,7 @@ describe('JavaScriptMutator', () => {
 
   it('should generate mutants for flow code', () => {
     const mutator = new JavaScriptMutator(new Config());
-    const files: File[] = [
-      {
-        name: 'testFile.js',
-        included: false,
-        mutated: true,
-        transpiled: false,
-        kind: FileKind.Text,
-        content: `
+    const files: File[] = [new File('testFile.js', `
           // @flow
           import React from 'react'
           
@@ -126,9 +91,7 @@ describe('JavaScriptMutator', () => {
           }
           
           export default App
-        `
-      }
-    ];
+        `)];
 
     const mutants = mutator.mutate(files);
 
@@ -143,14 +106,7 @@ describe('JavaScriptMutator', () => {
 
   it('should generate mutants for js vnext code', () => {
     const sut = new JavaScriptMutator(new Config());
-    const files: File[] = [
-      {
-        name: 'testFile.js',
-        included: false,
-        mutated: true,
-        transpiled: false,
-        kind: FileKind.Text,
-        content: `
+    const files: File[] = [new File('testFile.js', `
           function objectRestSpread(input) {
             return {
               ...input,
@@ -168,54 +124,18 @@ describe('JavaScriptMutator', () => {
           function dynamicImport(){
             import('./guy').then(a)
           }
-        `
-      }
-    ];
+        `)];
 
     const mutants = sut.mutate(files);
     expect(mutants).lengthOf.above(2);
   });
 
   it('should generate mutants for multiple files', () => {
-    let mutator = new JavaScriptMutator(new Config());
-    let file: File = {
-      name: 'testFile.js',
-      included: false,
-      mutated: true,
-      transpiled: false,
-      kind: FileKind.Text,
-      content: '"use strict"; var a = 1 + 2;'
-    };
-
-    let mutants = mutator.mutate([file, file]);
+    const mutator = new JavaScriptMutator(new Config());
+    const file: File = new File('testFile.js', '"use strict"; var a = 1 + 2;');
+    const file2: File = new File('testFile2.js', '"use strict"; var a = 1 + 2;');
+    const mutants = mutator.mutate([file, file2]);
 
     expect(mutants.length).to.equal(2);
-  });
-
-  it('should not mutate files with mutate: false', () => {
-    let mutator = new JavaScriptMutator(new Config());
-    let files: File[] = [
-      {
-        name: 'testFile.js',
-        included: false,
-        mutated: false,
-        transpiled: false,
-        kind: FileKind.Text,
-        content: '"use strict"; var a = 1 + 2;'
-      },
-      {
-        name: 'testFile2.js',
-        included: false,
-        mutated: true,
-        transpiled: false,
-        kind: FileKind.Text,
-        content: '"use strict"; var a = 1 + 2;'
-      }
-    ];
-
-    let mutants = mutator.mutate(files);
-
-    expect(mutants.length).to.equal(1);
-    expect(mutants[0].fileName).to.equal('testFile2.js');
   });
 });
