6c5626f249e992d2f3cc29b7d9f85c35846799f7
Simon de Lang <simondelang@gmail.com>
2018-04-10 06:06:29 +0200

    test: support Mocha 5

--- packages/stryker-jasmine/test/unit/JasmineTestFrameworkSpec.ts
+++ packages/stryker-jasmine/test/unit/JasmineTestFrameworkSpec.ts
@@ -3,22 +3,27 @@ import JasmineTestFramework from '../../src/JasmineTestFramework';
 
 describe('JasmineTestFramework', () => {
   let sut: JasmineTestFramework;
-  beforeEach(() => sut = new JasmineTestFramework());
+  beforeEach(() => { 
+    sut= new JasmineTestFramework();
+  });
 
   describe('beforeEach()', () => {
-    it('should result in a specStarted reporter hook', () =>
-      expect(sut.beforeEach('fragment')).to.contain('fragment').and.to.contain('specStarted: function () {'));
+    it('should result in a specStarted reporter hook', () => {
+      expect(sut.beforeEach('fragment')).to.contain('fragment').and.to.contain('specStarted: function () {');
+    });
   });
 
   describe('afterEach()', () => {
-    it('should result in an specDone reporter hook', () =>
-      expect(sut.afterEach('fragment')).to.contain('fragment').and.to.contain('specDone: function () {'));
+    it('should result in an specDone reporter hook', () => {
+      expect(sut.afterEach('fragment')).to.contain('fragment').and.to.contain('specDone: function () {');
+    });
   });
 
   describe('filter()', () => {
-    it('should result in a specFilter of jasmine it\'s', () =>
+    it('should result in a specFilter of jasmine it\'s', () => {
       expect(sut.filter([{ id: 5, name: 'test five' }, { id: 8, name: 'test eight' }]))
         .to.contain('jasmine.getEnv().specFilter = function (spec)')
-        .and.to.contain('return [5,8].indexOf(currentTestId++) !== -1;'));
+        .and.to.contain('return [5,8].indexOf(currentTestId++) !== -1;');
+    });
   });
 });
