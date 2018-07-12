f0a7e5a8fd9c39afe4c810fd844e7397f68f2b65
Nico Jansen <jansennico@gmail.com>
2018-07-12 07:30:35 +0200

    fix(Dependencies): Pin all deps on minor version (#974)
    This PR pins all _dependencies_ and _devDependencies_ on minor versions. I also consolidated all commonly used dependencies to most recent (supported) versions. Namely:
    
    * log4js@~1.1.0 (we're upgrading to 2.x with #954)
    * semver@~5.5.0
    * tslib@~1.9.3
    
    Fixes #967

--- packages/stryker/src/globals.ts
+++ packages/stryker/src/globals.ts
@@ -9,3 +9,24 @@ interface Event { }
 interface MessageEvent { }
 interface CloseEvent { }
 interface WebSocket { }
+declare namespace NodeJS {
+  type MessageListener = (message: any, sendHandle: any) => void;
+  type UncaughtExceptionListener = (error: Error) => void;
+  type UnhandledRejectionListener = (reason: any, promise: Promise<any>) => void;
+  type RejectionHandledListener = (promise: Promise<any>) => void;
+  export interface Process extends EventEmitter {
+    addListener(event: 'uncaughtException', listener: UncaughtExceptionListener): this;
+    addListener(event: 'unhandledRejection', listener: UnhandledRejectionListener): this;
+    addListener(event: 'rejectionHandled', listener: RejectionHandledListener): this;
+    emit(event: 'uncaughtException', error: Error): boolean;
+    emit(event: 'unhandledRejection', reason: any, promise: Promise<any>): boolean;
+    emit(event: 'rejectionHandled', promise: Promise<any>): boolean;
+    on(event: string, listener: (...args: any[]) => void): this;
+    on(event: 'uncaughtException', listener: UncaughtExceptionListener): this;
+    on(event: 'unhandledRejection', listener: UnhandledRejectionListener): this;
+    on(event: 'rejectionHandled', listener: RejectionHandledListener): this;
+  }
+}
+declare module 'fs' {
+  export function createWriteStream(path: string, options?: string): WriteStream;
+}
