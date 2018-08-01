71154e8c319d8c1a44dcaa1efe11ab626eac4e71
Nico Jansen <jansennico@gmail.com>
2018-08-01 18:41:06 +0200

    test(string builder): Implement unit tests

--- packages/stryker/src/utils/StringBuilder.ts
+++ packages/stryker/src/utils/StringBuilder.ts
@@ -1,14 +1,16 @@
 import * as os from 'os';
 
+const DEFAULT_MAX_SIZE = 2048;
+
 export default class StringBuilder {
-  private maxSize = 2048;
   private currentLength = 0;
   private strings: string[] = [];
+  private readonly maxSize = DEFAULT_MAX_SIZE;
 
   append(str: string) {
     this.strings.push(str);
     this.currentLength += str.length;
-    while (this.currentLength > this.maxSize && this.strings.length > 0) {
+    while (this.currentLength > this.maxSize && this.strings.length > 1) {
       const popped = this.strings.shift() as string;
       this.currentLength -= popped.length;
     }
---
+++ packages/stryker/test/unit/utils/StringBuilderSpec.ts
@@ -0,0 +1,33 @@
+import { expect } from 'chai';
+import { EOL } from 'os';
+import StringBuilder from '../../../src/utils/StringBuilder';
+
+describe.only(StringBuilder.name, () => {
+
+  it('should append strings with new lines when `toString()` is called', () => {
+    const sut = new StringBuilder();
+    sut.append('1');
+    sut.append('2');
+    sut.append('3');
+    expect(sut.toString()).eq(`1${EOL}2${EOL}3`);
+  });
+
+  const DEFAULT_MAX_CHARACTERS = 2048;
+  it(`should append a to maximum of ${DEFAULT_MAX_CHARACTERS} characters by default`, () => {
+    const sut = new StringBuilder();
+    for (let i = 0; i < DEFAULT_MAX_CHARACTERS; i++) {
+      sut.append('.');
+    }
+    sut.append('expected');
+    const result = sut.toString();
+    expect(result.replace(new RegExp(EOL, 'g'), '')).lengthOf(DEFAULT_MAX_CHARACTERS);
+    expect(result.endsWith(`.${EOL}.${EOL}.${EOL}expected`)).ok;
+  });
+
+  it('should not split the last string, even if it exceeds the max characters', () => {
+    const input = new Array(DEFAULT_MAX_CHARACTERS + 1).fill('.').join('');
+    const sut = new StringBuilder();
+    sut.append(input);
+    expect(sut.toString()).eq(input);
+  });
+});
