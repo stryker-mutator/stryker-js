4ca71c1e1e23501199fa237726d6d9db839aaaa9
Simon de Lang <simondelang@gmail.com>
2018-08-03 18:40:26 +0200

    test(Stryker-webpack-transpiler): mutate all files

--- packages/stryker-webpack-transpiler/stryker.conf.js
+++ packages/stryker-webpack-transpiler/stryker.conf.js
@@ -1,7 +1,7 @@
 module.exports = function (config) {
   config.set({
     mutate: [
-      'src/WebpackTranspiler.ts',
+      'src/**/*.ts',
       '!src/**/*.d.ts',
       '!src/enhanced-resolve.ts',
       '!src/index.ts'
