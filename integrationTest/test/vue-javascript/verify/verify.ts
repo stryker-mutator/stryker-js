122209b6df1d42cb0872917f3bd469ff9d2409f2
Simon de Lang <simondelang@gmail.com>
2018-08-04 22:34:48 +0200

    test(integration): Check if the correct mutation score is generated

--- integrationTest/test/vue-javascript/verify/verify.ts
+++ integrationTest/test/vue-javascript/verify/verify.ts
@@ -1,35 +1,19 @@
-import * as chai from 'chai';
 import * as fs from 'mz/fs';
-import * as chaiAsPromised from 'chai-as-promised';
-chai.use(chaiAsPromised);
-const expect = chai.expect;
-const expectFileExists = (path: string) => expect(fs.exists(path), `File ${path} does not exist`).to.eventually.eq(true);
+import { expect } from 'chai';
+import * as path from 'path';
+import { ScoreResult } from 'stryker-api/report';
 
-describe('Verify stryker has ran correctly', () => {
+describe('After running stryker on VueJS project', () => {
+  it('should report 20% mutation score', async () => {
+    const eventsDir = path.resolve(__dirname, '..', 'reports', 'mutation', 'events');
+    const allReportFiles = await fs.readdir(eventsDir);
+    const scoreResultReportFile = allReportFiles.find(file => !!file.match(/.*onScoreCalculated.*/));
+    expect(scoreResultReportFile).ok;
+    const scoreResultContent = await fs.readFile(path.resolve(eventsDir, scoreResultReportFile || ''), 'utf8');
+    const scoreResult = JSON.parse(scoreResultContent) as ScoreResult;
+    expect(scoreResult.killed).eq(2);
+    expect(scoreResult.survived).eq(8);
 
-  describe('html reporter', () => {
-
-    it('should report in html files', () => {
-      return Promise.all([
-        expectFileExists('reports/mutation/html/App.vue.html'),
-        expectFileExists('reports/mutation/html/HelloWorld.vue.html'),
-        expectFileExists('reports/mutation/html/router/index.js.html'),
-        expectFileExists('reports/mutation/html/main.js.html'),
-        expectFileExists('reports/mutation/html/index.html')
-      ]);
-    });
-
-    it('should copy over the resources', () => {
-      return Promise.all([
-        expectFileExists('reports/mutation/html/strykerResources/stryker.css'),
-        expectFileExists('reports/mutation/html/strykerResources/stryker.js'),
-        expectFileExists('reports/mutation/html/strykerResources/stryker-80x80.png'),
-        expectFileExists('reports/mutation/html/strykerResources/bootstrap/css/bootstrap.min.css'),
-        expectFileExists('reports/mutation/html/strykerResources/bootstrap/js/bootstrap.min.js'),
-        expectFileExists('reports/mutation/html/strykerResources/highlightjs/styles/default.css')
-      ]);
-    });
+    expect(scoreResult.mutationScore).to.equal(20);
   });
-
-
 });
