5302cfe5290157a8858b7f15ae0c59a51a2ef753
Tom Milligan <tommilligan@users.noreply.github.com>
2018-09-06 21:37:58 +0100

    Refactored js implementation

--- packages/stryker-javascript-mutator/src/mutators/SwitchStatementMutator.ts
+++ packages/stryker-javascript-mutator/src/mutators/SwitchStatementMutator.ts
@@ -3,6 +3,20 @@ import { types } from 'babel-core';
 import NodeMutator from './NodeMutator';
 
 /**
+ * Given an array of length n, return an array length n of arrays length n-1
+ * where each item has been removed in sequence
+ * 
+ * e.g. [0, 1, 2] -> [[1, 2], [0, 2], [0, 1]]
+ */
+function sequentialSplices<T>(collection: T[]): T[][] {
+  return collection.map(function (_, i) {
+    return collection.filter(function (_, j) {
+      return i !== j;
+    })
+  })
+}
+
+/**
  * Represents a mutator which can remove individual clauses from switch statements.
  */
 export default class SwitchStatementMutator implements NodeMutator {
@@ -10,14 +24,14 @@ export default class SwitchStatementMutator implements NodeMutator {
 
   public mutate(node: types.Node, copy: <T extends types.Node>(obj: T, deep?: boolean) => T): types.Node[] | void {
     if (types.isSwitchStatement(node)) {
-      const mutatedNodes = node.cases.map(function (_, i) {
+      // Generate possible case arrays
+      const caseSplices = sequentialSplices(node.cases);
+      // Map onto cloned nodes
+      return caseSplices.map(function (caseSplice) {
         const mutatedNode = copy(node);
-        mutatedNode.cases = node.cases.filter(function (_, j) {
-          return i !== j;
-        });
+        mutatedNode.cases = caseSplice;
         return mutatedNode;
-      });
-      return mutatedNodes;
+      })
     }
   }
 }
