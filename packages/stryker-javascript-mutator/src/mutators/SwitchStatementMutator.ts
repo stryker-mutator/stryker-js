2220a83c48da60eb1309b859ba0b017faaa38e0a
Tom Milligan <tommilligan@users.noreply.github.com>
2018-09-06 21:26:37 +0100

    Added js implementation

--- packages/stryker-javascript-mutator/src/mutators/SwitchStatementMutator.ts
+++ packages/stryker-javascript-mutator/src/mutators/SwitchStatementMutator.ts
@@ -1,29 +1,23 @@
 import { types } from 'babel-core';
-import { partition } from 'lodash';
 
 import NodeMutator from './NodeMutator';
 
 /**
- * Returns true if a switch case statement is the default one
- */
-function isDefaultSwitchCase(switchCase: types.SwitchCase): boolean {
-  return switchCase.test === null;
-}
-
-/**
- * Represents a mutator which can remove the conditional clause from statements.
+ * Represents a mutator which can remove individual clauses from switch statements.
  */
 export default class SwitchStatementMutator implements NodeMutator {
   public name = 'SwitchStatement';
 
   public mutate(node: types.Node, copy: <T extends types.Node>(obj: T, deep?: boolean) => T): types.Node[] | void {
     if (types.isSwitchStatement(node)) {
-      const [defaultCases, nonDefaultCases] = partition(node.cases, isDefaultSwitchCase);
-      if (nonDefaultCases.length > 0) {
+      const mutatedNodes = node.cases.map(function (_, i) {
         const mutatedNode = copy(node);
-        mutatedNode.cases = defaultCases;
-        return [mutatedNode];
-      }
+        mutatedNode.cases = node.cases.filter(function (_, j) {
+          return i !== j;
+        });
+        return mutatedNode;
+      });
+      return mutatedNodes;
     }
   }
 }
