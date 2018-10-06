b4203166b92143284ccc9e1cc6e370b721fc0618
Nico Jansen <jansennico@gmail.com>
2018-10-06 02:22:27 +0200

    Support `ObjectLiteralMutator` with babel 7

--- packages/stryker-javascript-mutator/test/unit/helpers/BabelParserSpec.ts
+++ packages/stryker-javascript-mutator/test/unit/helpers/BabelParserSpec.ts
@@ -1,13 +1,13 @@
 import { expect } from 'chai';
-import BabelParser from '../../../src/helpers/BabelHelper';
+import BabelHelper from '../../../src/helpers/BabelHelper';
 
 describe('BabelHelper', () => {
 
   describe('getNodes', () => {
     it('should get the correct amount of statements', () => {
-      const ast = BabelParser.parse('"use strict"; var a = 1 + 2;');
+      const ast = BabelHelper.parse('"use strict"; var a = 1 + 2;');
 
-      const nodes = BabelParser.getNodes(ast);
+      const nodes = BabelHelper.getNodes(ast);
 
       expect(nodes.length).to.equal(9);
     });
@@ -15,29 +15,29 @@ describe('BabelHelper', () => {
 
   describe('generateCode', () => {
     it('should work with "use strict"', () => {
-      const ast = BabelParser.parse('"use strict"; var a = 1 + 2;');
+      const ast = BabelHelper.parse('"use strict"; var a = 1 + 2;');
 
-      const result = BabelParser.generateCode(ast);
+      const result = BabelHelper.generateCode(ast);
 
       expect(result).to.equal('"use strict";\n\nvar a = 1 + 2;');
     });
 
     it('should keep comments', () => {
-      const ast = BabelParser.parse('var a = 1 + 2 /* Comment */;');
-      const result = BabelParser.generateCode(ast.program.body[0]);
+      const ast = BabelHelper.parse('var a = 1 + 2 /* Comment */;');
+      const result = BabelHelper.generateCode(ast.program.body[0]);
       expect(result).to.equal('var a = 1 + 2\n/* Comment */\n;');
     });
   });
 
   describe('parse', () => {
     it('should support scripts', () => {
-      const ast = BabelParser.parse(`var fs = require('fs'); var a = 1 + 2;`);
+      const ast = BabelHelper.parse(`var fs = require('fs'); var a = 1 + 2;`);
 
       expect(ast).exist;
     });
 
     it('should support modules', () => {
-      const ast = BabelParser.parse(`import fs from 'fs'; var a = 1 + 2;`);
+      const ast = BabelHelper.parse(`import fs from 'fs'; var a = 1 + 2;`);
 
       expect(ast).exist;
     });
