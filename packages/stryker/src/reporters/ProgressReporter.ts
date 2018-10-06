106f2ceb444db2109b4b644c0bddb4ca6d1dd486
Sander Koenders <sanderkoenders@gmail.com>
2018-10-06 14:18:00 +0200

    feat(ProgressBarReporter): add etc for progress to progressBarContent

--- packages/stryker/src/reporters/ProgressReporter.ts
+++ packages/stryker/src/reporters/ProgressReporter.ts
@@ -22,18 +22,21 @@ export default class ProgressBarReporter extends ProgressKeeper {
   public onMutantTested(result: MutantResult): void {
     const ticksBefore = this.progress.tested;
     super.onMutantTested(result);
+
+    const progressBarContent = Object.assign(this.progress, { etc: this.getEtc() });
+
     if (ticksBefore < this.progress.tested) {
-      this.tick();
+      this.tick(progressBarContent);
     } else {
-      this.render();
+      this.render(progressBarContent);
     }
   }
 
-  private tick(): void {
-    this.progressBar.tick(this.progress);
+  private tick(tickObj: object): void {
+    this.progressBar.tick(tickObj);
   }
 
-  private render(): void {
-    this.progressBar.render(this.progress);
+  private render(renderObj: object): void {
+    this.progressBar.render(renderObj);
   }
 }
