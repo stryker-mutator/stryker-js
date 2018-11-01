f7b581f0f70e58e2d4093e0b3c6ed02da1c056d1
Simon de Lang <simondelang@gmail.com>
2018-11-01 12:41:51 +0100

    Update testMatch

--- packages/stryker-jest-runner/test/integration/JestConfigEditorSpec.ts
+++ packages/stryker-jest-runner/test/integration/JestConfigEditorSpec.ts
@@ -53,8 +53,8 @@ describe('Integration test for Jest ConfigEditor', () => {
       setupTestFrameworkScriptFile: undefined,
       testEnvironment: 'jsdom',
       testMatch: [
-        '<rootDir>/src/**/__tests__/**/*.{js,jsx}',
-        '<rootDir>/src/**/?(*.)(spec|test).{js,jsx}'
+        '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+        '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}'
       ],
       testResultsProcessor: undefined,
       testURL: 'http://localhost',
