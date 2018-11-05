7dbcc0dba0a99189c37a1e4c9be946b6b522fdaa
Simon de Lang <simondelang@gmail.com>
2018-11-05 22:28:16 +0100

    Fix double quotes

--- packages/stryker-jest-runner/test/integration/JestConfigEditorSpec.ts
+++ packages/stryker-jest-runner/test/integration/JestConfigEditorSpec.ts
@@ -82,7 +82,7 @@ describe('Integration test for Jest ConfigEditor', () => {
       bail: false,
       collectCoverage: false,
       collectCoverageFrom: [
-        "!**/*.d.ts",
+        '!**/*.d.ts',
         'src/**/*.{js,jsx,ts,tsx}'
       ],
       globals: {
