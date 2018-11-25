f4a6052c8a8d73af79535a8df6e185d74801e461
Nico Jansen <jansennico@gmail.com>
2018-11-25 03:04:10 +0100

    Only log if verbose is true

--- packages/stryker-wct-runner/src/WctLogger.ts
+++ packages/stryker-wct-runner/src/WctLogger.ts
@@ -1,14 +1,18 @@
-import { EventEmitter } from 'events';
 import { LoggerFactory } from 'stryker-api/logging';
+import { Context } from 'web-component-tester/runner/context';
 
 export default class WctLogger {
 
   private readonly log = LoggerFactory.getLogger('web-component-tester');
 
-  constructor(emitter: EventEmitter) {
-    emitter.on('log:debug', this.log.debug.bind(this.log));
-    emitter.on('log:info', this.log.info.bind(this.log));
-    emitter.on('log:warn', this.log.warn.bind(this.log));
-    emitter.on('log:error', this.log.error.bind(this.log));
+  constructor(context: Context) {
+    if (context.options.verbose) {
+      context.on('log:debug', this.log.debug.bind(this.log));
+      context.on('log:info', this.log.info.bind(this.log));
+      context.on('log:warn', this.log.warn.bind(this.log));
+      context.on('log:error', this.log.error.bind(this.log));
+    } else {
+      this.log.debug('Keeping wct quiet. To enable wct logging, set `wct.verbose` to `true` in your stryker configuration file.');
+    }
   }
 }
