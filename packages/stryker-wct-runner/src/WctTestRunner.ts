bdb8ac053ad7a9a8058773af421d8530305bb304
Nico Jansen <jansennico@gmail.com>
2018-11-26 13:52:14 +0100

    Add some more logging to the wct test runner

--- packages/stryker-wct-runner/src/WctTestRunner.ts
+++ packages/stryker-wct-runner/src/WctTestRunner.ts
@@ -4,18 +4,25 @@ import { steps } from 'web-component-tester';
 import { StrykerOptions } from 'stryker-api/core';
 import WctReporter from './WctReporter';
 import WctLogger from './WctLogger';
+import { getLogger } from 'stryker-api/logging';
+const WCT_PACKAGE = 'web-component-tester';
 
 export default class WctTestRunner implements TestRunner {
 
   private readonly reporter: WctReporter;
   private readonly context: Context;
   private readonly logger: WctLogger;
+  private readonly log = getLogger(WctTestRunner.name);
 
   constructor(runnerOptions: { strykerOptions: StrykerOptions }) {
     if (runnerOptions.strykerOptions.coverageAnalysis !== 'off') {
       throw new Error(`Coverage analysis "${runnerOptions.strykerOptions.coverageAnalysis}" is not (yet) supported by the WCT test runner plugin. Please set \`coverageAnalysis: "off"\` in your stryker.conf.js file.`);
     }
+    this.log.debug('Running wct version %s from %s', require(`${WCT_PACKAGE}/package.json`).version, require.resolve(WCT_PACKAGE));
     this.context = new Context(runnerOptions.strykerOptions.wct);
+    if (this.log.isDebugEnabled()) {
+      this.log.debug(`WCT options: %s`, JSON.stringify(this.context.options));
+    }
     this.logger = new WctLogger(this.context, this.context.options.verbose || false);
     this.reporter = new WctReporter(this.context);
   }
