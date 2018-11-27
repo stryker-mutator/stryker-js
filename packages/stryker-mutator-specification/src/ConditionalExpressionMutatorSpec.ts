be4c990589ee5788cbf07fab4c0a57dddb0e6925
Bharanidharan Natesan <bharaninb@users.noreply.github.com>
2018-11-27 21:11:07 +0530

    feat(Conditional expression mutator): Mutate conditional operators (#1253)
    The `ConditionalExpressionMutator` now also mutates conditional expressions in the wild. Not only in `for`, `while` and `if` statements.
    
    For example: `const foo = bar < baz` will be mutated to `const foo = bar <= baz`. 
    
    Loops are still not mutated to obvious infinite loops (for example `while (i < 10) { i++ }` **isn't** mutated to `while (true) { i++ }`)

--- packages/stryker-mutator-specification/src/ConditionalExpressionMutatorSpec.ts
+++ packages/stryker-mutator-specification/src/ConditionalExpressionMutatorSpec.ts
@@ -9,8 +9,60 @@ export default function ConditionalExpressionMutatorSpec(name: string, expectMut
       expect(name).eq('ConditionalExpression');
     });
 
-    it('should replace conditional expressions', () => {
+    it('should mutate ternary operator', () => {
       expectMutation('a < 3? b : c', 'false? b : c', 'true? b : c');
+    });
+
+    it('should mutate < and >', () => {
+      expectMutation('a < b', 'true', 'false');
+      expectMutation('a > b', 'true', 'false');
+    });
+
+    it('should mutate <= and >=', () => {
+      expectMutation('a <= b', 'true', 'false');
+      expectMutation('a >= b', 'true', 'false');
+    });
+
+    it('should mutate == and ===', () => {
+      expectMutation('a == b', 'true', 'false');
+      expectMutation('a === b', 'true', 'false');
+    });
+
+    it('should mutate != and !==', () => {
+      expectMutation('a != b', 'true', 'false');
+      expectMutation('a !== b', 'true', 'false');
+    });
+
+    it('should mutate && and ||', () => {
+      expectMutation('a && b', 'true', 'false');
+      expectMutation('a || b', 'true', 'false');
+    });
+
+    it('should not mutate + and -', () => {
+      expectMutation('a + b');
+      expectMutation('a - b');
+    });
+
+    it('should not mutate *, % and /', () => {
+      expectMutation('a * b');
+      expectMutation('a / b');
+      expectMutation('a % b');
+    });
+
+    it('should not mutate `if statement`', () => {
+      expectMutation('if (a < 6) { a++ }');
+    });
+
+    it('should not mutate `for statement`', () => {
+      expectMutation('for(let i=0;i<10; i++) { console.log(); }');
+    });
+
+    it('should not mutate `while statement`', () => {
+      expectMutation('while(a < b) { console.log(); }');
+    });
+
+    it('should not mutate `do while statement`', () => {
+      expectMutation('do { console.log(); } while(a < b);');
     });
   });
 }
