5673e6b562e68ddb2e190dbb5092d696b0fcee53
Wmaarts <wouteraarts6@hotmail.com>
2018-11-27 12:30:19 +0100

    feat(Stryker CLI 'init'): Support for preset configuration during 'stryker init' (#1248)
    Add a question to the `init` command:
    
    ```
    Are you using one of these frameworks? Then select a preset configuration. (Use arrow keys)
    > angular-cli
      react
      vueJs
      ──────────────
      None/other
    ```
    
    If you choose a preset, preset specific questions may be asked. If all is well, Stryker will work out-of-the box.

---
+++ packages/stryker/src/initializer/presets/ReactPreset.ts
@@ -0,0 +1,60 @@
+import Preset from './Preset';
+import inquirer = require('inquirer');
+import PresetConfiguration from './PresetConfiguration';
+
+const handbookUrl = 'https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/react.md#react';
+
+/**
+ * More information can be found in the Stryker handbook:
+ * https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/react.md#react
+ */
+export class ReactPreset implements Preset {
+  public readonly name = 'react';
+  private readonly generalDependencies = [
+    'stryker',
+    'stryker-jest-runner',
+    'stryker-html-reporter'
+  ];
+
+  private readonly sharedConfig = `testRunner: 'jest',
+    reporters: ['progress', 'clear-text', 'html'],
+    coverageAnalysis: 'off',
+    jest: {
+      projectType: 'react'
+    }
+  `;
+
+  private readonly tsxDependencies = ['stryker-typescript', ...this.generalDependencies];
+  private readonly tsxConf = `{
+      mutate: ['src/**/*.ts?(x)', '!src/**/*@(.test|.spec|Spec).ts?(x)'],
+      mutator: 'typescript',
+      ${this.sharedConfig}
+    }`;
+
+  private readonly jsxDependencies = ['stryker-javascript-mutator', ...this.generalDependencies];
+  private readonly jsxConf = `{
+      mutate: ['src/**/*.js?(x)', '!src/**/*@(.test|.spec|Spec).js?(x)'],
+      mutator: 'javascript',
+      ${this.sharedConfig}
+    }`;
+
+  public async createConfig(): Promise<PresetConfiguration> {
+    const choices: inquirer.ChoiceType[] = ['JSX', 'TSX'];
+    const answers = await inquirer.prompt<{ choice: string }>({
+      choices,
+      message: 'Is your project a JSX project or a TSX project?',
+      name: 'choice',
+      type: 'list'
+    });
+    return this.load(answers.choice);
+  }
+  private load(choice: string): PresetConfiguration {
+    if (choice === 'JSX') {
+      return { config: this.jsxConf, handbookUrl, dependencies: this.jsxDependencies };
+    } else if (choice === 'TSX') {
+      return { config: this.tsxConf, handbookUrl, dependencies: this.tsxDependencies };
+    } else {
+      throw new Error(`Invalid project type ${choice}`);
+    }
+  }
+}
