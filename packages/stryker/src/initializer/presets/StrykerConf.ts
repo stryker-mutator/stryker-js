0715195d46ee00e218ef528db9bdeecd1e2f1480
Nico Jansen <jansennico@gmail.com>
2018-11-27 12:06:34 +0100

    Rename some things
    * `StrykerPreset` -> `Preset`
    * `StrykerPresetConf` -> `PresetConfiguration`

--- packages/stryker/src/initializer/presets/StrykerConf.ts
+++
@@ -1,6 +0,0 @@
-export class StrykerPresetConfig {
-    constructor(
-        public config: string,
-        public dependencies: string[]) {
-        }
-}
--- packages/stryker/src/initializer/presets/StrykerPreset.ts
+++
@@ -1,7 +0,0 @@
-import { StrykerPresetConfig } from './StrykerConf';
-
-abstract class StrykerPreset {
-  public abstract readonly name: string;
-  public abstract async createConfig(): Promise<StrykerPresetConfig>;
-}
-export default StrykerPreset;
