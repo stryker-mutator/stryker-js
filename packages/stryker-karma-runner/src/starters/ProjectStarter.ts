92b59e2cb199dd52ab356664c1267c1aae90ef11
Yoeri Van Damme <Yoeri.VanDamme@sdworx.com>
2018-12-07 15:20:00 +0100

    linting

--- packages/stryker-karma-runner/src/starters/ProjectStarter.ts
+++ packages/stryker-karma-runner/src/starters/ProjectStarter.ts
@@ -3,8 +3,8 @@ import * as angularStarter from './angularStarter';
 import * as karmaStarter from './karmaStarter';
 
 export default class ProjectStarter {
-  constructor(private setup: StrykerKarmaSetup) {}
-  start() {
+  constructor(private readonly setup: StrykerKarmaSetup) {}
+  public start() {
     if (this.setup.projectType === 'angular-cli') {
       return angularStarter.start(this.setup.ngConfig);
     } else {
--- packages/stryker-karma-runner/src/starters/angularStarter.ts
+++ packages/stryker-karma-runner/src/starters/angularStarter.ts
@@ -12,25 +12,35 @@ export async function start(ngConfig?: NgConfigOptions): Promise<void> {
   // Make sure require angular cli from inside this function, that way it won't break if angular isn't installed and this file is required.
   const version = semver.coerce(requireModule('@angular/cli/package').version);
   if (!version || semver.lt(version, MIN_ANGULAR_CLI_VERSION)) {
-    throw new Error(`Your @angular/cli version (${version}) is not supported. Please install ${MIN_ANGULAR_CLI_VERSION} or higher`);
+    throw new Error(
+      `Your @angular/cli version (${version}) is not supported. Please install ${MIN_ANGULAR_CLI_VERSION} or higher`
+    );
   }
   let cli = requireModule('@angular/cli');
   if ('default' in cli) {
     cli = cli.default;
   }
-  const cliArgs = ['test', '--progress=false', `--karma-config=${require.resolve('./stryker-karma.conf')}`];
+  const cliArgs = [
+    'test',
+    '--progress=false',
+    `--karma-config=${require.resolve('./stryker-karma.conf')}`
+  ];
   if (ngConfig && ngConfig.testArguments) {
     const testArguments: NgTestArguments = ngConfig.testArguments;
     Object.keys(testArguments).forEach(key => {
-      let decamelizedKey = decamelize(key);
-      if ('progress' !== key && 'karma-config' !== decamelizedKey && !decamelizedKey.trim().startsWith('-')) {
+      const decamelizedKey = decamelize(key);
+      if (
+        'progress' !== key &&
+        'karma-config' !== decamelizedKey &&
+        !decamelizedKey.trim().startsWith('-')
+      ) {
         cliArgs.push(`--${decamelizedKey}=${testArguments[key]}`);
       }
     });
   }
   logger.debug('Starting Angular tests: ng', cliArgs.join(' '));
   return cli({
-    cliArgs: cliArgs,
+    cliArgs,
     inputStream: process.stdin,
     outputStream: process.stdout
   });
