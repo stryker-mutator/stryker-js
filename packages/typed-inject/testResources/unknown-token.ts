5e8bb1ee385ad2a979503a2b1b96d524b25fffb3
Nico Jansen <nico.jansen@infosupport.com>
2019-01-30 07:33:24 +0100

    refactor(typed-inject): Move typed inject to it's own repo
    Move typed-inject to https://github.com/nicojs/typed-inject/

--- packages/typed-inject/testResources/unknown-token.ts
+++
@@ -1,9 +0,0 @@
-// error: "Type '[\"not-exists\"]' is not assignable to type 'InjectionToken<{}>[]"
-
-import { rootInjector, tokens } from '../src/index';
-
-function foo(bar: string) { }
-foo.inject = tokens('not-exists');
-
-rootInjector
-  .injectFunction(foo);
