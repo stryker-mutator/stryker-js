3789f4ae53f79ba7b043f8e69c41235690475307
Nico Jansen <nico.jansen@infosupport.com>
2019-02-05 12:01:49 +0100

    Test out perf test in push commit

--- perf/tasks/run-perf-tests.ts
+++ perf/tasks/run-perf-tests.ts
@@ -1,11 +1,10 @@
 import * as fs from 'mz/fs';
 import * as path from 'path';
 import * as execa from 'execa';
-import { interval, Observable, from, race } from 'rxjs';
-import { tap, last, map, count, scan } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { tap, throttleTime } from 'rxjs/operators';
 
 const testRootDir = path.resolve(__dirname, '..', 'test');
-const ONE_MINUTE = 60000;
 
 runPerfTests()
   .then(() => console.log('Done'))
@@ -24,32 +23,24 @@ async function runPerfTests() {
 
 async function runTest(testDir: string) {
   console.time(testDir);
-  await race(from(execNpm('test', testDir)), ticker(testDir)).toPromise();
+  await execNpm(['run', 'stryker'], testDir).pipe(
+    throttleTime(60000),
+    tap(logMessage => console.timeLog(testDir, 'last log message: ', logMessage))
+  ).toPromise();
   console.timeEnd(testDir);
 }
 
-function ticker(testDir: string) {
-  return interval(ONE_MINUTE).pipe(
-    scan((_, curr) => curr + 1, 0),
-    tap(minutes => console.log(`Running ${testDir} for ${minutes} minute${minutes > 1 ? 's' : ''}.`)),
-    last(),
-    map(() => undefined)
-  );
-}
-
-function execNpm(command: string, testDir: string): Observable<execa.ExecaReturns> {
+function execNpm(args: string[], testDir: string): Observable<string> {
   const currentTestDir = path.resolve(testRootDir, testDir);
-  console.log(`Exec ${testDir} npm ${command}`);
-  const testProcess = execa('npm', [command], { timeout: 500000, cwd: currentTestDir, stdio: 'pipe' });
-  let stderr = '';
-  let stdout = '';
-  testProcess.stderr.on('data', chunk => stderr += chunk.toString());
-  testProcess.stdout.on('data', chunk => stdout += chunk.toString());
+  console.log(`Exec ${testDir} npm ${args}`);
 
-  return from(testProcess.catch(error => {
-    console.log(`X ${testDir}`);
-    console.log(stdout);
-    console.error(stderr);
-    throw error;
-  }));
+  return new Observable(observer => {
+    const testProcess = execa('npm', args, { timeout: 500000, cwd: currentTestDir, stdio: 'pipe' });
+    let stderr = '';
+    testProcess.stderr.on('data', chunk => stderr += chunk.toString());
+    testProcess.stdout.on('data', chunk => observer.next(chunk.toString().trim()));
+    testProcess.catch(error => {
+      observer.error(error);
+    });
+  });
 }
