5640c2537b708b145431cc2e533d775667e639d9
Nico Jansen <nico.jansen@infosupport.com>
2019-02-08 12:12:25 +0100

    refactor(karma): Make project starter readonly

--- packages/stryker-karma-runner/src/starters/ProjectStarter.ts
+++ packages/stryker-karma-runner/src/starters/ProjectStarter.ts
@@ -4,7 +4,7 @@ import * as karmaStarter from './karmaStarter';
 import { LoggerFactoryMethod } from 'stryker-api/logging';
 
 export default class ProjectStarter {
-  constructor(private getLogger: LoggerFactoryMethod, private readonly setup: StrykerKarmaSetup) {}
+  constructor(private readonly getLogger: LoggerFactoryMethod, private readonly setup: StrykerKarmaSetup) {}
   public start() {
     if (this.setup.projectType === 'angular-cli') {
       return angularStarter.start(this.getLogger, this.setup.ngConfig);
