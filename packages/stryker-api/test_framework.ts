e36f00aff114edeecbf6ee48270186aecde7f7c8
Nico Jansen <nico.jansen@infosupport.com>
2019-02-11 18:55:17 +0100

    feat(api): also remove all unused option and setting interfaces

--- packages/stryker-api/test_framework.ts
+++ packages/stryker-api/test_framework.ts
@@ -1,3 +1,2 @@
 export { default as TestFramework } from './src/test_framework/TestFramework';
 export { default as TestSelection } from './src/test_framework/TestSelection';
-export { default as TestFrameworkSettings } from './src/test_framework/TestFrameworkSettings';
