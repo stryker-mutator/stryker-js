61d6e58fd7b2ce5c56e95a161eabcc3862a19307
Simon de Lang <simondelang@gmail.com>
2019-02-12 11:52:58 +0100

    feat(getLogger): remove getLogger and LoggerFactory from the API (#1385)
    BREAKING CHANGE: removed `getLogger` and `LoggerFactory` from the API. Please use dependency injection to inject a logger. See https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/api/plugins.md#plugins for more detail

--- packages/stryker-javascript-mutator/test/helpers/initSinon.ts
+++ packages/stryker-javascript-mutator/test/helpers/initSinon.ts
@@ -1,11 +1,7 @@
 import * as sinon from 'sinon';
-import * as logging from 'stryker-api/logging';
-import LogMock from './LogMock';
 
 beforeEach(() => {
   global.sandbox = sinon.createSandbox();
-  global.logMock = new LogMock();
-  global.sandbox.stub(logging, 'getLogger').returns(global.logMock);
 });
 
 afterEach(() => {
