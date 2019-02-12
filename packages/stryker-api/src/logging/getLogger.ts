2f587c77311d4fd5ff07c350d7d325a3611bb7f1
Simon de Lang <simondelang@gmail.com>
2019-02-12 08:50:16 +0100

    feat(getLogger): remove getLogger and LoggerFactory from the API

--- packages/stryker-api/src/logging/getLogger.ts
+++
@@ -1,6 +0,0 @@
-import LoggerFactory from './LoggerFactory';
-import LoggerFactoryMethod from './LoggerFactoryMethod';
-
-const getLogger: LoggerFactoryMethod = LoggerFactory.getLogger;
-
-export default getLogger;
