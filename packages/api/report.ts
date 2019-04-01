b85f98c95f6d319b356d46564792b42609458b7b
Nico Jansen <nico.jansen@infosupport.com>
2019-04-01 08:15:14 +0200

    feat(reporter): add `mutationReportReady` event
    Add the `onMutationTestReportReady` reporter interface method.
    The argument is the report that is compliant to the
    [mutation-testing-report-schema](https://github.com/stryker-mutator/mutation-testing-elements/tree/master/packages/mutation-testing-report-schema)

--- packages/api/report.ts
+++ packages/api/report.ts
@@ -1,6 +1,14 @@
+import * as mutationTestReportSchema from 'mutation-testing-report-schema/dist/src/api';
 export { default as Reporter } from './src/report/Reporter';
 export { default as MutantResult } from './src/report/MutantResult';
 export { default as MutantStatus } from './src/report/MutantStatus';
 export { default as SourceFile } from './src/report/SourceFile';
 export { default as MatchedMutant } from './src/report/MatchedMutant';
 export { default as ScoreResult } from './src/report/ScoreResult';
+export {
+  /**
+   * Types exported directly from mutation-testing-schema
+   * @see https://github.com/stryker-mutator/mutation-testing-elements/tree/master/packages/mutation-testing-report-schema
+   */
+  mutationTestReportSchema
+};
