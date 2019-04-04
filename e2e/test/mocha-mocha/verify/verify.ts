898143ed00595e7b7af08ca2b2949ab1cbc6631c
Nico Jansen <nico.jansen@infosupport.com>
2019-04-04 07:45:49 +0200

    test(e2e): add a e2e test for the html reporter

--- e2e/test/mocha-mocha/verify/verify.ts
+++ e2e/test/mocha-mocha/verify/verify.ts
@@ -1,6 +1,12 @@
 import { expectScoreResult } from '../../../helpers';
+import * as fs from 'fs';
+import { expect } from 'chai';
 
 describe('Verify stryker has ran correctly', () => {
+
+  function expectExists(fileName: string) {
+    expect(fs.existsSync(fileName), `Missing ${fileName}!`).true;
+  }
 
   it('should report correct score', async () => {
     await expectScoreResult({
@@ -9,5 +15,12 @@ describe('Verify stryker has ran correctly', () => {
       noCoverage: 0,
       survived: 9
     });
+  });
+
+  it('should report html files', () => {
+    expectExists('reports/mutation/html/index.html');
+    expectExists('reports/mutation/html/mutation-test-elements.js');
+    expectExists('reports/mutation/html/stryker-80x80.png');
+    expectExists('reports/mutation/html/bind-mutation-test-report.js');
   });
 });
