b73afc9f4bf2cf2b17c7ddb0be67c30a0a3a2ee2
Nico Jansen <nico.jansen@infosupport.com>
2019-04-04 07:45:17 +0200

    fix(html): load report json from a js file
    Support HTML fragments in user code by loading
    it from a JS file, instead if inserting it inside a script
    tag. Inserting in script tags resulted in errors when
    user code contains `"</script>"` .

--- packages/html-reporter/test/integration/MathReport.it.spec.ts
+++ packages/html-reporter/test/integration/MathReport.it.spec.ts
@@ -15,10 +15,13 @@ describe('HtmlReporter with example math project', () => {
     await sut.wrapUp();
   });
 
-  it('should have created "index.html", "mutation-test-elements.js" and "stryker-80x80.png" file in the configured directory', () => {
+  it('should have created "index.html", "mutation-test-elements.js", "stryker-80x80.png" and "bind-mutation-test-report.js" file in the configured directory', () => {
     expectFileExists(`${baseDir}/index.html`, true);
     expectFileExists(`${baseDir}/stryker-80x80.png`, true);
     expectFileExists(`${baseDir}/mutation-test-elements.js`, true);
+    expectFileExists(`${baseDir}/bind-mutation-test-report.js`, true);
+    const bindMutationTestReportContent = fs.readFileSync(`${baseDir}/bind-mutation-test-report.js`, 'utf8');
+    expect(bindMutationTestReportContent).include(JSON.stringify(simpleReport));
   });
 });
 
