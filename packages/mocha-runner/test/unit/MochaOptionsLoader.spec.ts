6df4102dbe227cf99c8d483b183faa628ae39bc5
Nico Jansen <nico.jansen@infosupport.com>
2019-04-18 16:11:11 +0200

    feat(mocha 6): support all config formats
    Add support for all mocha's configuration formats:
    * Arguments specified on command-line
    * Configuration file (.mocharc.js, .mocharc.yml, etc.)
        1. .mocharc.js
        2. .mocharc.yaml
        3. .mocharc.yml
        4. .mocharc.jsonc
        5. .mocharc.json
    * mocha property of package.json
    * mocha.opts
    
    This works by using mocha 6's [`loadOptions`](https://mochajs.org/api/module-lib_cli_options.html#.loadOptions) function if it is available.
    If not (mocha < 6), use old parsing logic.
    
    See https://mochajs.org/#configuring-mocha-nodejs

--- packages/mocha-runner/test/unit/MochaOptionsLoader.spec.ts
+++ packages/mocha-runner/test/unit/MochaOptionsLoader.spec.ts
@@ -3,11 +3,12 @@ import * as fs from 'fs';
 import { Config } from '@stryker-mutator/api/config';
 import MochaOptionsLoader from '../../src/MochaOptionsLoader';
 import { expect } from 'chai';
-import MochaRunnerOptions from '../../src/MochaRunnerOptions';
 import sinon = require('sinon');
 import { testInjector } from '@stryker-mutator/test-helpers';
+import LibWrapper from '../../src/LibWrapper';
+import { mochaOptionsKey } from '../../src/utils';
 
-describe('MochaOptionsLoader', () => {
+describe(MochaOptionsLoader.name, () => {
 
   let readFileStub: sinon.SinonStub;
   let existsFileStub: sinon.SinonStub;
@@ -15,140 +16,219 @@ describe('MochaOptionsLoader', () => {
   let sut: MochaOptionsLoader;
 
   beforeEach(() => {
-    readFileStub = sinon.stub(fs, 'readFileSync');
-    existsFileStub = sinon.stub(fs, 'existsSync').returns(true);
     sut = testInjector.injector.injectClass(MochaOptionsLoader);
-    config = new Config();
   });
 
-  afterEach(() => {
-    sinon.restore();
-  });
+  describe('with mocha >= 6', () => {
 
-  it('should load a mocha.opts file if specified', () => {
-    readFileStub.returns('');
-    config.mochaOptions = {
-      opts: 'some/mocha.opts/file'
-    };
-    sut.load(config);
-    expect(testInjector.logger.info).calledWith(`Loading mochaOpts from "${path.resolve('some/mocha.opts/file')}"`);
-    expect(fs.readFileSync).calledWith(path.resolve('some/mocha.opts/file'));
-  });
+    let rawOptions: { [option: string]: any };
 
-  it('should log an error if specified mocha.opts file doesn\'t exist', () => {
-    readFileStub.returns('');
-    existsFileStub.returns(false);
-    config.mochaOptions = {
-      opts: 'some/mocha.opts/file'
-    };
+    beforeEach(() => {
+      rawOptions = Object.create(null);
+      sinon.stub(LibWrapper, 'loadOptions').returns(rawOptions);
+    });
 
-    sut.load(config);
-    expect(testInjector.logger.error).calledWith(`Could not load opts from "${path.resolve('some/mocha.opts/file')}". Please make sure opts file exists.`);
-  });
+    it('should log about mocha >= 6', () => {
+      sut.load(testInjector.options);
+      expect(testInjector.logger.debug).calledWith(
+        'Mocha > 6 detected. Using mocha\'s `%s` to load mocha options', LibWrapper.loadOptions && LibWrapper.loadOptions.name
+      );
+    });
 
-  it('should load default mocha.opts file if not specified', () => {
-    readFileStub.returns('');
-    sut.load(config);
-    expect(testInjector.logger.info).calledWith(`Loading mochaOpts from "${path.resolve('test/mocha.opts')}"`);
-    expect(fs.readFileSync).calledWith(path.resolve('test/mocha.opts'));
-  });
+    it('should call `loadOptions` with serialized arguments', () => {
+      testInjector.options[mochaOptionsKey] = {
+        baz: true,
+        foo: 'bar'
+      };
+      sut.load(testInjector.options);
+      expect(LibWrapper.loadOptions).calledWith(['--baz', 'true', '--foo', 'bar']);
+    });
 
-  it('shouldn\'t load anything if mocha.opts = false', () => {
-    config.mochaOptions = {
-      opts: false
-    };
-    sut.load(config);
-    expect(fs.readFileSync).not.called;
-    expect(testInjector.logger.debug).calledWith('Not reading additional mochaOpts from a file');
-  });
+    it('should filter out invalid options from the `loadOptions` result', () => {
+      testInjector.options[mochaOptionsKey] = {
+        override: true
+      };
 
-  it('should not load default mocha.opts file if not found', () => {
-    existsFileStub.returns(false);
-    const options = sut.load(config);
-    expect(options).deep.eq({});
-    expect(testInjector.logger.debug).calledWith('No mocha opts file found, not loading additional mocha options (%s.opts was not defined).', 'mochaOptions');
-  });
+      // Following are valid options
+      rawOptions.extension = 'foo';
+      rawOptions.require = 'bar';
+      rawOptions.timeout = 'baz';
+      rawOptions['async-only'] = 'qux';
+      rawOptions.ui = 'quux';
+      rawOptions.grep = 'quuz';
+      rawOptions.exclude = 'corge';
+      rawOptions.file = 'grault';
 
-  it('should load `--require` and `-r` properties if specified in mocha.opts file', () => {
-    readFileStub.returns(`
-    --require  src/test/support/setup
-    -r babel-require
-    `);
-    config.mochaOptions = { opts: '.' };
-    const options = sut.load(config);
-    expect(options).deep.include({
-      require: [
-        'src/test/support/setup',
-        'babel-require'
-      ]
+      rawOptions.garply = 'waldo'; // this should be filtered out
+      const result = sut.load(testInjector.options);
+      expect(result).deep.eq({
+        exclude: 'corge',
+        extension: 'foo',
+        file: 'grault',
+        grep: 'quuz',
+        override: true,
+        require: 'bar',
+        timeout: 'baz',
+        ['async-only']: 'qux',
+        ui: 'quux',
+      });
+    });
+
+    it('should trace log the mocha call', () => {
+      testInjector.logger.isTraceEnabled.returns(true);
+      testInjector.options[mochaOptionsKey] = {
+        foo: 'bar'
+      };
+      rawOptions.baz = 'qux';
+      sut.load(testInjector.options);
+      const fnName = LibWrapper.loadOptions && LibWrapper.loadOptions.name;
+      expect(testInjector.logger.trace).calledWith(
+        `Mocha: ${fnName}(['--foo','bar']) => {"baz":"qux"}`
+      );
     });
   });
 
-  function itShouldLoadProperty(property: string, value: string, expectedConfig: Partial<MochaRunnerOptions>) {
-    it(`should load '${property} if specified`, () => {
-      readFileStub.returns(`${property} ${value}`);
-      config.mochaOptions = { opts: 'path/to/opts/file' };
-      expect(sut.load(config)).deep.include(expectedConfig);
+  describe('with mocha < 6', () => {
+
+    beforeEach(() => {
+      sinon.stub(LibWrapper, 'loadOptions').value(undefined);
+      readFileStub = sinon.stub(fs, 'readFileSync');
+      existsFileStub = sinon.stub(fs, 'existsSync').returns(true);
+      config = new Config();
     });
-  }
 
-  itShouldLoadProperty('--timeout', '2000', { timeout: 2000 });
-  itShouldLoadProperty('-t', '2000', { timeout: 2000 });
-  itShouldLoadProperty('-A', '', { asyncOnly: true });
-  itShouldLoadProperty('--async-only', '', { asyncOnly: true });
-  itShouldLoadProperty('--ui', 'qunit', { ui: 'qunit' });
-  itShouldLoadProperty('-u', 'qunit', { ui: 'qunit' });
-  itShouldLoadProperty('-g', 'grepthis', { grep: /grepthis/ });
-  itShouldLoadProperty('--grep', '/grep(this|that)/', { grep: /grep(this|that)/ });
-  itShouldLoadProperty('--grep', 'grep(this|that)?', { grep: /grep(this|that)?/ });
+    it('should log about mocha < 6', () => {
+      existsFileStub.returns(false);
+      sut.load(config);
+      expect(testInjector.logger.debug).calledWith('Mocha < 6 detected. Using custom logic to parse mocha options');
+    });
 
-  it('should not override additional properties', () => {
-    readFileStub.returns(`
-      -u qunit
-      -t 2000
-      -A
-      -r babel-register
-    `);
-    config.mochaOptions = {
-      asyncOnly: false,
-      opts: 'path/to/opts/file',
-      require: ['ts-node/register'],
-      timeout: 4000,
-      ui: 'exports'
-    };
-    const options = sut.load(config);
-    expect(options).deep.equal({
-      asyncOnly: false,
-      opts: 'path/to/opts/file',
-      require: ['ts-node/register'],
-      timeout: 4000,
-      ui: 'exports'
+    it('should load a mocha.opts file if specified', () => {
+      readFileStub.returns('');
+      config.mochaOptions = {
+        opts: 'some/mocha.opts/file'
+      };
+      sut.load(config);
+      expect(testInjector.logger.info).calledWith(`Loading mochaOpts from "${path.resolve('some/mocha.opts/file')}"`);
+      expect(fs.readFileSync).calledWith(path.resolve('some/mocha.opts/file'));
+    });
+
+    it('should log an error if specified mocha.opts file doesn\'t exist', () => {
+      readFileStub.returns('');
+      existsFileStub.returns(false);
+      config.mochaOptions = {
+        opts: 'some/mocha.opts/file'
+      };
+
+      sut.load(config);
+      expect(testInjector.logger.error).calledWith(`Could not load opts from "${path.resolve('some/mocha.opts/file')}". Please make sure opts file exists.`);
+    });
+
+    it('should load default mocha.opts file if not specified', () => {
+      readFileStub.returns('');
+      sut.load(config);
+      expect(testInjector.logger.info).calledWith(`Loading mochaOpts from "${path.resolve('test/mocha.opts')}"`);
+      expect(fs.readFileSync).calledWith(path.resolve('test/mocha.opts'));
+    });
+
+    it('shouldn\'t load anything if mocha.opts = false', () => {
+      config.mochaOptions = {
+        opts: false
+      };
+      sut.load(config);
+      expect(fs.readFileSync).not.called;
+      expect(testInjector.logger.debug).calledWith('Not reading additional mochaOpts from a file');
+    });
+
+    it('should not load default mocha.opts file if not found', () => {
+      existsFileStub.returns(false);
+      const options = sut.load(config);
+      expect(options).deep.eq({});
+      expect(testInjector.logger.debug).calledWith('No mocha opts file found, not loading additional mocha options (%s.opts was not defined).', 'mochaOptions');
+    });
+
+    it('should load `--require` and `-r` properties if specified in mocha.opts file', () => {
+      readFileStub.returns(`
+      --require  src/test/support/setup
+      -r babel-require
+      `);
+      config.mochaOptions = { opts: '.' };
+      const options = sut.load(config);
+      expect(options).deep.include({
+        require: [
+          'src/test/support/setup',
+          'babel-require'
+        ]
+      });
+    });
+
+    function itShouldLoadProperty(property: string, value: string, expectedConfig: Partial<MochaOptions>) {
+      it(`should load '${property} if specified`, () => {
+        readFileStub.returns(`${property} ${value}`);
+        config.mochaOptions = { opts: 'path/to/opts/file' };
+        expect(sut.load(config)).deep.include(expectedConfig);
+      });
+    }
+
+    itShouldLoadProperty('--timeout', '2000', { timeout: 2000 });
+    itShouldLoadProperty('-t', '2000', { timeout: 2000 });
+    itShouldLoadProperty('-A', '', { asyncOnly: true });
+    itShouldLoadProperty('--async-only', '', { asyncOnly: true });
+    itShouldLoadProperty('--ui', 'qunit', { ui: 'qunit' });
+    itShouldLoadProperty('-u', 'qunit', { ui: 'qunit' });
+    itShouldLoadProperty('-g', 'grepthis', { grep: /grepthis/ });
+    itShouldLoadProperty('--grep', '/grep(this|that)/', { grep: /grep(this|that)/ });
+    itShouldLoadProperty('--grep', 'grep(this|that)?', { grep: /grep(this|that)?/ });
+
+    it('should not override additional properties', () => {
+      readFileStub.returns(`
+        -u qunit
+        -t 2000
+        -A
+        -r babel-register
+      `);
+      config.mochaOptions = {
+        asyncOnly: false,
+        opts: 'path/to/opts/file',
+        require: ['ts-node/register'],
+        timeout: 4000,
+        ui: 'exports'
+      };
+      const options = sut.load(config);
+      expect(options).deep.equal({
+        asyncOnly: false,
+        opts: 'path/to/opts/file',
+        require: ['ts-node/register'],
+        timeout: 4000,
+        ui: 'exports'
+      });
+    });
+
+    it('should ignore additional properties', () => {
+      readFileStub.returns(`
+      --reporter dot
+      --ignore-leaks
+      `);
+      config.mochaOptions = {
+        opts: 'some/mocha.opts/file',
+      };
+      const options = sut.load(config);
+      expect(options).deep.eq({ opts: 'some/mocha.opts/file' });
+      expect(testInjector.logger.debug).calledWith('Ignoring option "--reporter" as it is not supported.');
+      expect(testInjector.logger.debug).calledWith('Ignoring option "--ignore-leaks" as it is not supported.');
+    });
+
+    it('should ignore invalid --ui and --timeout options', () => {
+      readFileStub.returns(`
+      --timeout
+      --ui
+      `);
+      config.mochaOptions = {
+        opts: 'some/mocha.opts/file',
+      };
+      const options = sut.load(config);
+      expect(options).deep.eq({ opts: 'some/mocha.opts/file', timeout: undefined, ui: undefined });
     });
   });
 
-  it('should ignore additional properties', () => {
-    readFileStub.returns(`
-    --reporter dot
-    --ignore-leaks
-    `);
-    config.mochaOptions = {
-      opts: 'some/mocha.opts/file',
-    };
-    const options = sut.load(config);
-    expect(options).deep.eq({ opts: 'some/mocha.opts/file' });
-    expect(testInjector.logger.debug).calledWith('Ignoring option "--reporter" as it is not supported.');
-    expect(testInjector.logger.debug).calledWith('Ignoring option "--ignore-leaks" as it is not supported.');
-  });
-
-  it('should ignore invalid --ui and --timeout options', () => {
-    readFileStub.returns(`
-    --timeout
-    --ui
-    `);
-    config.mochaOptions = {
-      opts: 'some/mocha.opts/file',
-    };
-    const options = sut.load(config);
-    expect(options).deep.eq({ opts: 'some/mocha.opts/file', timeout: undefined, ui: undefined });
-  });
 });
