4f700434fca4d73356fd1240f4138e065c390510
Simon de Lang <simondelang@gmail.com>
2019-11-14 14:27:31 +0100

    test(vue): use correct BlockStatement mutator name

--- packages/vue-mutator/test/integration/VueMutator.it.spec.ts
+++ packages/vue-mutator/test/integration/VueMutator.it.spec.ts
@@ -53,7 +53,7 @@ describe('VueMutator', () => {
       const mutants = sut.mutate(files);
 
       expect(mutants.filter(m => m.mutatorName === 'StringLiteral').length).to.equal(2);
-      expect(mutants.filter(m => m.mutatorName === 'BlockMutator').length).to.equal(3);
+      expect(mutants.filter(m => m.mutatorName === 'BlockStatement').length).to.equal(3);
       expect(mutants.filter(m => m.mutatorName === 'EqualityOperator').length).to.equal(2);
       expect(mutants.filter(m => m.mutatorName === 'ArithmeticOperator').length).to.equal(1);
     });
@@ -94,7 +94,7 @@ describe('VueMutator', () => {
       const mutants = sut.mutate(files);
 
       expect(mutants.filter(m => m.mutatorName === 'StringLiteral').length).to.equal(1);
-      expect(mutants.filter(m => m.mutatorName === 'BlockMutator').length).to.equal(3);
+      expect(mutants.filter(m => m.mutatorName === 'BlockStatement').length).to.equal(3);
       expect(mutants.filter(m => m.mutatorName === 'EqualityOperator').length).to.equal(2);
       expect(mutants.filter(m => m.mutatorName === 'ArithmeticOperator').length).to.equal(1);
     });
