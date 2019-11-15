9fc7f6f2c61c926db321026ced350beb73f5fd17
Nico Jansen <nico.jansen@infosupport.com>
2019-11-15 22:52:00 +0100

    chore: document new DeepPartial<T> type

--- packages/api/src/config/Config.ts
+++ packages/api/src/config/Config.ts
@@ -1,6 +1,11 @@
 import { LogLevel, MutationScoreThresholds, MutatorDescriptor, StrykerOptions, DashboardOptions } from '../../core';
 import { ReportType } from '../core/DashboardOptions';
 
+/**
+ * When configuring stryker, every option is optional
+ * Including deep properties like `dashboard.project`.
+ * That's why we use a `DeepPartial` mapped type here.
+ */
 type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
 };
