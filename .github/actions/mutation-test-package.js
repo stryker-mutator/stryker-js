bdb7314867764910c414beccf9d880ee5f907898
Nico Jansen <nico.jansen@infosupport.com>
2019-11-18 17:20:06 +0100

    Try to create a reusable action

---
+++ .github/actions/mutation-test-package.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const { execSync } = require('child_process');
+
+/**
+ * Ripped from @actions/core
+ * @param {string} name name of the input to retrieeve
+ * @param {any} options 
+ */
+function getInput(name, options) {
+  const val = process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] || '';
+  if (options && options.required && !val) {
+      throw new Error(`Input required and not supplied: ${name}`);
+  }
+  return val.trim();
+}
+
+const package = getInput('package');
+const apiKey = getInput('apiKey');
+console.log('Package ', package);
+
+execSync('npm install');
+execSync('npm run build');
+execSync('npm run stryker', { env: { STRYKER_DASHBOARD_API_KEY: apiKey },  cwd: path.resolve(__dirname, '..', '..', 'packages', package)});
