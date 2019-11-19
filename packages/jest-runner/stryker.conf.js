57de2a5cd7a0f8ea10a48538df0bd610a3a9ed13
Nico Jansen <nico.jansen@infosupport.com>
2019-11-19 07:09:22 +0100

    Fix problems in the jest-runner

--- packages/jest-runner/stryker.conf.js
+++ packages/jest-runner/stryker.conf.js
@@ -2,6 +2,7 @@ const path = require('path');
 const settings = require('../../stryker.parent.conf');
 const moduleName = __dirname.split(path.sep).pop();
 settings.dashboard.module = moduleName;
+settings.coverageAnalysis = 'off';
 module.exports = function (config) {
   config.set(settings);
 };
