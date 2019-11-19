6a58bc3d8b42b8b453a142fda70a23a4121526b8
Nico Jansen <jansennico@gmail.com>
2019-11-19 09:13:22 +0100

    Ping

--- packages/karma-runner/stryker.conf.js
+++ packages/karma-runner/stryker.conf.js
@@ -3,6 +3,7 @@ const settings = require('../../stryker.parent.conf');
 const moduleName = __dirname.split(path.sep).pop();
 settings.dashboard.module = moduleName;
 settings.coverageAnalysis = 'off';
+
 module.exports = function (config) {
   config.set(settings);
 };
