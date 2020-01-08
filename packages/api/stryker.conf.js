b8e391ed63796b72af2aebb569e3d1015786cd4b
Jelle de Beer <Jelle.deBeer@infosupport.com>
2020-01-08 09:45:52 +0100

    undo change

---
+++ packages/api/stryker.conf.js
@@ -0,0 +1,7 @@
+const path = require('path');	
+const settings = require('../../stryker.parent.conf');	
+const moduleName = __dirname.split(path.sep).pop();	
+settings.dashboard.module = moduleName;	
+module.exports = function (config) {	
+  config.set(settings);	
+};
