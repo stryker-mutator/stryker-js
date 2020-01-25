4bdb7a18e5ea388a55f00643593ea5efdde1b22f
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2020-01-25 23:39:56 +0100

    feat(api): Document StrykerOptions in JSON schema
    Document the `StrykerOptions` using a JSON schema. The TypeScript files representing the `StrykerOptions` are now generated from this file.
    
    This clears a path to validate StrykerOptions using JSON Schema in the future, as wel as helps with authoring a Stryker config file.

--- packages/api/core.ts
+++ packages/api/core.ts
@@ -1,9 +1,6 @@
-export { default as StrykerOptions } from './src/core/StrykerOptions';
 export { default as File } from './src/core/File';
 export { default as Position } from './src/core/Position';
 export { default as Location } from './src/core/Location';
 export { default as Range } from './src/core/Range';
-export { default as MutatorDescriptor } from './src/core/MutatorDescriptor';
-export { default as MutationScoreThresholds } from './src/core/MutationScoreThresholds';
-export { default as LogLevel } from './src/core/LogLevel';
-export * from './src/core/DashboardOptions';
+export * from './src-generated/core';
+export * from './src/core/ReportTypes';
--- packages/api/src/config/Config.ts
+++ packages/api/src/config/Config.ts
@@ -1,5 +1,4 @@
-import { LogLevel, MutationScoreThresholds, MutatorDescriptor, StrykerOptions, DashboardOptions } from '../../core';
-import { ReportType } from '../core/DashboardOptions';
+import { LogLevel, MutationScoreThresholds, MutatorDescriptor, StrykerOptions, DashboardOptions, ReportType } from '../../core';
 
 import defaultTempDirName from './DefaultTempDirName';
 
