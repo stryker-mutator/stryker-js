86dad1463b0f5c1d8f95b695bcc8f17e704ce086
Nico Jansen <jansennico@gmail.com>
2020-02-16 00:46:48 +0100

    Add more logging

--- e2e/tasks/run-e2e-tests.ts
+++ e2e/tasks/run-e2e-tests.ts
@@ -33,14 +33,24 @@ runE2eTests()
     },
   });
 
-function execNpm(command: string, testDir: string) {
+function execNpm(command: string, testDir: string, stream: boolean) {
   const currentTestDir = path.resolve(testRootDir, testDir);
   console.log(`Exec ${testDir} npm ${command}`);
   const testProcess = execa('npm', [command], { timeout: 500000, cwd: currentTestDir, stdio: 'pipe' });
   let stderr = '';
   let stdout = '';
-  testProcess.stderr.on('data', chunk => stderr += chunk.toString());
-  testProcess.stdout.on('data', chunk => stdout += chunk.toString());
+  testProcess.stderr.on('data', chunk => {
+    stderr += chunk.toString();
+    if (stream) {
+      console.error(chunk.toString());
+    }
+  });
+  testProcess.stdout.on('data', chunk => {
+    stdout += chunk.toString()
+    if (stream) {
+      console.log(chunk.toString());
+    }
+  });
   return testProcess.catch(error => {
     console.log(`X ${testDir}`);
     console.log(stdout);
@@ -62,7 +72,7 @@ function satisfiesNodeVersion(testDir: string): boolean {
 
 async function runTest(testDir: string) {
   if (satisfiesNodeVersion(testDir)) {
-    await execNpm('test', testDir);
+    await execNpm('test', testDir, testDir.startsWith('karma'));
   }
   return testDir;
 }
