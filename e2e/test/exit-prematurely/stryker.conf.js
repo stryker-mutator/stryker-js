26e21b27018361787ee0e0705636e7983e1b9727
Nico Jansen <jansennico@gmail.com>
2020-03-11 16:07:10 +0100

    Spread the joy accross or e2e tests

--- e2e/test/exit-prematurely/stryker.conf.js
+++ e2e/test/exit-prematurely/stryker.conf.js
@@ -1,19 +1,17 @@
-module.exports = function (config) {
-  config.set({
-    mutate: [
-      'src/*.js'
-    ],
-    testFramework: 'mocha',
-    testRunner: 'mocha',
-    coverageAnalysis: 'off',
-    mutator: 'javascript',
-    transpilers: [
-      'babel'
-    ],
-    timeoutMS: 60000,
-    reporters: ['clear-text', 'html', 'event-recorder'],
-    maxConcurrentTestRunners: 2,
-    logLevel: 'info',
-    fileLogLevel: 'info'
-  });
-};
+module.exports = {
+  mutate: [
+    'src/*.js'
+  ],
+  testFramework: 'mocha',
+  testRunner: 'mocha',
+  coverageAnalysis: 'off',
+  mutator: 'javascript',
+  transpilers: [
+    'babel'
+  ],
+  timeoutMS: 60000,
+  reporters: ['clear-text', 'html', 'event-recorder'],
+  maxConcurrentTestRunners: 2,
+  logLevel: 'info',
+  fileLogLevel: 'info'
+}
