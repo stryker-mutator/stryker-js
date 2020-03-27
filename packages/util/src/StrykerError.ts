a9dea8c638a61bd472e937b69bf37846074e09a1
Nico Jansen <jansennico@gmail.com>
2020-03-27 23:23:34 +0100

    fix(utils): make sure `instanceof StrykerError` works
    Remove es5 workarounds around `Error`, so `err instanceof StrykerError`
    works

--- packages/util/src/StrykerError.ts
+++ packages/util/src/StrykerError.ts
@@ -3,8 +3,5 @@ import { errorToString } from './errors';
 export default class StrykerError extends Error {
   constructor(message: string, public readonly innerError?: Error) {
     super(`${message}${innerError ? `. Inner error: ${errorToString(innerError)}` : ''}`);
-    Error.captureStackTrace(this, StrykerError);
-    // TS recommendation: https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
-    Object.setPrototypeOf(this, StrykerError.prototype);
   }
 }
