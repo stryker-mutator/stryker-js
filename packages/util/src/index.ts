13ebb025faf1ac45fcec2741a28eddf8140836fc
Nico Jansen <jansennico@gmail.com>
2020-03-27 23:21:58 +0100

    refactor(utils): move `noopLogger` to utils

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -2,3 +2,5 @@ export { default as childProcessAsPromised } from './childProcessAsPromised';
 export { default as StrykerError } from './StrykerError';
 export * from './errors';
 export * from './stringUtils';
+export * from './noopLogger';
+export * from './notEmpty';
---
+++ packages/util/src/noopLogger.ts
@@ -0,0 +1,38 @@
+export const noopLogger = {
+  isTraceEnabled() {
+    return false;
+  },
+  isDebugEnabled() {
+    return false;
+  },
+  isInfoEnabled() {
+    return false;
+  },
+  isWarnEnabled() {
+    return false;
+  },
+  isErrorEnabled() {
+    return false;
+  },
+  isFatalEnabled() {
+    return false;
+  },
+  trace() {
+    // noop
+  },
+  debug() {
+    // noop
+  },
+  info() {
+    // noop
+  },
+  warn() {
+    // noop
+  },
+  error() {
+    // noop
+  },
+  fatal() {
+    // noop
+  }
+};
