d1b3a09566a6bc99b5eb31aa0c5a81ed7ae1c9eb
Nico Jansen <jansennico@gmail.com>
2020-04-06 21:18:56 +0200

    fix: remove undesired require statement

--- packages/babel-transpiler/src/index.ts
+++ packages/babel-transpiler/src/index.ts
@@ -3,5 +3,3 @@ import { declareFactoryPlugin, PluginKind } from '@stryker-mutator/api/plugin';
 import { babelTranspilerFactory } from './BabelTranspiler';
 
 export const strykerPlugins = [declareFactoryPlugin(PluginKind.Transpiler, 'babel', babelTranspilerFactory)];
-
-export const strykerPluginSchema = require('');
