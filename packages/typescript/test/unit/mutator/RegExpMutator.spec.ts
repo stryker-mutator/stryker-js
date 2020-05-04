0f767d5ae53c5644727c620c2d0f0e0842682042
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-05-03 22:30:27 -0400

    fix @stryker-mutator/mutator-specification import in TypeScript test

--- packages/typescript/test/unit/mutator/RegExpMutator.spec.ts
+++ packages/typescript/test/unit/mutator/RegExpMutator.spec.ts
@@ -1,4 +1,5 @@
-import { RegExpMutatorSpec } from '../../../../mutator-specification/src';
+import { RegExpMutatorSpec } from '@stryker-mutator/mutator-specification/src/index';
+
 import RegExpMutator from '../../../src/mutator/RegExpMutator';
 
 import { verifySpecification } from './mutatorAssertions';
