c78ededd69d6ecd26f987b5ec23d7655f7ecc6e8
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-05-03 22:17:10 -0400

    fix @stryker-mutator/mutator-specification import in JavaScript test

--- packages/javascript-mutator/test/unit/mutators/RegExpMutator.spec.ts
+++ packages/javascript-mutator/test/unit/mutators/RegExpMutator.spec.ts
@@ -1,4 +1,5 @@
-import { RegExpMutatorSpec } from '../../../../mutator-specification/src';
+import { RegExpMutatorSpec } from '@stryker-mutator/mutator-specification/src/index';
+
 import RegExpMutator from '../../../src/mutators/RegExpMutator';
 import { verifySpecification } from '../../helpers/mutatorAssertions';
 
