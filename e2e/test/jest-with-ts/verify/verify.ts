5080e672062a4604b88d1d8136931029efe15ec7
Nico Jansen <jansennico@gmail.com>
2020-05-08 23:33:36 +0200

    fix test

--- e2e/test/jest-with-ts/verify/verify.ts
+++ e2e/test/jest-with-ts/verify/verify.ts
@@ -6,7 +6,7 @@ describe('Verify stryker has ran correctly', () => {
   it('should report correct score', async () => {
     await expectMetrics({
       ignored: 0,
-      killed: 26,
+      killed: 12,
       mutationScore: 54.55,
       noCoverage: 0,
       survived: 13,
