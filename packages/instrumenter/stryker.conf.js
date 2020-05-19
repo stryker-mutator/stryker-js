ba43da315890220b39c323bfa84ac381e8d969d0
Nico Jansen <jansennico@gmail.com>
2020-05-19 08:55:00 +0200

    chore(instrumenter): configure mutation testing

--- packages/instrumenter/stryker.conf.js
+++ packages/instrumenter/stryker.conf.js
@@ -2,4 +2,7 @@ const path = require('path');
 const settings = require('../../stryker.parent.conf');
 const moduleName = __dirname.split(path.sep).pop();
 settings.dashboard.module = moduleName;
+delete settings.mochaOptions.spec;
+delete settings.files;
+settings.coverageAnalysis = 'off';
 module.exports = settings;
