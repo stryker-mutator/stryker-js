bc44350e0161332aa9d449db87c20602a149a9f7
Nico Jansen <jansennico@gmail.com>
2020-06-11 18:06:09 +0200

    Add new suggestion for test runner api

--- packages/api/src/test_runner2/RunResult.ts
+++ packages/api/src/test_runner2/RunResult.ts
@@ -1,32 +1,46 @@
+import { TestSelection } from '../../test_framework';
+
 import { MutantCoverage } from './MutantCoverage';
 import { RunStatus } from './RunStatus';
 import { TestResult } from './TestResult';
 
-/**
- * Marker interface for a mutant run result.
- */
-export interface MutantRunResult extends RunResult {}
+export enum MutantRunStatus {
+  Killed = 'killed',
+  Survived = 'survived',
+}
+
+export type MutantRunResult = KilledMutantRunResult | SurvivedMutantRunResult;
+
+export interface KilledMutantRunResult extends RunResult {
+  status: MutantRunStatus.Killed;
+  culpritTest: TestSelection;
+}
+
+export interface SurvivedMutantRunResult extends RunResult {
+  status: MutantRunStatus.Survived;
+  testSelection: TestSelection[] | false;
+}
 
 /**
  * Represents the result of a test run.
  */
 export interface RunResult {
   /**
+   * If `state` is `error`, this collection should contain the error messages
+   */
+  errorMessage?: string;
+}
+
+export interface DryRunResult extends RunResult {
+  /**
    * The individual test results.
    */
   tests: TestResult[];
 
-  /**
-   * If `state` is `error`, this collection should contain the error messages
-   */
-  errorMessage?: string;
+  mutationCoverage?: MutantCoverage;
 
   /**
    * The status of the run
    */
   status: RunStatus;
-}
-
-export interface DryRunResult extends RunResult {
-  mutationCoverage?: MutantCoverage;
 }
