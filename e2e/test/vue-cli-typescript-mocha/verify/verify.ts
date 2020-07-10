9267b6f1578d8d8dbea90fc8aed37422ac4d9cf0
Nico Jansen <jansennico@gmail.com>
2020-07-10 13:08:06 +0200

    test(e2e): enable 'vue-cli-typescript-mocha'

--- e2e/test/vue-cli-typescript-mocha/verify/verify.ts
+++ e2e/test/vue-cli-typescript-mocha/verify/verify.ts
@@ -5,10 +5,12 @@ describe('Verify stryker has ran correctly', () => {
   it('should report correct score', async () => {
     await expectMetrics({
       killed: 4,
-      survived: 13,
+      survived: 1,
+      noCoverage: 12,
       compileErrors: 0,
       runtimeErrors: 0,
-      timeout: 0
+      timeout: 0,
+      mutationScore: 23.53
     });
   });
 });
