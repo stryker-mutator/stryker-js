91f02471c60f1b52564f3ad8ed9e1d1defda9243
Nico Jansen <jansennico@gmail.com>
2020-07-10 19:33:51 +0200

    feat(test-framework): remove `TestFramework` API
    Remove the test framework API and their implementations: `@stryker-mutator/jasmine-framework` and `@stryker-mutator/mocha-framework`. Test runners are now expected to cary their own support for different test frameworks they might support.
    
    A `TestFramework` provided decoupling between the framework and the test runner itself. It was used to provide test filtering and `perTest` coverage analysis. Historically, this decoupling only came in use for `karma`, since that is the only test runner that supports multiple test frameworks. Support for filtering `mocha` and `jasmine` tests is now build into the `@stryker-mutator/karma-runner` itself, as well as the other supported test runners.
    
    Therefore this API is no longer used, which makes it easier to configure Stryker and easier to maintain.

--- packages/api/core.ts
+++ packages/api/core.ts
@@ -7,4 +7,3 @@ export * from './src-generated/stryker-core';
 export * from './src/core/ReportTypes';
 export * from './src/core/StrykerOptionsSchema';
 export * from './src/core/PartialStrykerOptions';
-export * from './src/core/OptionsEditor';
--- packages/mocha-framework/src/index.ts
+++ packages/mocha-framework/src/index.ts
@@ -1,5 +1,1 @@
-import { declareClassPlugin, PluginKind } from '@stryker-mutator/api/plugin';
-
-import MochaTestFramework from './MochaTestFramework';
-
-export const strykerPlugins = [declareClassPlugin(PluginKind.TestFramework, 'mocha', MochaTestFramework)];
+console.warn('Framework plugins are no longer needed in Stryker 4.0. Please remove "@stryker-mutator/mocha-framework" from your devDependencies.');
