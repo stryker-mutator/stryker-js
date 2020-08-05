694a3760bcf15bc76ca42a8595a4d842a4909d0e
Nico Jansen <jansennico@gmail.com>
2020-08-05 17:03:16 +0200

    test(e2e): enable and fix jest-react test

--- e2e/test/jest-react/verify/verify.ts
+++ e2e/test/jest-react/verify/verify.ts
@@ -4,22 +4,22 @@ describe('After running stryker on jest-react project', () => {
   it('should report expected scores', async () => {
     await expectMetricsResult({
       metrics: produceMetrics({
-        killed: 34,
-        mutationScore: 64.15,
-        mutationScoreBasedOnCoveredCode: 64.15,
-        survived: 19,
-        totalCovered: 53,
-        totalDetected: 34,
-        totalMutants: 53,
-        totalUndetected: 19,
-        totalValid: 53
+        killed: 27,
+        timeout: 0,
+        mutationScore: 65.85,
+        mutationScoreBasedOnCoveredCode: 65.85,
+        survived: 14,
+        totalCovered: 41,
+        totalDetected: 27,
+        totalMutants: 41,
+        totalUndetected: 14,
+        totalValid: 41
       }),
     });
     /*
     ---------------|---------|----------|-----------|------------|----------|---------|
     File           | % score | # killed | # timeout | # survived | # no cov | # error |
     ---------------|---------|----------|-----------|------------|----------|---------|
-    All files      |   64.15 |       34 |         0 |         19 |        0 |       0 |
-    ---------------|---------|----------|-----------|------------|----------|---------|*/
+    All files      |   65.85 |       27 |         0 |         14 |        0 |       0 |*/
   });
 });
