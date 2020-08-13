720e0eb6f741b37b51b84ba8de6236088f65eaff
Nico Jansen <jansennico@gmail.com>
2020-08-13 18:29:44 +0200

    feat(mocha): deprecate mocha < v6
    BREAKING CHANGE: Mocha@<6 is deprecated and support for it will be removed in Stryker v5

--- packages/mocha-runner/src/LibWrapper.ts
+++ packages/mocha-runner/src/LibWrapper.ts
@@ -1,7 +1,11 @@
+import path = require('path');
+
 import * as Mocha from 'mocha';
 import glob = require('glob');
 
 import { MochaOptions } from '../src-generated/mocha-runner-options';
+
+const mochaRoot = path.dirname(require.resolve('mocha/package.json'));
 
 let loadOptions: undefined | ((argv?: string[] | string) => { [key: string]: any } | undefined);
 let collectFiles: undefined | ((options: MochaOptions) => string[]);
@@ -14,21 +18,21 @@ try {
    * @since 6.0.0'
    * @see https://mochajs.org/api/module-lib_cli_options.html#.loadOptions
    */
-  loadOptions = require('mocha/lib/cli/options').loadOptions;
+  loadOptions = require(`${mochaRoot}/lib/cli/options`).loadOptions;
 } catch {
   // Mocha < 6 doesn't support `loadOptions`
 }
 
 try {
   // https://github.com/mochajs/mocha/blob/master/lib/cli/run-helpers.js#L132
-  const runHelpers = require('mocha/lib/cli/run-helpers');
+  const runHelpers = require(`${mochaRoot}/lib/cli/run-helpers`);
   collectFiles = runHelpers.handleFiles;
   handleRequires = runHelpers.handleRequires; // handleRequires is available since mocha v7.2
   loadRootHooks = runHelpers.loadRootHooks; // loadRootHooks is available since mocha v7.2
 
   if (!collectFiles) {
     // Might be moved: https://github.com/mochajs/mocha/commit/15b96afccaf508312445770e3af1c145d90b28c6#diff-39b692a81eb0c9f3614247af744ab4a8
-    collectFiles = require('mocha/lib/cli/collect-files');
+    collectFiles = require(`${mochaRoot}/lib/cli/collect-files`);
   }
 } catch {
   // Mocha < 6 doesn't support `handleFiles`
