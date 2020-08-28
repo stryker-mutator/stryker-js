9b92ccdd269951dad3b91e8aaf21f9e9f188cef7
Nico Jansen <jansennico@gmail.com>
2020-08-28 11:59:36 +0200

    Enable perf test on express as submodule

---
+++ perf/tasks/merge-config.ts
@@ -0,0 +1,39 @@
+import { promises as fs } from 'fs';
+import path = require("path");
+
+const testRootDir = path.resolve(__dirname, '..', 'test');
+const configRootDir = path.resolve(__dirname, '..', 'config');
+
+mergeConfiguration().catch(error => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+async function mergeConfiguration() {
+  const testDirs = (await fs.readdir(testRootDir, { withFileTypes: true })).filter(testDir => testDir.isDirectory());
+  for await (const testDir of testDirs) {
+    const configOverrideDir = path.resolve(configRootDir, testDir.name);
+    try {
+      const configDir = await fs.stat(configOverrideDir);
+      if (configDir.isDirectory()) {
+        const overridePackageFileName = path.resolve(configOverrideDir, 'package.json');
+        const overrideStrykerConfigFileName = path.resolve(configOverrideDir, 'stryker.conf.json');
+        try {
+          const overrides = require(overridePackageFileName);
+          const original = require(path.resolve(testRootDir, testDir.name, 'package.json'));
+          await fs.writeFile(path.resolve(testRootDir, testDir.name, 'package.json'), JSON.stringify({ ...original, ...overrides }, null, 2))
+        } catch {
+          console.log(`Note: no overrides found at ${overridePackageFileName}`);
+        }
+        try {
+          await fs.copyFile(overrideStrykerConfigFileName, path.resolve(testRootDir, testDir.name, 'stryker.conf.json'));
+        } catch {
+          console.log(`Note: no stryker.conf.json file ${overrideStrykerConfigFileName}`);
+        }
+      }
+      console.log(`✅ Merged config for ${testDir.name}`)
+    } catch {
+      console.log(`Note: no config override directory found at ${configOverrideDir}`);
+    }
+  }
+}
--- perf/tasks/run-perf-tests.ts
+++ perf/tasks/run-perf-tests.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 import * as execa from 'execa';
 import { Observable } from 'rxjs';
 import { tap, throttleTime } from 'rxjs/operators';
+import * as minimatch from 'minimatch';
 
 const testRootDir = path.resolve(__dirname, '..', 'test');
 
@@ -14,7 +15,14 @@ runPerfTests()
   });
 
 async function runPerfTests() {
-  const testDirs = fs.readdirSync(testRootDir);
+  const globPattern = process.env.PERF_TEST_GLOB_PATTERN || '*';
+  const testDirs = fs.readdirSync(testRootDir).filter(testDir => minimatch(testDir, globPattern));
+  if (testDirs.length) {
+    console.log(`Running performance tests on ${testDirs.join(', ')}`)
+  } else {
+    console.warn(`No test files match glob expression ${globPattern}`);
+  }
+
   console.time('all tests');
   for (const testDir of testDirs) {
     await runTest(testDir);
@@ -24,19 +32,19 @@ async function runPerfTests() {
 
 async function runTest(testDir: string) {
   console.time(testDir);
-  await execNpm(['run', 'stryker'], testDir).pipe(
+  await npx(['stryker', 'run'], testDir).pipe(
     throttleTime(60000),
     tap(logMessage => console.timeLog(testDir, 'last log message: ', logMessage))
   ).toPromise();
   console.timeEnd(testDir);
 }
 
-function execNpm(args: string[], testDir: string): Observable<string> {
+function npx(args: string[], testDir: string): Observable<string> {
   const currentTestDir = path.resolve(testRootDir, testDir);
-  console.log(`Exec ${testDir} npm ${args.join(' ')}`);
+  console.log(`Exec ${testDir} npx ${args.join(' ')}`);
 
   return new Observable(observer => {
-    const testProcess = execa('npm', args, { timeout: 0, cwd: currentTestDir, stdio: 'pipe' });
+    const testProcess = execa('npx', args, { timeout: 0, cwd: currentTestDir, stdio: 'pipe' });
     let stderr = '';
     testProcess.stderr?.on('data', chunk => stderr += chunk.toString());
     testProcess.stdout?.on('data', chunk => observer.next(chunk.toString().trim()));
