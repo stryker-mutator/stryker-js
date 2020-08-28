7c55424a6deca5301af50206ea93905faaa0056b
Nico Jansen <jansennico@gmail.com>
2020-08-28 17:56:51 +0200

    feat(wct-runner): drop support for wct (#2440)
    BREAKING CHANGE: The @stryker-mutator/wct-runner package is dropped in Stryker 4.0. Please see https://github.com/stryker-mutator/stryker/issues/2386 for more details. Feel free to keep using @stryker-mutator/wct-runner@3 or start a community fork.  Note that [support for the web-component-tester itself is minimal](https://github.com/Polymer/tools/issues/3398), you might want to consider switching to a different test runner. Stryker still supports Mocha, Jest, Jasmine and Karma.

--- packages/wct-runner/src/index.ts
+++ packages/wct-runner/src/index.ts
@@ -1,9 +1,5 @@
-import { declareClassPlugin, PluginKind } from '@stryker-mutator/api/plugin';
-
-import * as strykerValidationSchema from '../schema/wct-runner-options.json';
-
-import WctTestRunner from './WctTestRunner';
-
-export const strykerPlugins = [declareClassPlugin(PluginKind.TestRunner, 'wct', WctTestRunner)];
-
-export { strykerValidationSchema };
+console.warn(
+  `The @stryker-mutator/wct-runner package is dropped in Stryker 4.0. Please see https://github.com/stryker-mutator/stryker/issues/2386 for more details. Feel free to keep using @stryker-mutator/wct-runner@3 or start a community fork. 
+  Note that [support for the web-component-tester itself is minimal](https://github.com/Polymer/tools/issues/3398), you might want to consider switching to a different test runner.
+  Stryker still supports Mocha, Jest, Jasmine and Karma.`
+);
--- packages/wct-runner/stryker.conf.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const settings = require('../../stryker.parent.conf');
-const moduleName = __dirname.split(path.sep).pop();
-settings.dashboard.module = moduleName;
-settings.coverageAnalysis = 'off';
-module.exports = settings;
