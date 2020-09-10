f487f99fc90c7ccdbbd6e19b3266dc9a6b4a297b
Nico Jansen <jansennico@gmail.com>
2020-09-10 14:16:09 +0200

    temp config files

--- packages/core/stryker.conf.js
+++ packages/core/stryker.conf.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const settings = require('../../stryker.parent.conf');
 const moduleName = __dirname.split(path.sep).pop();
+settings.plugins = settings.plugins.map(p => path.resolve(p));
 settings.dashboard.module = moduleName;
-settings.buildCommand = 'npm run build';
 module.exports = settings;
