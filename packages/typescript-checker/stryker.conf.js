fc24a85862431c8ef4a2ba2a344152aa04685f49
Nico Jansen <jansennico@gmail.com>
2020-09-13 12:16:40 +0200

    fix typescript-checker mutation test config

--- packages/typescript-checker/stryker.conf.js
+++ packages/typescript-checker/stryker.conf.js
@@ -2,6 +2,6 @@ const path = require('path');
 const settings = require('../../stryker.parent.conf');
 const moduleName = __dirname.split(path.sep).pop();
 settings.plugins = settings.plugins.map(p => path.resolve(p));
-settings.mochaOptions.spec = ['test/unit/**/*.js']
+settings.mochaOptions.spec = ['dist/test/**/*.js'];
 settings.dashboard.module = moduleName;
 module.exports = settings;
