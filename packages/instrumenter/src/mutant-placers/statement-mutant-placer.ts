cfd006eb37a40c0b9a78a266451599b80cca106f
Nico Jansen <jansennico@gmail.com>
2020-09-19 09:31:29 +0200

    refactor(instrumenter): cleanup `StatementMutantPlacer`

--- packages/instrumenter/src/mutant-placers/statement-mutant-placer.ts
+++ packages/instrumenter/src/mutant-placers/statement-mutant-placer.ts
@@ -1,6 +1,6 @@
 import { types } from '@babel/core';
 
-import { memberExpressionChain, createMutatedAst, mutationCoverageSequenceExpression, ID } from '../util/syntax-helpers';
+import { mutantTestExpression, createMutatedAst, mutationCoverageSequenceExpression } from '../util/syntax-helpers';
 
 import { MutantPlacer } from './mutant-placer';
 
@@ -16,28 +16,9 @@ const statementMutantPlacer: MutantPlacer = (path, mutants) => {
       ast: createMutatedAst(path, mutant),
     }));
 
-    // path.replaceWith(
-    //   types.blockStatement([
-    //     types.switchStatement(memberExpressionChain(ID.GLOBAL, ID.ACTIVE_MUTANT), [
-    //       ...appliedMutants.map(({ ast, mutant }) => types.switchCase(types.numericLiteral(mutant.id), [ast, types.breakStatement()])),
-    //       types.switchCase(null, [
-    //         // Add mutation covering statement
-    //         types.expressionStatement(mutationCoverageSequenceExpression(mutants)),
-    //         path.node,
-    //         types.breakStatement(),
-    //       ]),
-    //     ]),
-    //   ])
-    // );
-
     const instrumentedAst = appliedMutants.reduce(
       // Add if statements per mutant
-      (prev: types.Statement, { ast, mutant }) =>
-        types.ifStatement(
-          types.binaryExpression('===', memberExpressionChain(ID.GLOBAL, ID.ACTIVE_MUTANT), types.numericLiteral(mutant.id)),
-          types.blockStatement([ast]),
-          prev
-        ),
+      (prev: types.Statement, { ast, mutant }) => types.ifStatement(mutantTestExpression(mutant.id), types.blockStatement([ast]), prev),
       path.isBlockStatement()
         ? types.blockStatement([types.expressionStatement(mutationCoverageSequenceExpression(mutants)), ...path.node.body])
         : types.blockStatement([types.expressionStatement(mutationCoverageSequenceExpression(mutants)), path.node])
