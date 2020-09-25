06de66abbbe26812e0f9a60f765cde748c47be39
Simon de Lang <simondelang@gmail.com>
2020-09-25 11:40:37 +0200

    test(deprecation-warning): add case to verify if deprecation warning is logged

--- packages/jest-runner/test/unit/configLoaders/ReactScriptsTsJestConfigLoader.spec.ts
+++ packages/jest-runner/test/unit/configLoaders/ReactScriptsTsJestConfigLoader.spec.ts
@@ -2,6 +2,8 @@ import path from 'path';
 
 import { assert, expect } from 'chai';
 import sinon from 'sinon';
+import { Logger } from '@stryker-mutator/api/logging';
+import { logger } from '@stryker-mutator/test-helpers/src/factory';
 
 import ReactScriptsTSJestConfigLoader from '../../../src/configLoaders/ReactScriptsTSJestConfigLoader';
 import * as helper from '../../../src/utils/createReactJestConfig';
@@ -10,6 +12,7 @@ describe(ReactScriptsTSJestConfigLoader.name, () => {
   let sut: ReactScriptsTSJestConfigLoader;
   let requireResolveStub: sinon.SinonStub;
   let createReactJestConfigStub: sinon.SinonStub;
+  let loggerStub: sinon.SinonStubbedInstance<Logger>;
 
   const projectRoot = '/path/to/project';
   const reactScriptsTsPackagePath = './node_modules/react-scripts-ts/package.json';
@@ -24,14 +27,22 @@ describe(ReactScriptsTSJestConfigLoader.name, () => {
 
     requireResolveStub = sinon.stub();
     requireResolveStub.returns(reactScriptsTsPackagePath);
+    
+    loggerStub = logger();
 
-    sut = new ReactScriptsTSJestConfigLoader((requireResolveStub as unknown) as RequireResolve, projectRoot);
+    sut = new ReactScriptsTSJestConfigLoader(loggerStub, (requireResolveStub as unknown) as RequireResolve, projectRoot);
   });
 
   it('should load the configuration via the createJestConfig method provided by react-scripts-ts', () => {
     sut.loadConfig();
 
     assert(requireResolveStub.calledWith('react-scripts-ts/package.json'));
+  });
+
+  it('should log a deprecation warning', () => {
+    sut.loadConfig();
+
+    assert(loggerStub.warn.calledWith(`DEPRECATED: The support for create-react-app-ts projects is deprecated and will be removed in the future. Please migrate your project to create-react-app-ts and update your Stryker config setting to 'create-react-app'`));
   });
 
   it('should generate a configuration', () => {
