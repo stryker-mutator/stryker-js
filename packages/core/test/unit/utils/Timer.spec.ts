8c7024fbeee0dd5071bbbf42cfab4efa87bf07ff
Nico Jansen <jansennico@gmail.com>
2020-09-28 12:18:23 +0200

    Add unit test

--- packages/core/test/unit/utils/Timer.spec.ts
+++ packages/core/test/unit/utils/Timer.spec.ts
@@ -3,7 +3,7 @@ import * as sinon from 'sinon';
 
 import Timer from '../../../src/utils/Timer';
 
-describe('Timer', () => {
+describe(Timer.name, () => {
   let clock: sinon.SinonFakeTimers;
   let sut: Timer;
 
@@ -14,19 +14,27 @@ describe('Timer', () => {
 
   afterEach(() => clock.restore());
 
-  const arrangeActAssert = (elapsedMs: number, expectedTimeLabel: string) => {
-    describe(`after ${expectedTimeLabel}`, () => {
-      beforeEach(() => clock.tick(elapsedMs));
+  describe(Timer.prototype.humanReadableElapsed.name, () => {
+    const arrangeActAssert = (elapsedMs: number, expectedTimeLabel: string) => {
+      it(`should show "${expectedTimeLabel}" after ${elapsedMs} ms`, () => {
+        clock.tick(elapsedMs);
+        expect(sut.humanReadableElapsed()).to.be.eq(expectedTimeLabel);
+      });
+    };
 
-      it(`should show "${expectedTimeLabel}" when humanReadableElapsed()`, () => expect(sut.humanReadableElapsed()).to.be.eq(expectedTimeLabel));
+    arrangeActAssert(59999, '59 seconds');
+    arrangeActAssert(119999, '1 minute 59 seconds');
+    arrangeActAssert(120000, '2 minutes 0 seconds');
+    arrangeActAssert(121999, '2 minutes 1 second');
+    arrangeActAssert(61000, '1 minute 1 second');
+
+    it('should use the since marker when provided', () => {
+      clock.tick(1000);
+      sut.mark('foo');
+      clock.tick(1000);
+      expect(sut.humanReadableElapsed('foo')).eq('1 second');
     });
-  };
-
-  arrangeActAssert(59999, '59 seconds');
-  arrangeActAssert(119999, '1 minute 59 seconds');
-  arrangeActAssert(120000, '2 minutes 0 seconds');
-  arrangeActAssert(121999, '2 minutes 1 second');
-  arrangeActAssert(61000, '1 minute 1 second');
+  });
 
   describe('mark and elapsedMS', () => {
     it('should result in expected elapsedMS', () => {
