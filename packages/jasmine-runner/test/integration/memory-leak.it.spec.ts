071236bdf4406c0e0085f386068b1407c38b14ee
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2020-10-07 16:32:15 +0200

    fix file names

--- packages/jasmine-runner/test/integration/memory-leak.it.spec.ts
+++ packages/jasmine-runner/test/integration/memory-leak.it.spec.ts
@@ -8,7 +8,7 @@ import { JasmineTestRunner } from '../../src';
 
 describe(JasmineTestRunner.name, () => {
   it('should not leak memory when running multiple times (#2461)', async () => {
-    const childProcess = await execa.node(path.resolve(__dirname, 'MemoryLeak.worker.js'), [], {
+    const childProcess = await execa.node(path.resolve(__dirname, 'memory-leak.worker.js'), [], {
       stdio: 'pipe',
       nodeOptions: ['--max-old-space-size=32', '--max-semi-space-size=1'],
     });
