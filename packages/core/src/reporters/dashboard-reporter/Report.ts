4916d7fa59eedbf6d4ccbb73d3874e545bb2534d
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2020-10-07 16:23:40 +0200

    remove uppercase files

--- packages/core/src/reporters/dashboard-reporter/Report.ts
+++
@@ -1,6 +0,0 @@
-import { mutationTestReportSchema } from '@stryker-mutator/api/report';
-
-export interface MutationScoreOnlyReport {
-  mutationScore: number;
-}
-export type Report = mutationTestReportSchema.MutationTestResult | MutationScoreOnlyReport;
