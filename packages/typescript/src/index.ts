5862f715ec4a8df1abbd6ad692b0aa3263060805
Nico Jansen <jansennico@gmail.com>
2020-10-07 10:00:35 +0200

    chore: cleanup old packages

--- packages/typescript/src/index.ts
+++
@@ -1,1 +0,0 @@
-console.warn('The @stryker-mutator/typescript package does not have any function in Stryker 4.0. Please remove it from your devDependencies.');
--- packages/webpack-transpiler/src/index.ts
+++
@@ -1,3 +0,0 @@
-console.warn(
-  'Transpiler plugins are no longer supported since Stryker 4.0. Please use `"buildCommand": "webpack --config webpack.conf.js"` instead. You can remove "@stryker-mutator/webpack-transpiler" from your devDependencies.'
-);
