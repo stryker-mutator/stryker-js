46509e4e6b4308f5da49c7b82f37b17f89122f4c
Nico Jansen <jansennico@gmail.com>
2020-11-12 21:46:04 +0100

    Test with lower concurrency

--- packages/jasmine-runner/test/integration/memory-leak.it.spec.ts
+++ packages/jasmine-runner/test/integration/memory-leak.it.spec.ts
@@ -8,10 +8,6 @@ import { JasmineTestRunner } from '../../src';
 
 describe(JasmineTestRunner.name, () => {
   it('should not leak memory when running multiple times (#2461)', async function () {
-    if (process.version.startsWith('10')) {
-      console.log('OOM error test is unstable on node 10');
-      this.skip();
-    }
     const childProcess = await execa.node(path.resolve(__dirname, 'memory-leak.worker.js'), [], {
       stdio: 'pipe',
       nodeOptions: ['--max-old-space-size=32', '--max-semi-space-size=1'],
