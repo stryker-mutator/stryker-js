d89310f2ab7ffc4a41dd8fc65dea7e363b168530
Nico Jansen <jansennico@gmail.com>
2020-11-16 21:04:41 +0100

    Respect the package manager when installing the perf tests

---
+++ perf/tasks/install.ts
@@ -0,0 +1,38 @@
+import * as fs from 'fs';
+import * as minimatch from 'minimatch';
+import * as path from 'path';
+import * as execa from 'execa';
+
+const testRootDir = path.resolve(__dirname, '..', 'test');
+
+installAll()
+  .then(() => console.log('Done'))
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+/**
+ * Installs all packages under the "test" directory, while respecting their preferred package manager (yarn vs npm 🙄)
+ */
+async function installAll() {
+  const globPattern = process.env.PERF_TEST_GLOB_PATTERN || '*';
+  const testDirs = fs.readdirSync(testRootDir).filter(testDir => minimatch(testDir, globPattern));
+  if (testDirs.length) {
+    console.log(`Installing ${testDirs.join(', ')} (matched with glob pattern "${globPattern}")`)
+  } else {
+    console.warn(`No tests match glob expression ${globPattern}`);
+  }
+  await Promise.all(testDirs.map(install))
+}
+
+async function install(testDir: string) {
+  const testDirFullName = path.resolve(testRootDir, testDir);
+  const strykerConfig = require(path.resolve(testDirFullName, 'stryker.conf'));
+  const packageManager: string | undefined = strykerConfig.packageManager;
+  if(packageManager === 'yarn') {
+    await execa('yarn', ['install', '--frozenLockFile'], { cwd: testDirFullName });
+  }else {
+    await execa('npm', ['ci'], { cwd: testDirFullName });
+  }
+}
