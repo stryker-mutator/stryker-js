ccf20a8f216ab1b39453b3885cf8548a833dd24a
Nico Jansen <jansennico@gmail.com>
2020-11-19 19:01:20 +0100

    feat(jest-runner): resolve local jest version

--- packages/jest-runner/src/utils/jest-wrapper.ts
+++ packages/jest-runner/src/utils/jest-wrapper.ts
@@ -1,10 +1,14 @@
-import * as jest from 'jest';
+import { requireResolve } from '@stryker-mutator/util';
+import type * as jestModule from 'jest';
+
+// Use requireResolve, that way you can use this plugin from a different directory
+const jest = requireResolve('jest') as typeof jestModule;
 
 /**
  * Direct stubbing on jest is no longer possible since jest > 25
  */
 class JestWrapper {
-  public runCLI: typeof jest.runCLI = (...args) => {
+  public runCLI: typeof jestModule.runCLI = (...args) => {
     return jest.runCLI(...args);
   };
 }
