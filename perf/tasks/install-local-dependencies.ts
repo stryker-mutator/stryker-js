5964d559301b5ec457fa57e785f397f5f60f832a
Nico Jansen <jansennico@gmail.com>
2020-11-26 13:23:57 +0100

    test(perf): add benchmark for jest runner (#2618)
    * Add a git submodule for https://github.com/GoogleChrome/lighthouse, a big open source project that uses jest
    * Add functionality to run performance tests without installing stryker explicitly in the project
    * Add functionality to install dependencies with `yarn` instead of `npm` (used for lighthouse)

--- perf/tasks/install-local-dependencies.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapLocalDependencies } from '../../helpers/bootstrap-local-dependencies';
-import path = require('path');
-
-bootstrapLocalDependencies(path.resolve(__dirname, '..'))
-  .then(() => console.log('Installed local dependencies'))
-  .catch(err => console.error(err));
