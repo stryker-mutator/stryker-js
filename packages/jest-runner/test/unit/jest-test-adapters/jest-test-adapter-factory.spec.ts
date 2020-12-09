f9a25505a3c5e268f9b07b189864f7e590e61758
Nico Jansen <jansennico@gmail.com>
2020-12-09 21:40:49 +0100

    feat(jest-runner): guard for jest 24 when coverage analysis is enabled. Deprecate jest@<24

--- packages/jest-runner/test/unit/jest-test-adapters/jest-test-adapter-factory.spec.ts
+++ packages/jest-runner/test/unit/jest-test-adapters/jest-test-adapter-factory.spec.ts
@@ -13,6 +13,12 @@ describe(jestTestAdapterFactory.name, () => {
     return testInjector.injector.provideValue(pluginTokens.jestVersion, jestVersion).injectFunction(jestTestAdapterFactory);
   }
 
+  it('should log the jest version on debug', () => {
+    jestVersion = '25.0.0';
+    act();
+    expect(testInjector.logger.debug).calledWith('Detected Jest version %s', jestVersion);
+  });
+
   it('should return a JestGreaterThan25Adapter when the Jest version is higher or equal to 25.0.0', () => {
     jestVersion = '25.0.0';
     const testAdapter = act();
@@ -29,7 +35,32 @@ describe(jestTestAdapterFactory.name, () => {
   it('should throw an error when the Jest version is lower than 22.0.0', () => {
     jestVersion = '21.0.0';
 
-    expect(() => act()).to.throw(Error, 'You need Jest version >= 22.0.0 to use Stryker');
-    expect(testInjector.logger.debug).calledWith('Detected Jest below 22.0.0: %s', jestVersion);
+    expect(() => act()).to.throw(Error, 'You need Jest version >= 22.0.0 to use the @stryker-mutator/jest-runner plugin, found 21.0.0');
+  });
+
+  it('should throw an error when the Jest version is between 22 and 24, but coverage analysis is enabled', () => {
+    jestVersion = '23.0.0';
+    testInjector.options.coverageAnalysis = 'all';
+
+    expect(() => act()).to.throw(
+      Error,
+      'You need Jest version >= 24.0.0 to use the @stryker-mutator/jest-runner with "coverageAnalysis": "all", you\'re currently using version 23.0.0. Please upgrade your jest version, or set "coverageAnalysis": "off".'
+    );
+  });
+
+  it('should allow Jest version is between 22 and 24 if coverage analysis is "off"', () => {
+    jestVersion = '23.0.0';
+    testInjector.options.coverageAnalysis = 'off';
+
+    expect(() => act()).to.not.throw();
+  });
+
+  it('should log a deprecation warning when using jest version < 24', () => {
+    jestVersion = '23.1.2';
+    expect(() => act()).to.not.throw();
+    expect(testInjector.logger.warn).calledWith(
+      '[DEPRECATED] Support for Jest version < 24 is deprecated and will be removed in the next major version of Stryker, please upgrade your jest version (your current version is %s).',
+      '23.1.2'
+    );
   });
 });
