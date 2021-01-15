31e5f41527fa694807de6a61164753a1ec74a28f
Nico Jansen <jansennico@gmail.com>
2021-01-15 20:28:29 +0100

    further reduce stress of OOM tests

--- packages/jasmine-runner/testResources/big-project/lib/big-text.js
+++ packages/jasmine-runner/testResources/big-project/lib/big-text.js
@@ -1,6 +1,6 @@
 
 // Change the text each iteration to prevent nodejs from optimizing it into one value on the heap
 if (!global.n) {
-  global.n = 400000;
+  global.n = 300000;
 }
 module.exports.text = new Array(global.n++).fill('i').join(',');
