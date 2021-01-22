558d1835deed7b567cf7ef00c1cdd01b3267b898
Nico Jansen <jansennico@gmail.com>
2021-01-22 18:24:39 +0100

    Don't crash on regex parse error

--- packages/instrumenter/src/mutators/regex-mutator.ts
+++ packages/instrumenter/src/mutators/regex-mutator.ts
@@ -6,21 +6,31 @@ import { NodeMutation } from '../mutant';
 
 import { NodeMutator } from '.';
 
+// function isObviousRegexString(path: NodePath<types.StringLiteral>){
+//   if(path.parentPath.isCallExpression() && path.parentPath.isNewExpression())
+// }
+
 export class RegexMutator implements NodeMutator {
   public name = 'Regex';
 
   public mutate(path: NodePath): NodeMutation[] {
     if (path.isRegExpLiteral()) {
-      return mutate(path.node.pattern, { mutationLevels: [1] }).map((mutant) => {
-        const replacement = types.cloneNode(path.node, false);
-        replacement.pattern = mutant.pattern;
-        return {
-          original: path.node,
-          replacement,
-        };
-      });
-    } else {
-      return [];
-    }
+      try {
+        return mutate(path.node.pattern, { mutationLevels: [1] }).map((mutant) => {
+          const replacement = types.cloneNode(path.node, false);
+          replacement.pattern = mutant.pattern;
+          return {
+            original: path.node,
+            replacement,
+          };
+        });
+      } catch (err) {
+        console.error(
+          `[RegexMutator]: The Regex parser of weapon-regex couldn't parse this regex pattern: "${path.node.pattern}". Please report this issue at https://github.com/stryker-mutator/weapon-regex/issues. Inner error: ${err.message}`
+        );
+      }
+    }//else if(path.isStringLiteral() && isObviousRegexString(path)){}
+    return [];
   }
+
 }
