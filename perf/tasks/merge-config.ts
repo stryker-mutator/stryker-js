766c4147c22f77cb0d43af1943decf02d9f2a1b3
Nico Jansen <jansennico@gmail.com>
2021-01-27 12:16:10 +0100

    enable `--esModuleInterop` in performance tests

--- perf/tasks/merge-config.ts
+++ perf/tasks/merge-config.ts
@@ -1,5 +1,5 @@
-import { promises as fs } from 'fs';
-import path = require('path');
+import { promises as fsPromises } from 'fs';
+import path from 'path';
 
 const testRootDir = path.resolve(__dirname, '..', 'test');
 const configRootDir = path.resolve(__dirname, '..', 'config');
@@ -10,23 +10,23 @@ mergeConfiguration().catch((error) => {
 });
 
 async function mergeConfiguration() {
-  const testDirs = (await fs.readdir(testRootDir, { withFileTypes: true })).filter((testDir) => testDir.isDirectory());
+  const testDirs = (await fsPromises.readdir(testRootDir, { withFileTypes: true })).filter((testDir) => testDir.isDirectory());
   for await (const testDir of testDirs) {
     const configOverrideDir = path.resolve(configRootDir, testDir.name);
     try {
-      const configDir = await fs.stat(configOverrideDir);
+      const configDir = await fsPromises.stat(configOverrideDir);
       if (configDir.isDirectory()) {
         const overridePackageFileName = path.resolve(configOverrideDir, 'package.json');
         const overrideStrykerConfigFileName = path.resolve(configOverrideDir, 'stryker.conf.json');
         try {
           const overrides = require(overridePackageFileName);
           const original = require(path.resolve(testRootDir, testDir.name, 'package.json'));
-          await fs.writeFile(path.resolve(testRootDir, testDir.name, 'package.json'), JSON.stringify({ ...original, ...overrides }, null, 2));
+          await fsPromises.writeFile(path.resolve(testRootDir, testDir.name, 'package.json'), JSON.stringify({ ...original, ...overrides }, null, 2));
         } catch {
           console.log(`Note: no overrides found at ${overridePackageFileName}`);
         }
         try {
-          await fs.copyFile(overrideStrykerConfigFileName, path.resolve(testRootDir, testDir.name, 'stryker.conf.json'));
+          await fsPromises.copyFile(overrideStrykerConfigFileName, path.resolve(testRootDir, testDir.name, 'stryker.conf.json'));
         } catch {
           console.log(`Note: no stryker.conf.json file ${overrideStrykerConfigFileName}`);
         }
