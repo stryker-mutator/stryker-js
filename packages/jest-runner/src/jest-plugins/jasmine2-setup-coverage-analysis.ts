c9b36b5a17f430107c79a4c3591814bd535024cd
Gareth Parker <blackfireweb@gmail.com>
2021-04-29 17:16:06 +0100

    fix(jest-runner): Don't try to add a reporter to the jasmine env if the jasmine env doesn't exist

--- packages/jest-runner/src/jest-plugins/jasmine2-setup-coverage-analysis.ts
+++ packages/jest-runner/src/jest-plugins/jasmine2-setup-coverage-analysis.ts
@@ -1,5 +1,5 @@
 const instrumenterContext = global[global.__strykerGlobalNamespace__] ?? (global[global.__strykerGlobalNamespace__] = {});
-global.jasmine.getEnv().addReporter({
+global.jasmine?.getEnv().addReporter({
   specStarted(spec) {
     instrumenterContext.currentTestId = spec.fullName;
   },
