72785ff983d5b97bff74ab5211d5db3b3fde293e
Nico Jansen <jansennico@gmail.com>
2021-05-10 16:38:12 +0200

    fix linting warning

--- packages/instrumenter/src/mutators/optional-chaining-mutator.ts
+++ packages/instrumenter/src/mutators/optional-chaining-mutator.ts
@@ -7,10 +7,10 @@ import { NodeMutator } from './node-mutator';
 /**
  * Mutates optional chaining operators
  * Note that the AST for optional chaining might not be what you expect. Nodes of type `OptionalMemberExpression` can be either optional or not-optional
- * 
+ *
  * For example: In this expression: `foo?.bar.baz` the `.baz` member expression is of type `OptionalMemberExpression`, because it is part of an optional chain, but is is _not_ optional.
- * Only the `.bar` optional member expression is optional. 
- * 
+ * Only the `.bar` optional member expression is optional.
+ *
  * @example
  * foo?.bar -> foo.bar
  * foo?.[1] -> foo[1]
