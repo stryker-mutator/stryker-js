3f7b81eeb2950a62944393be6f83451d3af79550
Nico Jansen <jansennico@gmail.com>
2021-06-01 22:55:07 +0200

    chore(deps): update TS to 4.3

--- packages/util/src/known-keys.ts
+++ packages/util/src/known-keys.ts
@@ -2,8 +2,6 @@
  * Known keys filters out the index signature from the keys of a type
  * @see https://stackoverflow.com/questions/51465182/typescript-remove-index-signature-using-mapped-types
  */
-export type KnownKeys<T> = {
-  [K in keyof T]: string extends K ? never : number extends K ? never : K;
-} extends { [_ in keyof T]: infer U }
-  ? U
-  : never;
+export type KnownKeys<T> = keyof {
+  [P in keyof T as string extends P ? never : number extends P ? never : P]: T[P];
+};
