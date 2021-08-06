90ea439b2405781ce0efe9574432a2ad3e719f2a
Nico Jansen <jansennico@gmail.com>
2021-08-06 13:25:14 +0200

    Implement review comment: split actual

--- packages/api/test/unit/test_runner/run-result-helpers.spec.ts
+++ packages/api/test/unit/test_runner/run-result-helpers.spec.ts
@@ -14,60 +14,61 @@ describe('runResultHelpers', () => {
   describe(determineHitLimitReached.name, () => {
     it('should determine a timeout result when hit count is higher than limit', () => {
       const expected: TimeoutDryRunResult = { status: DryRunStatus.Timeout, reason: 'Hit limit reached (10/9)' };
-      expect(determineHitLimitReached(10, 9)).deep.eq(expected);
+      const actual = determineHitLimitReached(10, 9);
+      expect(actual).deep.eq(expected);
     });
     it('should not determine a timeout result when hit count is less than or equal to limit', () => {
-      expect(determineHitLimitReached(10, 10)).undefined;
+      const actual = determineHitLimitReached(10, 10);
+      expect(actual).undefined;
     });
   });
 
   describe(toMutantRunResult.name, () => {
     it('should convert "timeout" to "timeout"', () => {
       const expected: MutantRunResult = { status: MutantRunStatus.Timeout, reason: 'timeout reason' };
-      expect(toMutantRunResult({ status: DryRunStatus.Timeout, reason: 'timeout reason' })).deep.eq(expected);
+      const actual = toMutantRunResult({ status: DryRunStatus.Timeout, reason: 'timeout reason' });
+      expect(actual).deep.eq(expected);
     });
 
     it('should convert "error" to "error"', () => {
       const expected: MutantRunResult = { status: MutantRunStatus.Error, errorMessage: 'some error' };
-      expect(toMutantRunResult({ status: DryRunStatus.Error, errorMessage: 'some error' })).deep.eq(expected);
+      const actual = toMutantRunResult({ status: DryRunStatus.Error, errorMessage: 'some error' });
+      expect(actual).deep.eq(expected);
     });
 
     it('should report a failed test as "killed"', () => {
       const expected: MutantRunResult = { status: MutantRunStatus.Killed, failureMessage: 'expected foo to be bar', killedBy: '42', nrOfTests: 3 };
-      expect(
-        toMutantRunResult({
-          status: DryRunStatus.Complete,
-          tests: [
-            { status: TestStatus.Success, id: 'success1', name: 'success1', timeSpentMs: 42 },
-            { status: TestStatus.Failed, id: '42', name: 'error', timeSpentMs: 42, failureMessage: 'expected foo to be bar' },
-            { status: TestStatus.Success, id: 'success2', name: 'success2', timeSpentMs: 42 },
-          ],
-        })
-      ).deep.eq(expected);
+      const actual = toMutantRunResult({
+        status: DryRunStatus.Complete,
+        tests: [
+          { status: TestStatus.Success, id: 'success1', name: 'success1', timeSpentMs: 42 },
+          { status: TestStatus.Failed, id: '42', name: 'error', timeSpentMs: 42, failureMessage: 'expected foo to be bar' },
+          { status: TestStatus.Success, id: 'success2', name: 'success2', timeSpentMs: 42 },
+        ],
+      });
+      expect(actual).deep.eq(expected);
     });
 
     it('should report only succeeded tests as "survived"', () => {
       const expected: MutantRunResult = { status: MutantRunStatus.Survived, nrOfTests: 3 };
-      expect(
-        toMutantRunResult({
-          status: DryRunStatus.Complete,
-          tests: [
-            { status: TestStatus.Success, id: 'success1', name: 'success1', timeSpentMs: 42 },
-            { status: TestStatus.Success, id: '42', name: 'error', timeSpentMs: 42 },
-            { status: TestStatus.Success, id: 'success2', name: 'success2', timeSpentMs: 42 },
-          ],
-        })
-      ).deep.eq(expected);
+      const actual = toMutantRunResult({
+        status: DryRunStatus.Complete,
+        tests: [
+          { status: TestStatus.Success, id: 'success1', name: 'success1', timeSpentMs: 42 },
+          { status: TestStatus.Success, id: '42', name: 'error', timeSpentMs: 42 },
+          { status: TestStatus.Success, id: 'success2', name: 'success2', timeSpentMs: 42 },
+        ],
+      });
+      expect(actual).deep.eq(expected);
     });
 
     it('should report an empty suite as "survived"', () => {
       const expected: MutantRunResult = { status: MutantRunStatus.Survived, nrOfTests: 0 };
-      expect(
-        toMutantRunResult({
-          status: DryRunStatus.Complete,
-          tests: [],
-        })
-      ).deep.eq(expected);
+      const actual = toMutantRunResult({
+        status: DryRunStatus.Complete,
+        tests: [],
+      });
+      expect(actual).deep.eq(expected);
     });
 
     it("should set nrOfTests with the amount of tests that weren't `skipped`", () => {
