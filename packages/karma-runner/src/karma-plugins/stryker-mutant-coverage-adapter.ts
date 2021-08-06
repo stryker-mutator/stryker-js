8330c0215530ae576917a1ad4ac51de6e205950e
Nico Jansen <jansennico@gmail.com>
2021-08-06 13:27:21 +0200

    Review comment rename ns to context and add reasons for expect error

--- packages/karma-runner/src/karma-plugins/stryker-mutant-coverage-adapter.ts
+++ packages/karma-runner/src/karma-plugins/stryker-mutant-coverage-adapter.ts
@@ -1,8 +1,8 @@
 // THIS FILE IS LOADED IN THE BROWSER AND SHOULD NOT BE A NODE / ES MODULE
 
-// @ts-expect-error
+// @ts-expect-error window is not defined
 const originalComplete = window.__karma__.complete.bind(window.__karma__);
-// @ts-expect-error
+// @ts-expect-error window is not defined
 window.__karma__.complete = (...args) => {
   function firstArg() {
     if (!args.length) {
@@ -10,14 +10,14 @@ window.__karma__.complete = (...args) => {
     }
     return args[0];
   }
-  // @ts-expect-error
-  const ns = window.__stryker__ || {};
-  // @ts-expect-error
+  // @ts-expect-error window is not defined
+  const context = window.__stryker__ || {};
+  // @ts-expect-error window is not defined
   if (window.__strykerShouldReportCoverage__) {
-    firstArg().mutantCoverage = ns.mutantCoverage;
+    firstArg().mutantCoverage = context.mutantCoverage;
   }
-  if (ns.hitCount !== undefined) {
-    firstArg().hitCount = ns.hitCount;
+  if (context.hitCount !== undefined) {
+    firstArg().hitCount = context.hitCount;
   }
   originalComplete(...args);
 };
