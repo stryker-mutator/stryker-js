84b66efbda42dafa1566fde2b5e79c3b1209c062
Nico Jansen <jansennico@gmail.com>
2021-10-15 13:36:26 +0200

    Remove unnecessary

--- packages/mocha-runner/test/integration/timeout-on-infinite-loop.it.spec.ts
+++ packages/mocha-runner/test/integration/timeout-on-infinite-loop.it.spec.ts
@@ -18,42 +18,20 @@ describe('Infinite loop', () => {
     await sut.init();
   });
 
-  it.skip('should report a timeout eventually and be able to recover from it', async function () {
-    // TODO Mocha runner doesn't seem to detect timeout in infinite loop
-    this.timeout(1000);
-
-    // This is a slow test, so I decided to put 2 tests into one 🤷‍♀️
-    // Act
-    const result = await sut.mutantRun(factory.mutantRunOptions());
-
-    // Assert
-    assertions.expectTimeout(result);
-
-    // Second test, should be recovered by now.
-    const secondResult = await sut.mutantRun(factory.mutantRunOptions({ testFilter: ['should be able to recover and test others'] }));
-    assertions.expectSurvived(secondResult);
-    expect(secondResult.nrOfTests).eq(1);
-  });
-
   it('should be able to recover using a hit counter', async () => {
-    // Time this test, this should be fairly stable because there is large margin
-    const startTime = new Date();
-    const maxTestDurationMS = 5000;
+    // Arrange
+    const options = factory.mutantRunOptions({
+      activeMutant: factory.mutant({ id: '20' }),
+      testFilter: ['should be able to break out of an infinite loop with a hit counter'],
+      hitLimit: 10,
+    });
 
-    const result = await sut.mutantRun(
-      factory.mutantRunOptions({
-        activeMutant: factory.mutant({ id: '20' }),
-        testFilter: ['should be able to break out of an infinite loop with a hit counter'],
-        hitLimit: 10,
-      })
-    );
+    // Act
+    const result = await sut.mutantRun(options);
 
     // Assert
     assertions.expectTimeout(result);
     expect(result.reason).contains('Hit limit reached');
-    expect(new Date().valueOf() - startTime.valueOf(), 'Test took longer than 3 sec to complete, was the hit counter malfunctioning?').lt(
-      maxTestDurationMS
-    );
   });
 
   it('should reset hit counter state correctly between runs', async () => {
