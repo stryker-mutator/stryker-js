871782b708383a8c6b61cf4292f412dfe71cae9b
Frédéric Bonnet <fredericbonnet@free.fr>
2021-10-15 22:23:57 +0200

    feat(hit limit): cleanup

--- e2e/test/hit-limit/test/loop.jasmine.spec.js
+++
@@ -1,9 +0,0 @@
-const { loop } = require('../src/loop');
-
-describe('loop', () => {
-  it('should result in 15 for n=5 and a sum function', () => {
-    let result = 0;
-    loop(5, (n) => (result += n));
-    expect(result).toBe(15);
-  });
-});
--- packages/jasmine-runner/test/integration/timeout-on-infinite-loop.it.spec.ts
+++ packages/jasmine-runner/test/integration/timeout-on-infinite-loop.it.spec.ts
@@ -16,42 +16,20 @@ describe('Infinite loop', () => {
     await sut.dispose();
   });
 
-  it.skip('should report a timeout eventually and be able to recover from it', async function () {
-    // TODO Mocha runner doesn't seem to detect timeout in infinite loop
-    this.timeout(1000);
-
-    // This is a slow test, so I decided to put 2 tests into one 🤷‍♀️
-    // Act
-    const result = await sut.mutantRun(factory.mutantRunOptions());
-
-    // Assert
-    assertions.expectTimeout(result);
-
-    // Second test, should be recovered by now.
-    const secondResult = await sut.mutantRun(factory.mutantRunOptions({ testFilter: ['spec1'] }));
-    assertions.expectSurvived(secondResult);
-    expect(secondResult.nrOfTests).eq(1);
-  });
-
   it('should be able to recover using a hit counter', async () => {
-    // Time this test, this should be fairly stable because there is large margin
-    const startTime = new Date();
-    const maxTestDurationMS = 5000;
+    // Arrange
+    const options = factory.mutantRunOptions({
+      activeMutant: factory.mutant({ id: '19' }),
+      testFilter: ['spec2'],
+      hitLimit: 10,
+    });
 
-    const result = await sut.mutantRun(
-      factory.mutantRunOptions({
-        activeMutant: factory.mutant({ id: '19' }),
-        testFilter: ['spec2'],
-        hitLimit: 10,
-      })
-    );
+    // Act
+    const result = await sut.mutantRun(options);
 
     // Assert
     assertions.expectTimeout(result);
     expect(result.reason).contains('Hit limit reached');
-    expect(new Date().valueOf() - startTime.valueOf(), 'Test took longer than 3 sec to complete, was the hit counter malfunctioning?').lt(
-      maxTestDurationMS
-    );
   });
 
   it('should reset hit counter state correctly between runs', async () => {
