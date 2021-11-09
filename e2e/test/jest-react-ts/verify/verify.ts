1bd780ea62b305bb32425953e2c9dcbd907131e1
Nico Jansen <jansennico@gmail.com>
2021-11-09 13:36:33 +0100

    Fix jest-react-ts

--- e2e/test/jest-react-ts/verify/verify.ts
+++ e2e/test/jest-react-ts/verify/verify.ts
@@ -1,8 +1,8 @@
-import { expectMetrics } from '../../../helpers';
+import { expectMetricsJson } from '../../../helpers';
 
 describe('After running stryker on jest-react-ts project', () => {
   it('should report expected scores', async () => {
-    await expectMetrics({
+    await expectMetricsJson({
       killed: 63,
       noCoverage: 19,
       survived: 25,
