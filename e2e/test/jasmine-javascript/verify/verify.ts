36e821a89a970f9aae5497e614401fc08b9efb10
Nico Jansen <jansennico@gmail.com>
2021-12-03 09:09:10 +0100

    test(e2e): make jasmine-javascript cope with not having a package-lock.json file

--- e2e/test/jasmine-javascript/verify/verify.ts
+++ e2e/test/jasmine-javascript/verify/verify.ts
@@ -1,11 +1,11 @@
 import { promises as fsPromises } from 'fs';
 
 import { expect } from 'chai';
-import { expectMetrics } from '../../../helpers';
+import { expectMetricsJson } from '../../../helpers';
 
 describe('After running stryker with test runner jasmine, test framework jasmine', () => {
   it('should report 85% mutation score', async () => {
-    await expectMetrics({
+    await expectMetricsJson ({
       killed: 12,
       mutationScore: 85.71,
       mutationScoreBasedOnCoveredCode: 100,
@@ -21,7 +21,7 @@ describe('After running stryker with test runner jasmine, test framework jasmine
 
   it('should write to a log file', async () => {
     const strykerLog = await fsPromises.readFile('./stryker.log', 'utf8');
-    expect(strykerLog).matches(/INFO InputFileResolver Found 2 of 9 file\(s\) to be mutated/);
+    expect(strykerLog).matches(/INFO InputFileResolver Found 2 of \d+ file\(s\) to be mutated/);
     expect(strykerLog).matches(/Done in \d+ second/);
     // TODO, we now have an error because of a memory leak: https://github.com/jasmine/jasmine-npm/issues/134
     // expect(strykerLog).not.contains('ERROR');
