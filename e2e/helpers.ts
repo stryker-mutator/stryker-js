d8a84c04bc4993ca782847279ce0ef1156a749c4
Nico Jansen <jansennico@gmail.com>
2022-01-26 19:49:36 +0100

    Add even more snaps

--- e2e/helpers.ts
+++ e2e/helpers.ts
@@ -73,29 +73,8 @@ export async function readMutationTestingJsonResult(jsonReportFile = path.resolv
   return metricsResult;
 }
 
-type WritableMetricsResult = {
-  -readonly [K in keyof MetricsResult]: MetricsResult[K];
-};
-
 export function readLogFile(fileName = path.resolve('stryker.log')): Promise<string> {
   return fsPromises.readFile(fileName, 'utf8');
-}
-
-export async function expectMetricsResult(expectedMetricsResult: Partial<MetricsResult>) {
-  const actualMetricsResult = await readMutationTestResult();
-  const actualSnippet: Partial<WritableMetricsResult> = {};
-  for (const key in expectedMetricsResult) {
-    actualSnippet[key as keyof MetricsResult] = actualMetricsResult.systemUnderTestMetrics[key as keyof MetricsResult] as any;
-  }
-  if (actualSnippet.metrics) {
-    if (typeof actualSnippet.metrics.mutationScore === 'number') {
-      actualSnippet.metrics.mutationScore = parseFloat(actualSnippet.metrics.mutationScore.toFixed(2));
-    }
-    if (typeof actualSnippet.metrics.mutationScoreBasedOnCoveredCode === 'number') {
-      actualSnippet.metrics.mutationScoreBasedOnCoveredCode = parseFloat(actualSnippet.metrics.mutationScoreBasedOnCoveredCode.toFixed(2));
-    }
-  }
-  expect(actualSnippet).deep.eq(expectedMetricsResult);
 }
 
 export async function expectMetricsJsonToMatchSnapshot() {
--- e2e/test/typescript-transpiling/verify/verify.ts
+++ e2e/test/typescript-transpiling/verify/verify.ts
@@ -1,23 +1,8 @@
-import { expectMetricsResult, produceMetrics } from '../../../helpers';
+import { expectMetricsJsonToMatchSnapshot } from '../../../helpers';
 
 describe('Verify stryker has ran correctly', () => {
 
   it('should report correct score', async () => {
-    await expectMetricsResult({
-      metrics: produceMetrics({
-        compileErrors: 10,
-        killed: 42,
-        mutationScore: 75,
-        mutationScoreBasedOnCoveredCode: 91.3,
-        survived: 4,
-        noCoverage: 10,
-        totalCovered: 46,
-        totalDetected: 42,
-        totalMutants: 66,
-        totalUndetected: 14,
-        totalInvalid: 10,
-        totalValid: 56
-      })
-    });
+    await expectMetricsJsonToMatchSnapshot();
   });
 });
