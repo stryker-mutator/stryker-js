c4f2be81b72ea496319b10073956228cccf6d387
Edgar Hipp <hipp.edg@gmail.com>
2022-01-26 20:14:47 +0100

    test(e2e): update tests to use snapshots (#3360)
    Use snapshots to verify e2e test outcome when appropriate.
    
    Co-authored-by: Nico Jansen <jansennico@gmail.com>

--- e2e/test/jest-with-ts/verify/verify.ts
+++ e2e/test/jest-with-ts/verify/verify.ts
@@ -1,15 +1,7 @@
-import { expectMetricsJson } from '../../../helpers';
+import { expectMetricsJsonToMatchSnapshot } from '../../../helpers';
 
 describe('Verify stryker has ran correctly', () => {
   it('should report correct score', async () => {
-    await expectMetricsJson({
-      ignored: 0,
-      killed: 14,
-      mutationScore: 58.33,
-      noCoverage: 0,
-      survived: 10,
-      timeout: 0,
-      compileErrors: 35,
-    });
+    await expectMetricsJsonToMatchSnapshot();
   });
 });
