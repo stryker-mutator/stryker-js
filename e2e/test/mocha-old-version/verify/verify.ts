84786922ad8f8673c9a5b59a44449ce5112615d7
Nico Jansen <jansennico@gmail.com>
2022-01-28 15:20:18 +0100

    Update the mocha old version e2e test, now 7.2

--- e2e/test/mocha-old-version/verify/verify.ts
+++ e2e/test/mocha-old-version/verify/verify.ts
@@ -1,16 +1,7 @@
-import { promises as fsPromises } from 'fs';
-import { expect } from 'chai';
+import { expectMetricsJsonToMatchSnapshot } from '../../../helpers';
 
-describe('Verify stryker runs with mocha < 6', () => {
-
-  let strykerLog: string;
-
-  before(async () => {
-    strykerLog = await fsPromises.readFile('./stryker.log', 'utf8');
+describe('Verify stryker runs on the lowest supported mocha version', () => {
+  it('should have run correctly', () => {
+    expectMetricsJsonToMatchSnapshot();
   });
-
-  it('should warn about old mocha version', async () => {
-    expect(strykerLog).contains('DEPRECATED: Mocha < 6 detected. Please upgrade to at least Mocha version 6. Stryker will drop support for Mocha < 6 in V5');
-  });
-
 });
