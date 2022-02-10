9021cb27980752eef0d633ec210e8f16ae01470a
Nico Jansen <jansennico@gmail.com>
2022-02-10 11:23:52 +0100

    Fix cjs stryker config in typescript checker

--- packages/typescript-checker/stryker.conf.cjs
+++ packages/typescript-checker/stryker.conf.cjs
@@ -1,6 +1,6 @@
 const path = require('path');
 const settings = require('../../stryker.parent.conf');
-const moduleName = path.dirname(fileURLToPath(import.meta.url)).split(path.sep).pop();
+const moduleName = __dirname.split(path.sep).pop();
 settings.plugins = settings.plugins.map(p => path.resolve(p));
 settings.mochaOptions.spec = ['dist/test/**/*.js'];
 settings.dashboard.module = moduleName;
