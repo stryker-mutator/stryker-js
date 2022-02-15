10910566fcaf427f7e643b2e1b8f0284b462a322
Nico Jansen <jansennico@gmail.com>
2022-02-15 09:18:37 +0100

    Add integration tests to typescript-checker mutation testing

--- packages/typescript-checker/stryker.conf.js
+++ packages/typescript-checker/stryker.conf.js
@@ -5,6 +5,7 @@ import { URL } from 'url';
 
 const settings = JSON.parse(fs.readFileSync(new URL('../../stryker.parent.conf.json', import.meta.url), 'utf-8'));
 settings.dashboard.module = import.meta.url.split('/').slice(-2)[0];
+settings.mochaOptions.spec = ['dist/test/**/*.js'];
 /**
  * @type {import('../api/dist/src/core/index.js').PartialStrykerOptions}
  */
