5fecd520abd1826ee4c8296d7f1bbee197a300dc
Nico Jansen <jansennico@gmail.com>
2022-02-18 18:33:04 +0100

    feat(hit limit): infinite loop prevention in jest-runner (#3439)
    Add infinite loop prevention using a hit counter to `@stryker-mutator/jest-runner`.
    
    Special thanks to @erie0210 for doing all this work, just not having time to finish it.

--- packages/jest-runner/src/jest-plugins/mixin-jest-environment.ts
+++ packages/jest-runner/src/jest-plugins/mixin-jest-environment.ts
@@ -1,5 +1,6 @@
 import type { JestEnvironment, EnvironmentContext } from '@jest/environment';
 import type { Config, Circus } from '@jest/types';
+import { InstrumenterContext } from '@stryker-mutator/api/core';
 
 import { state } from '../messaging';
 
@@ -24,28 +25,44 @@ export function mixinJestEnvironment<T extends typeof JestEnvironment>(JestEnvir
     return JestEnvironmentClass;
   } else {
     class StrykerJestEnvironment extends JestEnvironmentClass {
-      private readonly fileName: string;
+      private readonly strykerFileName: string;
+
+      /**
+       * The shared instrumenter context with the test environment (the `__stryker__` global variable)
+       */
+      private readonly strykerContext: InstrumenterContext;
 
       public static readonly [STRYKER_JEST_ENV] = true;
 
       constructor(config: Config.ProjectConfig, context?: EnvironmentContext) {
         super(config, context);
-        this.fileName = context!.testPath!;
+        this.strykerFileName = context!.testPath!;
+        this.strykerContext = this.global[this.global.__strykerGlobalNamespace__] = this.global[this.global.__strykerGlobalNamespace__] ?? {};
       }
 
       public handleTestEvent: Circus.EventHandler = async (event: Circus.Event, eventState: Circus.State) => {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         await super.handleTestEvent?.(event as any, eventState);
         if (state.coverageAnalysis === 'perTest' && event.name === 'test_start') {
-          const ns = (this.global[this.global.__strykerGlobalNamespace__] = this.global[this.global.__strykerGlobalNamespace__] ?? {});
-          ns.currentTestId = fullName(event.test);
+          this.strykerContext.currentTestId = fullName(event.test);
         }
       };
 
       public async teardown() {
-        const mutantCoverage = this.global[this.global.__strykerGlobalNamespace__]?.mutantCoverage;
-        state.handleMutantCoverage(this.fileName, mutantCoverage);
+        const mutantCoverage = this.strykerContext.mutantCoverage;
+        state.hitCount = this.strykerContext.hitCount;
+        state.hitLimit = this.strykerContext.hitLimit;
+        state.handleMutantCoverage(this.strykerFileName, mutantCoverage);
         await super.teardown();
+      }
+
+      public async setup() {
+        await super.setup();
+        if (state.firstTestFile) {
+          this.strykerContext.hitCount = 0;
+          this.strykerContext.hitLimit = state.hitLimit;
+          state.firstTestFile = false;
+        }
       }
     }
     return StrykerJestEnvironment;
--- packages/jest-runner/stryker.conf.js
+++ packages/jest-runner/stryker.conf.js
@@ -3,5 +3,4 @@ const settings = require('../../stryker.parent.conf');
 const moduleName = __dirname.split(path.sep).pop();
 settings.plugins = settings.plugins.map(p => path.resolve(p));
 settings.dashboard.module = moduleName;
-settings.files = ['{src-generated,src,test,schema}/**/*.{ts,json}', '*.{ts,json}', '!**/*.d.ts']
 module.exports = settings;
