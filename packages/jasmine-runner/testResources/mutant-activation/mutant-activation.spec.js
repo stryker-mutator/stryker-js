56f09234505e2baddd91d1be33deb8e71ebacb7f
Nico Jansen <jansennico@gmail.com>
2022-02-21 15:28:45 +0100

    Fix syntax error

--- packages/jasmine-runner/testResources/mutant-activation/mutant-activation.spec.js
+++ packages/jasmine-runner/testResources/mutant-activation/mutant-activation.spec.js
@@ -1,9 +1,15 @@
 describe('mutant-activation', () => {
-  const staticActiveMutant = globalThis.__stryker2__ && globalThis.__stryker2__.activeMutant ?? null;
+  function getActiveMutant() {
+    if (globalThis.__stryker2__ && globalThis.__stryker2__.activeMutant) {
+      return globalThis.__stryker2__.activeMutant;
+    }
+    return null;
+  }
+  const staticActiveMutant = getActiveMutant();
 
   it('should report active mutants', () => {
-    const runtimeActiveMutant = globalThis.__stryker2__ && globalThis.__stryker2__.activeMutant ?? null;
+    const runtimeActiveMutant = getActiveMutant();
 
-    throw new Error(JSON.stringify({ staticActiveMutant, runtimeActiveMutant}));
+    throw new Error(JSON.stringify({ staticActiveMutant, runtimeActiveMutant }));
   });
 });
