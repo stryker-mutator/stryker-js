c949dc3eb948db274ae23a133374fcb7fa9749b2
Nico Jansen <jansennico@gmail.com>
2022-02-22 17:57:09 +0100

    Add logging to debug karma issues

--- packages/karma-runner/src/starters/karma-starter.ts
+++ packages/karma-runner/src/starters/karma-starter.ts
@@ -1,3 +1,5 @@
+import { createRequire } from 'module';
+
 import { Task } from '@stryker-mutator/util';
 import type { Config, ConfigOptions } from 'karma';
 
@@ -15,6 +17,17 @@ export const karmaConfigStarter: ProjectStarter = {
     };
     if (karma.config?.parseConfig) {
       config = await karma.config.parseConfig(configFile, {}, { promiseConfig: true, throwErrors: true });
+      config.logLevel = 'debug';
+      const require = createRequire(import.meta.url);
+      const log4js = require(require.resolve('log4js', { paths: [require.resolve('karma')] }));
+      log4js.configure({
+        appenders: {
+          out: { type: 'stdout' },
+        },
+        categories: {
+          default: { appenders: ['out'], level: 'debug' },
+        },
+      });
     }
 
     const exitTask = new Task<number>();
