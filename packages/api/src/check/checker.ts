12b56313351f138c6742bd7e3d34ae38f10bd101
Nico Jansen <jansennico@gmail.com>
2022-02-25 17:00:06 +0100

    Document api

--- packages/api/src/check/checker.ts
+++ packages/api/src/check/checker.ts
@@ -5,7 +5,18 @@ import { CheckResult } from './check-result.js';
 export interface Checker {
   init(): Promise<void>;
 
+  /**
+   * Check a mutant group.
+   * @param mutants A group of mutants to check
+   * @returns key-value pairs of mutant ids with the check results
+   */
   check(mutants: Mutant[]): Promise<Record<string, CheckResult>>;
 
-  createGroups?(mutants: Mutant[]): Promise<Mutant[][] | undefined>;
+  /**
+   * Group mutants into groups to check.
+   * If missing, groups of singular mutants are assumed.
+   * @param mutants the mutants to group
+   * @returns the groups of mutant ids
+   */
+  group?(mutants: Mutant[]): Promise<string[][] | undefined>;
 }
