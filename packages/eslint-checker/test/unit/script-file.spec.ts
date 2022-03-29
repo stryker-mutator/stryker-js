1334e57ae2110c789d577c94771d0ab820df3c9a
Tim Lehner <timothy.lehner.2011@live.rhul.ac.uk>
2022-03-29 17:03:36 +0100

    feat: add script-file inspired by typescript-checker

---
+++ packages/eslint-checker/test/unit/script-file.spec.ts
@@ -0,0 +1,47 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import { Mutant } from '@stryker-mutator/api/core';
+
+import { ScriptFile } from '../../src/script-file.js';
+
+describe('ScriptFile unit tests', () => {
+  const initialContents = 'add(x, y) { return x + y; }';
+  let testFile: ScriptFile;
+
+  beforeEach(() => {
+    testFile = new ScriptFile(initialContents, 'add.js');
+  });
+
+  it("should create a 'file'", () => {
+    expect(testFile.content).to.equal(initialContents);
+  });
+
+  it('should remember the original contents on update', () => {
+    testFile.write('updated');
+    testFile.resetMutant();
+    expect(testFile.content).to.equal(initialContents);
+  });
+
+  it('should apply a mutation', () => {
+    const mutation: Mutant = {
+      fileName: testFile.fileName,
+      id: 'mutation-id',
+      location: {
+        start: {
+          line: 0,
+          column: testFile.content.indexOf('{'),
+        },
+        end: {
+          line: 0,
+          column: testFile.content.indexOf('}') + 1,
+        },
+      },
+      mutatorName: 'test-mutator',
+      replacement: '{}',
+    };
+
+    testFile.mutate(mutation);
+
+    expect(testFile.content).to.equal('add(x, y) {}');
+  });
+});
