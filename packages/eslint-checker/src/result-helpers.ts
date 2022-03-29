119200ac3a1d0b15c72fd31df1980f1b73ac5d86
Tim Lehner <timothy.lehner.2011@live.rhul.ac.uk>
2022-03-29 17:47:52 +0100

    feat: add functionality to map lint result to stryker check

--- packages/eslint-checker/src/result-helpers.ts
+++ packages/eslint-checker/src/result-helpers.ts
@@ -1,5 +1,22 @@
 import { CheckResult, CheckStatus, FailedCheckResult } from '@stryker-mutator/api/check';
+import { ESLint } from 'eslint';
 
 export function isFailedResult(result: CheckResult): result is FailedCheckResult {
   return result.status === CheckStatus.CompileError;
+}
+
+export async function makeResultFromLintReport(reports: ESLint.LintResult[], formatter: ESLint.Formatter): Promise<CheckResult> {
+  const failures = reports.filter((_result) => _result.errorCount > 0);
+
+  if (failures.length > 0) {
+    const reason = await formatter.format(failures);
+    return {
+      status: CheckStatus.CompileError,
+      reason,
+    };
+  }
+
+  return {
+    status: CheckStatus.Passed,
+  };
 }
--- packages/eslint-checker/test/unit/result-helper.spec.ts
+++ packages/eslint-checker/test/unit/result-helper.spec.ts
@@ -1,8 +1,13 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import { ESLint } from 'eslint';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
 
 import { CheckStatus } from '@stryker-mutator/api/check';
 
-import { isFailedResult } from '../../src/result-helpers.js';
+import { isFailedResult, makeResultFromLintReport } from '../../src/result-helpers.js';
 
 describe('eslint-checker unit tests', () => {
   describe('isFailedResult', () => {
@@ -12,6 +17,50 @@ describe('eslint-checker unit tests', () => {
         status: CheckStatus.CompileError,
       });
       expect(result).to.be.true;
+    });
+  });
+
+  describe('makeResultFromLintReports', () => {
+    function makeFakeLintResult(partial?: Partial<ESLint.LintResult>): ESLint.LintResult {
+      return {
+        errorCount: 0,
+        ...partial,
+      } as ESLint.LintResult;
+    }
+
+    it('should pass for a good report', async () => {
+      const lintReports: ESLint.LintResult[] = [makeFakeLintResult()];
+      const checkResult = await makeResultFromLintReport(lintReports, {} as ESLint.Formatter);
+      expect(checkResult.status).to.equal(CheckStatus.Passed);
+    });
+
+    it('should be rejected and include all the messages for a bad report', async () => {
+      const lintReports: ESLint.LintResult[] = [makeFakeLintResult({ errorCount: 1 })];
+      const checkResult = await makeResultFromLintReport(lintReports, { format: () => '' });
+      expect(checkResult.status).to.equal(CheckStatus.CompileError);
+    });
+
+    it('should use the formatter for the reason', async () => {
+      const formatSpy = sinon.spy();
+      const lintReports: ESLint.LintResult[] = [makeFakeLintResult({ errorCount: 1 })];
+      await makeResultFromLintReport(lintReports, { format: formatSpy });
+      expect(formatSpy).to.have.been.called;
+    });
+
+    it('should use the formatter for all failed results', async () => {
+      const formatSpy = sinon.spy();
+      const failures = [makeFakeLintResult({ errorCount: 1 }), makeFakeLintResult({ errorCount: 1 })];
+      const lintReports: ESLint.LintResult[] = failures;
+      await makeResultFromLintReport(lintReports, { format: formatSpy });
+      expect(formatSpy).to.have.been.calledWith(failures);
+    });
+
+    it('should not use the formatter for any passed results', async () => {
+      const formatSpy = sinon.spy();
+      const failures = [makeFakeLintResult({ errorCount: 1 }), makeFakeLintResult({ errorCount: 1 })];
+      const lintReports: ESLint.LintResult[] = [...failures, makeFakeLintResult({ errorCount: 0 })];
+      await makeResultFromLintReport(lintReports, { format: formatSpy });
+      expect(formatSpy).to.have.been.calledWith(failures);
     });
   });
 });
