cf1b73ae188892c9d7274a8b42efa56724e771d2
Nico Jansen <jansennico@gmail.com>
2022-03-30 11:09:46 +0200

    Test helpers

--- packages/test-helpers/src/factory.ts
+++ packages/test-helpers/src/factory.ts
@@ -190,8 +190,9 @@ export function logger(): sinon.SinonStubbedInstance<Logger> {
   };
 }
 
-export function testRunner(): sinon.SinonStubbedInstance<Required<TestRunner>> {
+export function testRunner(index = 0): sinon.SinonStubbedInstance<Required<TestRunner> & { index: number }> {
   return {
+    index,
     capabilities: sinon.stub(),
     init: sinon.stub(),
     dryRun: sinon.stub(),
