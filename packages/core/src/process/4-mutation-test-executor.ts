dce5882f103097fe7ec9aba56b5bd7cedfb22877
Nico Jansen <jansennico@gmail.com>
2022-03-30 17:21:24 +0200

    fix(reporter): report progress of failed check results only once (#3472)
    Share the replay of failed check results across all subscribers using the `shareReplay()` rxjs operator.

--- packages/core/src/process/4-mutation-test-executor.ts
+++ packages/core/src/process/4-mutation-test-executor.ts
@@ -126,7 +126,10 @@ export class MutationTestExecutor {
     let passedMutant$ = input$;
     for (const checkerName of this.options.checkers) {
       // Use this checker
-      const [checkFailedResult$, checkPassedResult$] = partition(this.executeSingleChecker(checkerName, passedMutant$), isEarlyResult);
+      const [checkFailedResult$, checkPassedResult$] = partition(
+        this.executeSingleChecker(checkerName, passedMutant$).pipe(shareReplay()),
+        isEarlyResult
+      );
 
       // Prepare for the next one
       passedMutant$ = checkPassedResult$;
@@ -159,7 +162,6 @@ export class MutationTestExecutor {
       .schedule(group$, (checker, group) => checker.check(checkerName, group))
       .pipe(
         mergeMap((mutantGroupResults) => mutantGroupResults),
-        shareReplay(),
         map(([mutantRunPlan, checkResult]) =>
           checkResult.status === CheckStatus.Passed
             ? mutantRunPlan
