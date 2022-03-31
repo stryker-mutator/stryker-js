a47fdaa4bde18c7a00043cd0ac1503740fc597c8
Tim Lehner <timothy.lehner.2011@live.rhul.ac.uk>
2022-03-31 14:07:14 +0100

    docs: add schema for LintCheckerOptions
     this adds documentation for the `lintConfigFile` option on the eslint-checker plugin

--- packages/eslint-checker/src/index.ts
+++ packages/eslint-checker/src/index.ts
@@ -1,7 +1,13 @@
+import fs from 'fs';
+
 import { PluginKind, declareFactoryPlugin } from '@stryker-mutator/api/plugin';
 
 import { create } from './eslint-checker.js';
 
 export const strykerPlugins = [declareFactoryPlugin(PluginKind.Checker, 'eslint', create)];
+
+export const strykerValidationSchema: typeof import('../schema/eslint-checker-options.json') = JSON.parse(
+  fs.readFileSync(new URL('../schema/eslint-checker-options.json', import.meta.url), 'utf-8')
+);
 
 export const createLintChecker = create;
