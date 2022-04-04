8b1533702f04b834f15d8ef95ba388c2b72f9b0d
Tim Lehner <timothy.lehner.2011@live.rhul.ac.uk>
2022-04-04 22:46:51 +0100

    test: add e2e test for mocha-javascript with eslint checker

---
+++ e2e/test/mocha-javascript-eslint/src/IsEven.js
@@ -0,0 +1,17 @@
+module.exports.isEven = function (number) {
+  // Note: Implemented with a case switch statement, in order to reproduce this issue:
+  // https://github.com/stryker-mutator/stryker-js/issues/2469#issuecomment-690303849
+  let mod2 = number % 2;
+  let isEven;
+  switch (mod2) {
+    case 0: {
+      isEven = true;
+      break;
+    }
+    case 1: {
+      isEven = false;
+      break;
+    }
+  }
+  return isEven;
+};
