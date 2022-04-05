009fc875d7be46e4a510fbb6260864771b07b14e
Tim Lehner <timothy.lehner.2011@live.rhul.ac.uk>
2022-04-05 17:16:08 +0100

    fix: add no-unused-vars to default ignored rules
    - leads to many incorrect false negatives in javascript files

--- packages/eslint-checker/src/esconfig-helpers.ts
+++ packages/eslint-checker/src/esconfig-helpers.ts
@@ -4,6 +4,7 @@ import { CosmiconfigResult } from 'cosmiconfig/dist/types';
 // Override some compiler options that have to do with code quality. When mutating, we're not interested in the resulting code quality
 // See https://github.com/stryker-mutator/stryker-js/issues/391 for more info
 const LINT_RULES_OVERRIDES: Readonly<Partial<Record<string, string>>> = Object.freeze({
+  'no-unused-vars': 'off',
   'import/no-unresolved': 'off',
   '@typescript-eslint/no-unused-vars': 'off',
   '@typescript-eslint/no-empty-function': 'off',
