7a067f7a1b22fde937684767d6b5e38eaec680ae
Nico Jansen <jansennico@gmail.com>
2022-05-05 19:39:05 +0200

    Remove unused comment

--- packages/jest-runner/src/jest-plugins/cjs/jest-environment-generic.ts
+++ packages/jest-runner/src/jest-plugins/cjs/jest-environment-generic.ts
@@ -5,7 +5,6 @@ import { loadJestEnvironment } from './import-jest-environment.js';
 import { mixinJestEnvironment } from './mixin-jest-environment.js';
 
 export = function jestEnvironmentGeneric(...args: [JestEnvironmentConfig, EnvironmentContext]): JestEnvironment {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
   const JestEnvironmentImpl = loadJestEnvironment(state.jestEnvironment);
   const StrykerAwareJestEnvironment = mixinJestEnvironment(JestEnvironmentImpl);
   return new StrykerAwareJestEnvironment(...args);
