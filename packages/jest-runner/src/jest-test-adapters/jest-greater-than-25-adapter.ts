70e2edb1afd0a321cc66b60729f8f31088d6e490
Nico Jansen <jansennico@gmail.com>
2022-05-05 20:34:55 +0200

    Make sure jest passes with no tests

--- packages/jest-runner/src/jest-test-adapters/jest-greater-than-25-adapter.ts
+++ packages/jest-runner/src/jest-test-adapters/jest-greater-than-25-adapter.ts
@@ -14,6 +14,7 @@ export class JestGreaterThan25TestAdapter implements JestTestAdapter {
         config,
         runInBand: true,
         silent: true,
+        passWithNoTests: true,
         testNamePattern,
         testLocationInResults,
       },
