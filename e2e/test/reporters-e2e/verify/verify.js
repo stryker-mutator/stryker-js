6e3b4ebd247a11eea16b6d905972bec6209084f2
Nico Jansen <jansennico@gmail.com>
2022-07-02 19:01:31 +0200

    Make e2e test deterministic

--- e2e/test/reporters-e2e/verify/verify.js
+++ e2e/test/reporters-e2e/verify/verify.js
@@ -27,8 +27,18 @@ describe('Verify stryker has ran correctly', () => {
 
   it('should report json report with expected results', async () => {
     const result = await readMutationTestingJsonResult();
-    expect(result.files).matchSnapshot();
-    expect(result.testFiles).matchSnapshot();
+    const files = Object.fromEntries(
+      Object.entries(result.files)
+        .map(([fileName, fileResult]) => [fileName, { ...fileResult, mutants: fileResult.mutants.sort((a, b) => (a.id < b.id ? -1 : 1)) }])
+        .sort(([a], [b]) => (a < b ? -1 : 1))
+    );
+    const testFiles = Object.fromEntries(
+      Object.entries(result.testFiles)
+        .map(([fileName, fileResult]) => [fileName, { ...fileResult, tests: fileResult.tests.sort((a, b) => (a.id < b.id ? -1 : 1)) }])
+        .sort(([a], [b]) => (a < b ? -1 : 1))
+    );
+    expect(files).matchSnapshot();
+    expect(testFiles).matchSnapshot();
   });
 
   describe('clearText report', () => {
