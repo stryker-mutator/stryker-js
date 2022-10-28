e87e930c8c1d2bcfd0319a87b383ce30e31f1ec5
Odin van der Linden <odin.vanderlinden@infosupport.com>
2022-10-28 13:39:49 +0200

    getAllParentReferences implementation

--- packages/typescript-checker/src/graph/node.ts
+++ packages/typescript-checker/src/graph/node.ts
@@ -1,7 +1,14 @@
 export class Node {
   constructor(public fileName: string, public parents: Node[] | null, public childs: Node[] | null) {}
 
-  public GetAllParentReferences(): Node[] {
-    return [];
+  public getAllParentReferences(): Node[] {
+    const allParentReferences: Node[] = [];
+    this.parents?.forEach((parent) => {
+      const innerParents = parent.getAllParentReferences();
+      innerParents.forEach((node) => {
+        allParentReferences.push(node);
+      });
+    });
+    return allParentReferences;
   }
 }
