0e7787c5f0149843087e0c12c429bb597d5f7c5f
Teun Lamers <tjwnlamers@gmail.com>
2022-12-05 13:19:52 +0100

    e2e tests svelte project

--- e2e/test/svelte-jest-project/src/App.svelte
+++ e2e/test/svelte-jest-project/src/App.svelte
@@ -2,9 +2,31 @@
   export function sum(a, b) {
     return a + b;
   }
+
+  // This function is  exported, but will not be tested to see if it affects the no coverage statistic
+  export function multiply(a, b) {
+    return a * b;
+  }
+
+  export function isOldEnough(age) {
+    return age >= 18;
+  }
+
+    /**
+   * This loop will result in an infinite loop when Stryker mutates it.
+   */
+  export function loop(n, action) {
+    let goOn = true;
+    while(goOn) {
+      action(n);
+      n--;
+      goOn = n > 0;
+    }
+  }
 </script>
 
 <main>
   <p>Hello world!</p>
   <p>1 + 2 = {sum(1, 2)}</p>
+  <p>Over legal drinking age: {isOldEnough(18)}</p>
 </main>
