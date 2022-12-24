86208f450e2d8fbfa081f3e6e5f8afd8dbb0953b
Arpad Magosanyi <m4gw4s@gmail.com>
2022-12-24 19:24:26 +0100

    first stab at statementRemoverMutator #3913

---
+++ packages/instrumenter/src/mutators/statement-remover-mutator.ts
@@ -0,0 +1,52 @@
+import { NodePath, types } from '@babel/core';
+
+import { ExpressionStatement, CallExpression } from '@babel/types';
+
+import { NodeMutator } from './node-mutator.js';
+
+export const statementRemoverMutator: NodeMutator = {
+  name: 'StatementRemover',
+
+  *mutate(path) {
+    if (path.isStatement()) {
+      if (isOkayToRemove(path)) {
+        yield types.emptyStatement();
+      }
+    }
+  },
+};
+
+function isOkayToRemove(path: NodePath): boolean {
+  if (path.isBlockStatement()) {
+    return false;
+  }
+  if (path.isClassDeclaration()) {
+    return false;
+  }
+  if (path.isImportDeclaration()) {
+    return false;
+  }
+  if (path.isExportNamedDeclaration()) {
+    return false;
+  }
+  if (path.isVariableDeclaration()) {
+    if(path.parentPath.isExportNamedDeclaration()) {
+      return false;
+    }
+    return true;
+  }
+  if (path.isFunctionDeclaration()) {
+    return false;
+  }
+  if (path.node.type == 'ExpressionStatement') {
+    const stmt: ExpressionStatement = path.node;
+    if (stmt.expression.type == 'CallExpression') {
+      const ce: CallExpression = stmt.expression;
+      if (ce.callee.type == 'Super') {
+        return false;
+      }
+    }
+  }
+  console.log(path.node.type);
+  return true;
+}
