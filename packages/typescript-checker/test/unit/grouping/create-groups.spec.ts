3ead12aa4d299b4327c72b3837963939c2009478
Danny Berkelaar <dannyberkelaar@gmail.com>
2023-01-20 18:03:13 +0100

    Removed structuredClone from tests

--- packages/typescript-checker/test/unit/grouping/create-groups.spec.ts
+++ packages/typescript-checker/test/unit/grouping/create-groups.spec.ts
@@ -18,20 +18,18 @@ describe(createGroups.name, () => {
 
   it('two mutants in different files without reference to each other should create single group', () => {
     const mutants = [factory.mutant({ fileName: 'a.js', id: '1' }), factory.mutant({ fileName: 'b.js', id: '2' })];
-    const mutantsClone = structuredClone(mutants);
     const nodes = new Map<string, TSFileNode>([
       ['a.js', new TSFileNode('a.js', [], [])],
       ['b.js', new TSFileNode('b.js', [], [])],
     ]);
     const groups = createGroups(mutants, nodes);
     expect(groups).to.have.lengthOf(1);
-    expect(groups[0][0]).to.be.equal(mutantsClone[0].id);
-    expect(groups[0][1]).to.be.equal(mutantsClone[1].id);
+    expect(groups[0][0]).to.be.equal('1');
+    expect(groups[0][1]).to.be.equal('2');
   });
 
   it('two mutants in different files with reference to each other should create 2 groups', () => {
     const mutants = [factory.mutant({ fileName: 'a.js', id: '1' }), factory.mutant({ fileName: 'b.js', id: '2' })];
-    const mutantsClone = structuredClone(mutants);
     const nodeA = new TSFileNode('a.js', [], []);
     const nodeB = new TSFileNode('b.js', [nodeA], []);
     const nodes = new Map<string, TSFileNode>([
@@ -40,13 +38,12 @@ describe(createGroups.name, () => {
     ]);
     const groups = createGroups(mutants, nodes);
     expect(groups).to.have.lengthOf(2);
-    expect(groups[0][0]).to.be.equal(mutantsClone[0].id);
-    expect(groups[1][0]).to.be.equal(mutantsClone[1].id);
+    expect(groups[0][0]).to.be.equal('1');
+    expect(groups[1][0]).to.be.equal('2');
   });
 
   it('two mutants in different files with circular dependency to each other should create 2 groups', () => {
     const mutants = [factory.mutant({ fileName: 'a.js', id: '1' }), factory.mutant({ fileName: 'b.js', id: '2' })];
-    const mutantsClone = structuredClone(mutants);
     const nodeA = new TSFileNode('a.js', [], []);
     const nodeB = new TSFileNode('b.js', [nodeA], []);
     nodeA.parents.push(nodeB);
@@ -56,19 +53,18 @@ describe(createGroups.name, () => {
     ]);
     const groups = createGroups(mutants, nodes);
     expect(groups).to.have.lengthOf(2);
-    expect(groups[0][0]).to.be.equal(mutantsClone[0].id);
-    expect(groups[1][0]).to.be.equal(mutantsClone[1].id);
+    expect(groups[0][0]).to.be.equal('1');
+    expect(groups[1][0]).to.be.equal('2');
   });
 
   it('two mutants in same file should create 2 groups', () => {
     const mutants = [factory.mutant({ fileName: 'a.js', id: '1' }), factory.mutant({ fileName: 'a.js', id: '2' })];
-    const mutantsClone = structuredClone(mutants);
     const nodeA = new TSFileNode('a.js', [], []);
     const nodes = new Map<string, TSFileNode>([[nodeA.fileName, nodeA]]);
     const groups = createGroups(mutants, nodes);
     expect(groups).to.have.lengthOf(2);
-    expect(groups[0][0]).to.be.equal(mutantsClone[0].id);
-    expect(groups[1][0]).to.be.equal(mutantsClone[1].id);
+    expect(groups[0][0]).to.be.equal('1');
+    expect(groups[1][0]).to.be.equal('2');
   });
 
   it('complex graph should contain multiples 4 groups', () => {
@@ -80,7 +76,6 @@ describe(createGroups.name, () => {
       factory.mutant({ fileName: 'e.js', id: '5' }),
       factory.mutant({ fileName: 'f.js', id: '6' }),
     ];
-    const mutantsClone = structuredClone(mutants);
     const nodeA = new TSFileNode('a.js', [], []);
     const nodeB = new TSFileNode('b.js', [nodeA], []);
     const nodeC = new TSFileNode('c.js', [nodeA], []);
@@ -97,12 +92,12 @@ describe(createGroups.name, () => {
     ]);
     const groups = createGroups(mutants, nodes);
     expect(groups).to.have.lengthOf(4);
-    expect(groups[0][0]).to.be.equal(mutantsClone[0].id);
-    expect(groups[1][0]).to.be.equal(mutantsClone[1].id);
-    expect(groups[1][1]).to.be.equal(mutantsClone[2].id);
-    expect(groups[1][2]).to.be.equal(mutantsClone[4].id);
-    expect(groups[2][0]).to.be.equal(mutantsClone[3].id);
-    expect(groups[3][0]).to.be.equal(mutantsClone[5].id);
+    expect(groups[0][0]).to.be.equal('1');
+    expect(groups[1][0]).to.be.equal('2');
+    expect(groups[1][1]).to.be.equal('3');
+    expect(groups[1][2]).to.be.equal('5');
+    expect(groups[2][0]).to.be.equal('4');
+    expect(groups[3][0]).to.be.equal('6');
   });
 
   it('should throw error when not is not in graph', () => {
