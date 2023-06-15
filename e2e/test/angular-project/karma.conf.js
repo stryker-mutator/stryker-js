a703c60e0b0d874a8a86ae406479ccfbb1d02468
Nico Jansen <jansennico@gmail.com>
2023-06-15 23:44:46 +0200

    chore(e2e): remove "lerna" (#4301)
    Remove lerna from the e2e tests. It was only used as a handy tool to install all test packages. Since [lerna 7 removed `boostrap` support](https://github.com/lerna/lerna/releases/tag/7.0.0), we replace it with a manual script.

--- e2e/test/angular-project/karma.conf.js
+++ e2e/test/angular-project/karma.conf.js
@@ -33,12 +33,7 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
     restartOnFileChange: true
   });
 };
