df42e489205e92fd9446562ed7a8af2982ef1a10
Nico Jansen <jansennico@gmail.com>
2023-10-15 00:29:47 +0200

    test(e2e): use value plugin

--- e2e/test/ignore-project/stryker-plugins/ignorers/global.types.ts
+++ e2e/test/ignore-project/stryker-plugins/ignorers/global.types.ts
@@ -1,6 +1,7 @@
+/// <reference types="@stryker-mutator/api/ignore" />
 import type babel from '@babel/core';
 
 declare module '@stryker-mutator/api/ignore' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface NodePath extends babel.NodePath {}
+  export interface NodePath extends babel.NodePath {}
 }
