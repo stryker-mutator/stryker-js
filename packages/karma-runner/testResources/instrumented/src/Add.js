1127660daf9dcb1ba7a8246cff956062b3f533a3
Nico Jansen <jansennico@gmail.com>
2023-10-24 07:51:29 +0200

    chore(testResources): regenerate instrumentated files

--- packages/karma-runner/testResources/instrumented/src/Add.js
+++ packages/karma-runner/testResources/instrumented/src/Add.js
@@ -1,66 +1,50 @@
 // This file is generated with tasks/instrument-test-resources.js
  function stryNS_9fa48() {
-  var g = new Function("return this")();
+  var g = typeof globalThis === 'object' && globalThis && globalThis.Math === Math && globalThis || new Function("return this")();
   var ns = g.__stryker__ || (g.__stryker__ = {});
-
   if (ns.activeMutant === undefined && g.process && g.process.env && g.process.env.__STRYKER_ACTIVE_MUTANT__) {
     ns.activeMutant = g.process.env.__STRYKER_ACTIVE_MUTANT__;
   }
-
   function retrieveNS() {
     return ns;
   }
-
   stryNS_9fa48 = retrieveNS;
   return retrieveNS();
 }
-
 stryNS_9fa48();
-
 function stryCov_9fa48() {
   var ns = stryNS_9fa48();
   var cov = ns.mutantCoverage || (ns.mutantCoverage = {
     static: {},
     perTest: {}
   });
-
   function cover() {
     var c = cov.static;
-
     if (ns.currentTestId) {
       c = cov.perTest[ns.currentTestId] = cov.perTest[ns.currentTestId] || {};
     }
-
     var a = arguments;
-
     for (var i = 0; i < a.length; i++) {
       c[a[i]] = (c[a[i]] || 0) + 1;
     }
   }
-
   stryCov_9fa48 = cover;
   cover.apply(null, arguments);
 }
-
 function stryMutAct_9fa48(id) {
   var ns = stryNS_9fa48();
-
   function isActive(id) {
     if (ns.activeMutant === id) {
       if (ns.hitCount !== void 0 && ++ns.hitCount > ns.hitLimit) {
         throw new Error('Stryker: Hit count limit reached (' + ns.hitCount + ')');
       }
-
       return true;
     }
-
     return false;
   }
-
   stryMutAct_9fa48 = isActive;
   return isActive(id);
 }
-
 var add = function (num1, num2) {
   if (stryMutAct_9fa48("0")) {
     {}
@@ -69,7 +53,6 @@ var add = function (num1, num2) {
     return stryMutAct_9fa48("1") ? num1 - num2 : (stryCov_9fa48("1"), num1 + num2);
   }
 };
-
 var addOne = function (number) {
   if (stryMutAct_9fa48("2")) {
     {}
@@ -79,7 +62,6 @@ var addOne = function (number) {
     return number;
   }
 };
-
 var negate = function (number) {
   if (stryMutAct_9fa48("4")) {
     {}
@@ -88,14 +70,12 @@ var negate = function (number) {
     return stryMutAct_9fa48("5") ? +number : (stryCov_9fa48("5"), -number);
   }
 };
-
 var isNegativeNumber = function (number) {
   if (stryMutAct_9fa48("6")) {
     {}
   } else {
     stryCov_9fa48("6");
     var isNegative = stryMutAct_9fa48("7") ? true : (stryCov_9fa48("7"), false);
-
     if (stryMutAct_9fa48("11") ? number >= 0 : stryMutAct_9fa48("10") ? number <= 0 : stryMutAct_9fa48("9") ? false : stryMutAct_9fa48("8") ? true : (stryCov_9fa48("8", "9", "10", "11"), number < 0)) {
       if (stryMutAct_9fa48("12")) {
         {}
@@ -104,7 +84,6 @@ var isNegativeNumber = function (number) {
         isNegative = stryMutAct_9fa48("13") ? false : (stryCov_9fa48("13"), true);
       }
     }
-
     return isNegative;
   }
 };
