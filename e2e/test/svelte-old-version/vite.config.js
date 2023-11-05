3df1b3ca8c0d14fb1c40af347be9d08ed7f5a920
TeunLamers <tjwnlamers@gmail.com>
2023-11-05 20:30:42 +0100

    initial svelte old version e2e test

---
+++ e2e/test/svelte-old-version/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./test/vitest.setup.js'],
+  },
+})
