50faf171395975ce909f8a5c0a3534e214879bbf
Nico Jansen <jansennico@gmail.com>
2023-11-06 18:39:30 +0100

    pin version of old to svelte 4.0.0

--- e2e/test/svelte-old-version/src/App.spec.js
+++ e2e/test/svelte-old-version/src/App.spec.js
@@ -1,18 +1,11 @@
 import { describe, it, expect, afterEach } from 'vitest';
-import App, { sum, isOldEnough, loop} from './App.svelte';
+import App, { sum, isOldEnough } from './App.svelte';
 import { render, screen, fireEvent, cleanup } from '@testing-library/svelte';
 
-describe("App.svelte", () => {
-
+describe('App.svelte', () => {
   afterEach(() => {
     cleanup();
   });
-
-  it("says 'hello world!'", () => {
-    render(App);
-    const node = screen.queryByText("Hello world!");
-    expect(node).not.toBeNull();
-  })
 
   it('add one when hitting the button', async () => {
     render(App);
@@ -22,21 +15,10 @@ describe("App.svelte", () => {
   });
 
   it('adds 1 + 2 to equal 3', () => {
-      expect(sum(1, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(3);
   });
 
   it('check if age goes over ', () => {
     expect(isOldEnough(18)).toBe(true);
   });
-
-  it('should result in 15 for n=5 and a sum function', () => {
-    let result = 0;
-    loop(5, (n) => (result += n));
-    expect(result).toEqual(15);
-  });
-  
-})
-
-
-
-
+});
--- e2e/test/svelte-old-version/src/App.svelte
+++ e2e/test/svelte-old-version/src/App.svelte
@@ -2,23 +2,8 @@
   export function sum(a, b) {
     return a + b;
   }
-  // This function is  exported, but will not be tested to see if it affects the no coverage statistic
-  export function multiply(a, b) {
-    return a * b;
-  }
   export function isOldEnough(age) {
     return age >= 18;
-  }
-  /**
-   * This loop will result in an infinite loop when Stryker mutates it.
-   */
-  export function loop(n, action) {
-    let goOn = true;
-    while (goOn) {
-      action(n);
-      n--;
-      goOn = n > 0;
-    }
   }
 </script>
 
@@ -31,5 +16,4 @@
   <p>1 + 2 = {sum(1, 2)}</p>
   <p>Over legal drinking age: {isOldEnough(18)}</p>
   <button on:click={() => hits++}>Hits {hits}</button>
-  <button on:click={() => (hits += 10)}>Add 10 ({hits + 10})</button>
 </main>
