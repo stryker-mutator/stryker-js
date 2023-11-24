b1521383fdcc71b04ea673e0be89a61577648888
Luc Timmerman <48736891+Luctia@users.noreply.github.com>
2023-11-24 20:01:43 +0100

    #20 assignment operator (#46)
    * WIP support for assignmentoperator
    
    * Added assignmentoperator in definitions
    
    * Added tests for edge cases
    
    * Added comments about what's being mutated
    
    * Resolved merge conflict?

--- packages/instrumenter/src/mutators/assignment-operator-mutator.ts
+++ packages/instrumenter/src/mutators/assignment-operator-mutator.ts
@@ -1,22 +1,22 @@
-import type { types as t } from '@babel/core';
+import type { types } from '@babel/core';
 
 import { deepCloneNode } from '../util/index.js';
 
 import { NodeMutator } from './index.js';
 
 const assignmentOperatorReplacements = Object.freeze({
-  '+=': '-=',
-  '-=': '+=',
-  '*=': '/=',
-  '/=': '*=',
-  '%=': '*=',
-  '<<=': '>>=',
-  '>>=': '<<=',
-  '&=': '|=',
-  '|=': '&=',
-  '&&=': '||=',
-  '||=': '&&=',
-  '??=': '&&=',
+  '+=': { replacement: '-=', mutatorName: '+=To-=' },
+  '-=': { replacement: '+=', mutatorName: '-=To+=' },
+  '*=': { replacement: '/=', mutatorName: '*=To/=' },
+  '/=': { replacement: '*=', mutatorName: '/=To*=' },
+  '%=': { replacement: '*=', mutatorName: '%=To*=' },
+  '<<=': { replacement: '>>=', mutatorName: '<<=To>>=' },
+  '>>=': { replacement: '<<=', mutatorName: '>>=To<<=' },
+  '&=': { replacement: '|=', mutatorName: '&=To|=' },
+  '|=': { replacement: '&=', mutatorName: '|=To&=' },
+  '&&=': { replacement: '||=', mutatorName: '&&=To||=' },
+  '||=': { replacement: '&&=', mutatorName: '||=To&&=' },
+  '??=': { replacement: '&&=', mutatorName: '??=To&&=' },
 } as const);
 
 const stringTypes = Object.freeze(['StringLiteral', 'TemplateLiteral']);
@@ -25,9 +25,14 @@ const stringAssignmentTypes = Object.freeze(['&&=', '||=', '??=']);
 export const assignmentOperatorMutator: NodeMutator = {
   name: 'AssignmentOperator',
 
-  *mutate(path) {
-    if (path.isAssignmentExpression() && isSupportedAssignmentOperator(path.node.operator) && isSupported(path.node)) {
-      const mutatedOperator = assignmentOperatorReplacements[path.node.operator];
+  *mutate(path, options) {
+    if (
+      path.isAssignmentExpression() &&
+      isSupportedAssignmentOperator(path.node.operator) &&
+      isSupported(path.node) &&
+      isInMutationLevel(path.node, options)
+    ) {
+      const mutatedOperator = assignmentOperatorReplacements[path.node.operator].replacement;
       const replacement = deepCloneNode(path.node);
       replacement.operator = mutatedOperator;
       yield replacement;
@@ -35,11 +40,19 @@ export const assignmentOperatorMutator: NodeMutator = {
   },
 };
 
+function isInMutationLevel(node: types.AssignmentExpression, operations: string[] | undefined): boolean {
+  if (operations === undefined) {
+    return true;
+  }
+  const { mutatorName } = assignmentOperatorReplacements[node.operator as keyof typeof assignmentOperatorReplacements];
+  return operations.some((op) => op === mutatorName);
+}
+
 function isSupportedAssignmentOperator(operator: string): operator is keyof typeof assignmentOperatorReplacements {
   return Object.keys(assignmentOperatorReplacements).includes(operator);
 }
 
-function isSupported(node: t.AssignmentExpression): boolean {
+function isSupported(node: types.AssignmentExpression): boolean {
   // Excludes assignment operators that apply to strings.
   if (stringTypes.includes(node.right.type) && !stringAssignmentTypes.includes(node.operator)) {
     return false;
--- packages/instrumenter/test/unit/mutators/assignment-operator-mutator.spec.ts
+++ packages/instrumenter/test/unit/mutators/assignment-operator-mutator.spec.ts
@@ -1,7 +1,29 @@
 import { expect } from 'chai';
 
+import { MutationLevel } from '@stryker-mutator/api/core';
+
 import { assignmentOperatorMutator as sut } from '../../../src/mutators/assignment-operator-mutator.js';
-import { expectJSMutation } from '../../helpers/expect-mutation.js';
+import { expectJSMutation, expectJSMutationWithLevel } from '../../helpers/expect-mutation.js';
+
+const assignmentOperatorLevel: MutationLevel = { name: 'AssignmentOperatorLevel', AssignmentOperator: ['-=To+=', '<<=To>>=', '&&=To||='] };
+const assignmentOperatorAllLevel: MutationLevel = {
+  name: 'AssignmentOperatorLevel',
+  AssignmentOperator: [
+    '+=To-=',
+    '-=To+=',
+    '*=To/=',
+    '/=To*=',
+    '%=To*=',
+    '<<=To>>=',
+    '>>=To<<=',
+    '&=To|=',
+    '|=To&=',
+    '&&=To||=',
+    '||=To&&=',
+    '??=To&&=',
+  ],
+};
+const assignmentOperatorUndefinedLevel: MutationLevel = { name: 'AssignmentOperatorLevel' };
 
 describe(sut.name, () => {
   it('should have name "AssignmentOperator"', () => {
@@ -71,5 +93,48 @@ describe(sut.name, () => {
     expectJSMutation(sut, 'a &&= `b`', 'a ||= `b`');
     expectJSMutation(sut, 'a ||= `b`', 'a &&= `b`');
     expectJSMutation(sut, 'a ??= `b`', 'a &&= `b`');
+  });
+
+  it('should only mutate what is defined in the mutator level', () => {
+    expectJSMutationWithLevel(
+      sut,
+      assignmentOperatorLevel.AssignmentOperator,
+      'a += b; a -= b; a *= b; a /= b; a <<= b; a &&= b;',
+      'a += b; a += b; a *= b; a /= b; a <<= b; a &&= b;', // mutated -= to +=
+      'a += b; a -= b; a *= b; a /= b; a >>= b; a &&= b;', // mutated <<= to >>=
+      'a += b; a -= b; a *= b; a /= b; a <<= b; a ||= b;', // mutated &&= to ||=
+    );
+  });
+
+  it('should not mutate anything if there are no values in the mutation level', () => {
+    expectJSMutationWithLevel(sut, [], 'a += b; a -= b; a *= b; a /= b; a <<= b; a &&= b;');
+  });
+
+  it('should mutate everything if everything is in the mutation level', () => {
+    expectJSMutationWithLevel(
+      sut,
+      assignmentOperatorAllLevel.BooleanLiteral,
+      'a += b; a -= b; a *= b; a /= b; a <<= b; a &&= b;',
+      'a -= b; a -= b; a *= b; a /= b; a <<= b; a &&= b;', // mutated += to -=
+      'a += b; a += b; a *= b; a /= b; a <<= b; a &&= b;', // mutated -= to +=
+      'a += b; a -= b; a /= b; a /= b; a <<= b; a &&= b;', // mutated *= to /=
+      'a += b; a -= b; a *= b; a *= b; a <<= b; a &&= b;', // mutated /= to *=
+      'a += b; a -= b; a *= b; a /= b; a >>= b; a &&= b;', // mutated <<= to >>=
+      'a += b; a -= b; a *= b; a /= b; a <<= b; a ||= b;', // mutated &&= to ||=
+    );
+  });
+
+  it('should mutate everything if the mutation level is undefined', () => {
+    expectJSMutationWithLevel(
+      sut,
+      assignmentOperatorUndefinedLevel.BooleanLiteral,
+      'a += b; a -= b; a *= b; a /= b; a <<= b; a &&= b;',
+      'a -= b; a -= b; a *= b; a /= b; a <<= b; a &&= b;', // mutated += to -=
+      'a += b; a += b; a *= b; a /= b; a <<= b; a &&= b;', // mutated -= to +=
+      'a += b; a -= b; a /= b; a /= b; a <<= b; a &&= b;', // mutated *= to /=
+      'a += b; a -= b; a *= b; a *= b; a <<= b; a &&= b;', // mutated /= to *=
+      'a += b; a -= b; a *= b; a /= b; a >>= b; a &&= b;', // mutated <<= to >>=
+      'a += b; a -= b; a *= b; a /= b; a <<= b; a ||= b;', // mutated &&= to ||=
+    );
   });
 });
