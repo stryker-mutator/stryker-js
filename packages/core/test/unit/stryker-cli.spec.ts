e190207e25926179c1a3ed2c0ff97a13720c57bd
Nico Jansen <jansennico@gmail.com>
2023-11-30 15:47:28 +0100

    feat(node): drop official support for node 16 (#4542)
    - Set TS target to es2022 for more concise JavaScript
    - Drop support for node 16. The lowest supported version is now NodeJS 18.
    
    BREAKING CHANGE: NodeJS 16 is no longer supported. Please use NodeJS 18 or higher. See https://nodejs.org/en/about/previous-releases

--- packages/core/test/unit/stryker-cli.spec.ts
+++ packages/core/test/unit/stryker-cli.spec.ts
@@ -108,12 +108,12 @@ describe(StrykerCli.name, () => {
 
   describe(guardMinimalNodeVersion.name, () => {
     it('should fail for < v16.0.0', () => {
-      expect(() => guardMinimalNodeVersion('v14.21.3')).throws(
-        'Node.js version v14.21.3 detected. StrykerJS requires version to match >=16.0.0. Please update your Node.js version or visit https://nodejs.org/ for additional instructions',
+      expect(() => guardMinimalNodeVersion('v16.20.2')).throws(
+        'Node.js version v16.20.2 detected. StrykerJS requires version to match >=18.0.0. Please update your Node.js version or visit https://nodejs.org/ for additional instructions',
       );
     });
-    it('should not fail for >= v16.0.0', () => {
-      expect(() => guardMinimalNodeVersion('v16.0.0')).not.throws();
+    it('should not fail for >= v18.0.0', () => {
+      expect(() => guardMinimalNodeVersion('v18.0.0')).not.throws();
     });
   });
 
