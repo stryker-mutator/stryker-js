7ac2587d9a4a2d2ce1df1837a0e36df7236d27a9
Ja4pp <70022125+Ja4pp@users.noreply.github.com>
2023-12-05 12:11:35 +0100

    #30 restrict updateoperator mutator (#51)
    * updated to conform to mutationlevels
    
    * modified tests
    
    * modified config file
    
    * initial implementation of condition
    
    * Revert "initial implementation of condition"
    
    This reverts commit bcb6cc0c355534a50ca9c01c378576080c123351.
    
    * changed tests to pass CI/CD
    
    * changes nessecary for pass
    
    * small refactor
    
    * remove line added by mistake
    
    ---------
    
    Co-authored-by: Danut Copae <66919951+dvcopae@users.noreply.github.com>
    Co-authored-by: Danut Copae <d.v.copae@gmail.com>

--- packages/instrumenter/src/mutators/update-operator-mutator.ts
+++ packages/instrumenter/src/mutators/update-operator-mutator.ts
@@ -6,17 +6,39 @@ import { NodeMutator } from './index.js';
 
 const { types } = babel;
 
-enum UpdateOperators {
-  '++' = '--',
-  '--' = '++',
-}
+const operators = Object.assign({
+  'Post++To--': { replacementOperator: '--', mutatorName: 'Post++To--' },
+  'Post--To++': { replacementOperator: '++', mutatorName: 'Post--To++' },
+  'Pre++To--': { replacementOperator: '--', mutatorName: 'Pre++To--' },
+  'Pre--To++': { replacementOperator: '++', mutatorName: 'Pre--To++' },
+  '++': { replacementOperator: '--', mutatorName: '++all' },
+  '--': { replacementOperator: '++', mutatorName: '--all' },
+} as const);
 
 export const updateOperatorMutator: NodeMutator = {
   name: 'UpdateOperator',
 
-  *mutate(path) {
+  *mutate(path, operations) {
     if (path.isUpdateExpression()) {
-      yield types.updateExpression(UpdateOperators[path.node.operator], deepCloneNode(path.node.argument), path.node.prefix);
+      if (operations === undefined) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        yield types.updateExpression(operators[path.node.operator].replacementOperator, deepCloneNode(path.node.argument), path.node.prefix);
+      } else {
+        let replacement = undefined;
+        if (path.node.prefix && path.node.operator == '++' && operations.includes(operators['Pre++To--'].mutatorName as string)) {
+          replacement = operators['Pre++To--'].replacementOperator;
+        } else if (path.node.prefix && path.node.operator == '--' && operations.includes(operators['Pre--To++'].mutatorName as string)) {
+          replacement = operators['Pre--To++'].replacementOperator;
+        } else if (!path.node.prefix && path.node.operator == '++' && operations.includes(operators['Post++To--'].mutatorName as string)) {
+          replacement = operators['Post++To--'].replacementOperator;
+        } else if (!path.node.prefix && path.node.operator == '--' && operations.includes(operators['Post--To++'].mutatorName as string)) {
+          replacement = operators['Post--To++'].replacementOperator;
+        }
+        if (replacement !== undefined) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+          yield types.updateExpression(replacement, deepCloneNode(path.node.argument), path.node.prefix);
+        }
+      }
     }
   },
 };
--- packages/instrumenter/test/unit/mutators/update-operator-mutator.spec.ts
+++ packages/instrumenter/test/unit/mutators/update-operator-mutator.spec.ts
@@ -1,7 +1,11 @@
 import { expect } from 'chai';
 
 import { updateOperatorMutator as sut } from '../../../src/mutators/update-operator-mutator.js';
-import { expectJSMutation } from '../../helpers/expect-mutation.js';
+import { expectJSMutation, expectJSMutationWithLevel } from '../../helpers/expect-mutation.js';
+
+const updateLevel: string[] = ['Pre--To++', 'Pre++To--'];
+const updateLevel2: string[] = ['Post++To--', 'Post--To++'];
+const updateLevel3 = undefined;
 
 describe(sut.name, () => {
   it('should have name "UpdateOperator"', () => {
@@ -22,5 +26,37 @@ describe(sut.name, () => {
 
   it('should mutate --a to ++a', () => {
     expectJSMutation(sut, '--a', '++a');
+  });
+
+  it('should only mutate --a and ++a', () => {
+    expectJSMutationWithLevel(
+      sut,
+      updateLevel,
+      '--a; ++a; a--; a++',
+      '++a; ++a; a--; a++', //mutates --a
+      '--a; --a; a--; a++', //mutates ++a
+    );
+  });
+
+  it('should only mutate a-- and a++', () => {
+    expectJSMutationWithLevel(
+      sut,
+      updateLevel2,
+      '--a; ++a; a--; a++',
+      '--a; ++a; a--; a--', //mutates a++
+      '--a; ++a; a++; a++', //mutates a--
+    );
+  });
+
+  it('should mutate all', () => {
+    expectJSMutationWithLevel(
+      sut,
+      updateLevel3,
+      '--a; ++a; a--; a++',
+      '++a; ++a; a--; a++', //mutates --a
+      '--a; --a; a--; a++', //mutates ++a
+      '--a; ++a; a--; a--', //mutates a++
+      '--a; ++a; a++; a++', //mutates a--
+    );
   });
 });
