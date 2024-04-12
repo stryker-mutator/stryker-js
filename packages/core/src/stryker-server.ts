639b9b452bcda81bf8eaef33660ca5b24136ce25
Jasper van der Veen <jasper.vanderveen99@gmail.com>
2024-04-12 18:40:01 +0200

    chore: no longer keep preparation context in memory

--- packages/core/src/stryker-server.ts
+++ packages/core/src/stryker-server.ts
@@ -1,31 +1,19 @@
 import { WebSocketServer, WebSocket } from 'ws';
 import { JSONRPCServer, TypedJSONRPCServer, createJSONRPCErrorResponse } from 'json-rpc-2.0';
-
-import { Injector, createInjector } from 'typed-inject';
-
-import { commonTokens } from '@stryker-mutator/api/plugin';
-
+import { createInjector } from 'typed-inject';
 import { MutationTestResult } from 'mutation-testing-report-schema';
-
-import { MutantResult, StrykerOptions } from '@stryker-mutator/api/core';
-
+import { MutantResult } from '@stryker-mutator/api/core';
 import { Command } from 'commander';
 
 import { MutationServerMethods } from './server/mutation-server-methods.js';
-
 import { provideLogger } from './di/provide-logger.js';
 import { PrepareExecutor } from './process/1-prepare-executor.js';
-import { MutantInstrumenterContext } from './process/2-mutant-instrumenter-executor.js';
 import { MutantInstrumenterExecutor } from './server/mutant-instrument-executor.js';
-import { ProjectReader } from './fs/project-reader.js';
-import { coreTokens } from './di/index.js';
 import { Stryker } from './stryker.js';
 
 export class StrykerServer {
   private readonly jsonRpcServer: TypedJSONRPCServer<MutationServerMethods>;
   private readonly webSocketServer: WebSocketServer;
-  private mutantInstrumenterInjector: Injector<MutantInstrumenterContext> | undefined;
-  private options: StrykerOptions | undefined;
 
   constructor(
     private readonly argv: string[],
@@ -64,34 +52,18 @@ export class StrykerServer {
   }
 
   private async instrument(globPatterns?: string[]): Promise<MutationTestResult> {
-    // Load instrumenter context once and reuse it partially for every subsequent run for performance reasons
-    if (!this.mutantInstrumenterInjector) {
-      this.mutantInstrumenterInjector = await this.provideInstrumenterContext();
-      this.options = this.mutantInstrumenterInjector.resolve(commonTokens.options);
-    }
-    const options: StrykerOptions = { ...this.options! };
-
-    if (globPatterns) {
-      // Override the mutate property with the provided glob patterns
-      options.mutate = globPatterns;
-    }
-
-    const updatedMutantInstrumenterInjector = this.mutantInstrumenterInjector.provideValue(commonTokens.options, options);
-
-    // Reload project as files might have changed since the last run
-    const project = await updatedMutantInstrumenterInjector.injectClass(ProjectReader).read();
-
-    const mutantInstrumenter = updatedMutantInstrumenterInjector.provideValue(coreTokens.project, project).injectClass(MutantInstrumenterExecutor);
-
-    return await mutantInstrumenter.execute();
-  }
-
-  private async provideInstrumenterContext() {
     const rootInjector = this.injectorFactory();
+
     const loggerProvider = provideLogger(rootInjector);
 
     const prepareExecutor = loggerProvider.injectClass(PrepareExecutor);
-    return await prepareExecutor.execute({});
+
+    const options = globPatterns?.length ? { mutate: globPatterns } : {};
+    const mutantInstrumenterInjector = await prepareExecutor.execute(options);
+
+    const mutantInstrumenter = mutantInstrumenterInjector.injectClass(MutantInstrumenterExecutor);
+
+    return await mutantInstrumenter.execute();
   }
 
   private async mutate(globPatterns?: string[]): Promise<MutantResult[]> {
