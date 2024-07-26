7280076ce22477867f0eb0b6a169678c81375a25
Nico Jansen <jansennico@gmail.com>
2024-07-26 11:45:18 +0200

    Fix failing test

--- packages/instrumenter/testResources/instrumenter/type-definitions.ts
+++ packages/instrumenter/testResources/instrumenter/type-definitions.ts
@@ -2,5 +2,3 @@
  * @see https://github.com/stryker-mutator/stryker-js/issues/2465
  */ 
 const flatten = require('lodash/flatten') as typeof import('lodash/flatten');
-
-fields.forEach((field) => ((value[field] as any) = (entity as any)[field]._));
