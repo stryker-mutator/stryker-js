03946fa597004cfc93c02f309d702a4661a0cd4a
Nico Jansen <jansennico@gmail.com>
2024-07-27 15:37:24 +0200

    remove 'chai' dependence from karma integration tests

--- packages/karma-runner/testResources/configs/mocha-client-options-echo-ui.spec.js
+++ packages/karma-runner/testResources/configs/mocha-client-options-echo-ui.spec.js
@@ -1,10 +1,11 @@
 suite('mocha client options', () => {
-  test('should not override client options', async function() {
-    expect(mocha.options.global).include('jQuery');
+  test('should not override client options', async function () {
+    if (!mocha.options.global.includes('jQuery')) {
+      throw new Error('Expected global to include jQuery');
+    }
   });
   test('should override bail', async () => {
     throw new Error('Expected exception');
   });
-  test('should not execute this test', async () => {
-  });
-})
+  test('should not execute this test', async () => {});
+});
