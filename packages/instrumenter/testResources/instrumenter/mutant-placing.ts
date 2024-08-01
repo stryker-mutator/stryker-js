4b8885c06f84854ffa24e4c145a70231e9fa930a
Nico Jansen <jansennico@gmail.com>
2024-08-01 22:45:03 +0200

    fix(mutant placement): don't place mutants inside assignment expression (#4927)
    Prevent trying to place mutants inside assignment expresssions.
    
    Fixes #4884

--- packages/instrumenter/testResources/instrumenter/mutant-placing.ts
+++ packages/instrumenter/testResources/instrumenter/mutant-placing.ts
@@ -2,3 +2,7 @@
 
 // https://github.com/stryker-mutator/stryker-js/issues/3702
 directoryFiles[file[0].substr(1)] = file[1]
+
+// https://github.com/stryker-mutator/stryker-js/issues/4884
+initialNodes.filter((n) => n.id === 'tiptilt')[0].className = tiptiltState;
+
