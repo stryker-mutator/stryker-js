3eae54c7bf2915061da92c66204a25d65577cfa3
Nico Jansen <jansennico@gmail.com>
2024-10-18 16:57:51 +0200

    Chunk discover

--- packages/core/client.ts
+++ packages/core/client.ts
@@ -1,35 +1,45 @@
+import type { DiscoverResult } from './src/mtsp-schema.js';
 import { JSONRPCClient, JSONRPCServer, JSONRPCServerAndClient } from 'json-rpc-2.0';
 import net from 'net';
 
 class Sender {
   #client: JSONRPCServerAndClient;
+  #msgQueue: string[] = [];
 
   constructor(socket: net.Socket) {
     this.#client = new JSONRPCServerAndClient(
       new JSONRPCServer(),
       new JSONRPCClient((jsonRPCRequest) => {
-        socket.write(JSON.stringify(jsonRPCRequest));
+        socket.write(`${JSON.stringify(jsonRPCRequest)}\n`);
       }),
     );
-    this.#client.addMethod('count', ({ count }) => console.log(`Received: ${count}`));
     socket.on('data', (data) => {
-      this.#client.receiveAndSend(JSON.parse(data.toString()));
+      this.#msgQueue.push(data.toString());
+      try {
+        const msg = JSON.parse(this.#msgQueue.join(''));
+        this.#msgQueue = [];
+        this.#client.receiveAndSend(msg);
+      } catch (e) {
+        console.log('caught', e);
+      }
     });
   }
 
-  discover(initial: number) {
-    return this.#client.request('start-counting', { initial });
+  discover(): PromiseLike<DiscoverResult> {
+    return this.#client.request('discover', {});
   }
 }
 
 const socket = new net.Socket();
 console.log('start!');
-socket.connect(42879, 'localhost');
+socket.connect(44269, 'localhost');
 socket.setEncoding('utf-8');
 const sender = new Sender(socket);
 
 socket.on('close', function () {
   console.log('Connection closed');
 });
-const result = await sender.discover({});
-console.log('Finished with', result);
+const result = await sender.discover();
+import fs from 'fs';
+fs.writeFileSync('discover.json', JSON.stringify(result, null, 2));
+console.log('Finished with', result.mutants.length, 'mutants');
