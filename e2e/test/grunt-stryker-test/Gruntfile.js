cfeb31d49aae176970de1d9f01ba43a0ed890cbb
Nico Jansen <jansennico@gmail.com>
2025-10-24 23:46:35 +0200

    fix grunt e2e test

--- e2e/test/grunt-stryker-test/Gruntfile.js
+++ e2e/test/grunt-stryker-test/Gruntfile.js
@@ -3,17 +3,11 @@ module.exports = function (grunt) {
     stryker: {
       withoutConfigFile: {
         files: {
-          src: [
-            'sampleProject/src/**/*.js'
-          ],
-          tests: [
-            'sampleProject/test/**/*.js'
-          ]
+          src: ['sampleProject/src/**/*.js'],
+          tests: ['sampleProject/test/**/*.js'],
         },
         mutate: {
-          src: [
-            'sampleProject/src/**/*.js'
-          ]
+          src: ['sampleProject/src/**/*.js'],
         },
         options: {
           testRunner: 'karma',
@@ -21,18 +15,24 @@ module.exports = function (grunt) {
           fileLogLevel: 'warn',
           concurrency: 2,
           tempDirName: '.stryker-tmp-2',
-          karma: { config: { files: [ 'sampleProject/**' ], plugins: [ 'karma-jasmine', 'karma-chrome-launcher' ] } },
-          plugins: [
-            '@stryker-mutator/karma-runner'
-          ]
+          karma: {
+            config: {
+              files: ['sampleProject/**'],
+              plugins: [
+                require.resolve('karma-jasmine'),
+                require.resolve('karma-chrome-launcher'),
+              ],
+            },
+          },
+          plugins: ['@stryker-mutator/karma-runner'],
         },
       },
       withConfigFile: {
         options: {
-          configFile: './stryker.grunt.conf.json'
-        }
-      }
-    }
+          configFile: './stryker.grunt.conf.js',
+        },
+      },
+    },
   });
 
   require('grunt-stryker/tasks/stryker')(grunt);
