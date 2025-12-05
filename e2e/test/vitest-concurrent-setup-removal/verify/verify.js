25a6d48b73ea1e0d5b82e9df42b39af03becfb3e
Andreas Hoornstra <69248785+Andreas02-dev@users.noreply.github.com>
2025-12-05 13:12:26 +0100

    fix(vitest-runner): fix ebusy warning on windows when concurrency >=2 (#5671)
    * test(vitest-concurrent-setup-removal): add repro test for stryker warning
    
    on windows, the following error will be logged when using vitest with
    concurrency set higher than 1:
    ```
    10:55:32 (568) WARN ChildProcessTestRunnerProxy An unexpected error occurred
    during test runner disposal. This might be worth looking into. Stryker will
    ignore this error. StrykerError: Error: EBUSY (unlink) Error: EBUSY: resource
    busy or locked, unlink 'C:\vitest-concurrent-setup-removal\.stryker-tmp\sandbox
    -GWmgX6\stryker-setup-2.js'
        at ChildProcess.<anonymous>
    (file:///C:/vitest-concurrent-setup-removal/node_modules/@stryker-mutator/core/
    dist/src/child-proxy/child-process-proxy.js:123:33)
        at ChildProcess.emit (node:events:508:28)
        at emit (node:internal/child_process:949:14)
        at process.processTicksAndRejections (node:internal/process/task_queues:91:
    21) {
      innerError: undefined
    }
    ```
    
    * fix(vitest-runner): fix ebusy resource busy or locked warning on windows when
    concurrency is >=2
    
    closes #5670

---
+++ e2e/test/vitest-concurrent-setup-removal/verify/verify.js
@@ -0,0 +1,13 @@
+import { expectMetricsJsonToMatchSnapshot } from '../../../helpers.js';
+import fs from 'fs';
+import { expect } from 'chai';
+
+describe('Verify stryker has ran correctly', () => {
+  it('should report correct score', async () => {
+    await expectMetricsJsonToMatchSnapshot();
+  });
+
+  it("should not have any warnings, errors or fatal logs, so doesn't create 'stryker.log'", () => {
+    expect(fs.existsSync('./stryker.log')).to.be.false;
+  });
+});
