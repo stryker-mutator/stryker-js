8f3767f362a770f1d1f83ec9e3f96030b553d7da
Andreas Hoornstra <69248785+Andreas02-dev@users.noreply.github.com>
2026-01-16 18:26:57 +0100

    chore(vitest-browser-mode): add configurable browser executable path (#5735)
    Optionally allows for configuring the used browser executable using an
    environment variable. This can be useful to allow using pre-installed browser
    versions instead of having to install the Playwright browser executable.

--- e2e/test/coverage-analysis/vitest.browser.config.js
+++ e2e/test/coverage-analysis/vitest.browser.config.js
@@ -4,7 +4,11 @@ import { playwright } from '@vitest/browser-playwright';
 config.test.browser = {
   enabled: true,
   instances: [{ browser: 'chromium' }],
-  provider: playwright(),
+  provider: playwright({
+    launchOptions: {
+      executablePath: process.env.PLAYWRIGHT_LAUNCH_OPTIONS_EXECUTABLE_PATH,
+    },
+  }),
   headless: true,
 };
 
