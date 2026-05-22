54b8372c0975761307e5db4bf6de2be8e8c05430
Andreas Hoornstra <andreashoornstra@gmail.com>
2026-05-22 22:46:22 +0200

    feat: empty-expression-mutator
    closes: #5765

---
+++ packages/instrumenter/src/mutators/empty-expression-mutator.ts
@@ -0,0 +1,32 @@
+import babel from '@babel/core';
+import { NodeMutator } from './node-mutator.js';
+
+const { types } = babel;
+
+function isSuperCall(expression: babel.types.CallExpression): boolean {
+  return expression.callee.type === 'Super';
+}
+
+export const emptyExpressionMutator: NodeMutator = {
+  name: 'CallExpression',
+
+  *mutate(path) {
+    if (path.node.type === 'ExpressionStatement') {
+      if (
+        path.node.expression.type === 'CallExpression' &&
+        !isSuperCall(path.node.expression)
+      ) {
+        yield types.emptyStatement();
+      }
+    }
+
+    if (path.node.type === 'ThrowStatement')
+      if (path.node.argument.type === 'NewExpression') {
+        yield types.emptyStatement();
+      }
+  },
+
+  filter(mutantsInScope) {
+    return mutantsInScope.length === 1;
+  },
+};
